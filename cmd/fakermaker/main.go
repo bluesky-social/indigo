094f1d5d152895df8a738d1f3c064c1c426abac7
bryan newbold <bnewbold@robocracy.org>
2023-02-12 20:31:04 -0800

    fakermaker: refactor cmds and use xrpc.Client directly

--- cmd/fakermaker/main.go
+++ cmd/fakermaker/main.go
@@ -1,4 +1,4 @@
-// Tool to generate fake accounts and fake content.
+// Tool to generate fake accounts, content, and interactions.
 // Intended for development and benchmarking. Similar to 'stress' and could
 // merge at some point.
 
@@ -12,10 +12,10 @@ import (
 	"os"
 	"time"
 
-	"github.com/bluesky-social/indigo/api"
 	comatproto "github.com/bluesky-social/indigo/api/atproto"
 	appbsky "github.com/bluesky-social/indigo/api/bsky"
 	cliutil "github.com/bluesky-social/indigo/cmd/gosky/util"
+	lexutil "github.com/bluesky-social/indigo/lex/util"
 	"github.com/bluesky-social/indigo/xrpc"
 
 	"github.com/brianvoe/gofakeit/v6"
@@ -58,7 +58,7 @@ func main() {
 	app.Commands = []*cli.Command{
 		&cli.Command{
 			Name:   "gen-accounts",
-			Usage:  "create accounts (DID, handle, no content)",
+			Usage:  "create accounts (DID, handle, profile)",
 			Action: genAccounts,
 			Flags: []cli.Flag{
 				&cli.IntFlag{
@@ -74,9 +74,9 @@ func main() {
 			},
 		},
 		&cli.Command{
-			Name:   "gen-initial-content",
-			Usage:  "creates social graph and initial posts for accounts",
-			Action: genInitialContent,
+			Name:   "gen-graph",
+			Usage:  "creates social graph (follows and mutes)",
+			Action: genGraph,
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:  "catalog",
@@ -111,9 +111,9 @@ func main() {
 			},
 		},
 		&cli.Command{
-			Name:   "gen-content",
-			Usage:  "creates posts and interactions for accounts",
-			Action: genContent,
+			Name:   "gen-posts",
+			Usage:  "creates posts for accounts",
+			Action: genPosts,
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:  "catalog",
@@ -135,6 +135,18 @@ func main() {
 					Usage: "of posts created, fraction to include mentions in",
 					Value: 0.50,
 				},
+			},
+		},
+		&cli.Command{
+			Name:   "gen-interactions",
+			Usage:  "create interactions between accounts",
+			Action: genInteractions,
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:  "catalog",
+					Usage: "file path of account catalog JSON file",
+					Value: "fakermaker-accounts.json",
+				},
 				&cli.Float64Flag{
 					Name:  "frac-like",
 					Usage: "fraction of posts in timeline to like",
@@ -153,9 +165,9 @@ func main() {
 			},
 		},
 		&cli.Command{
-			Name:   "bench-browsing",
+			Name:   "run-browsing",
 			Usage:  "creates read-only load on service (notifications, timeline, etc)",
-			Action: benchBrowsing,
+			Action: runBrowsing,
 			Flags: []cli.Flag{
 				&cli.StringFlag{
 					Name:  "catalog",
@@ -192,9 +204,10 @@ func genAccounts(cctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
+	xrpcc := atpc.C
 	adminToken := cctx.String("admin-token")
 	if len(adminToken) > 0 {
-		atpc.C.AdminToken = &adminToken
+		xrpcc.AdminToken = &adminToken
 	}
 
 	// call helper to do actual creation
@@ -203,7 +216,7 @@ func genAccounts(cctx *cli.Context) error {
 	countCelebrities := cctx.Int("count-celebrities")
 	t1 := measureIterations("register celebrity accounts", countCelebrities)
 	for i := 0; i < countCelebrities; i++ {
-		if usr, err = pdsGenAccount(atpc, i, "celebrity"); err != nil {
+		if usr, err = pdsGenAccount(xrpcc, i, "celebrity"); err != nil {
 			return err
 		}
 		// compact single-line JSON by default
@@ -216,7 +229,7 @@ func genAccounts(cctx *cli.Context) error {
 	countRegulars := cctx.Int("count-regulars")
 	t2 := measureIterations("register regular accounts", countRegulars)
 	for i := 0; i < countRegulars; i++ {
-		if usr, err = pdsGenAccount(atpc, i, "regular"); err != nil {
+		if usr, err = pdsGenAccount(xrpcc, i, "regular"); err != nil {
 			return err
 		}
 		// compact single-line JSON by default
@@ -239,8 +252,7 @@ func measureIterations(name string, count int) func() {
 		log.Infof("%s wall runtime: count=%d total=%s mean=%s", name, count, total, total/time.Duration(count))
 	}
 }
-
-func pdsGenAccount(atpc *api.ATProto, index int, accountType string) (*AccountContext, error) {
+func pdsGenAccount(xrpcc *xrpc.Client, index int, accountType string) (*AccountContext, error) {
 	var suffix string
 	if accountType == "celebrity" {
 		suffix = "C"
@@ -255,7 +267,11 @@ func pdsGenAccount(atpc *api.ATProto, index int, accountType string) (*AccountCo
 	email := gofakeit.Email()
 	password := gofakeit.Password(true, true, true, true, true, 24)
 	ctx := context.TODO()
-	resp, err := atpc.CreateAccount(ctx, email, handle, password, nil)
+	resp, err := comatproto.AccountCreate(ctx, xrpcc, &comatproto.AccountCreate_Input{
+		Email:    email,
+		Handle:   handle,
+		Password: password,
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -309,18 +325,15 @@ func readAccountCatalog(path string) (*AccountCatalog, error) {
 	return catalog, nil
 }
 
-func genInitialContent(cctx *cli.Context) error {
+func genGraph(cctx *cli.Context) error {
 	catalog, err := readAccountCatalog(cctx.String("catalog"))
 	if err != nil {
 		return err
 	}
 
 	pdsHost := cctx.String("pds")
-	maxPosts := cctx.Int("max-posts")
 	maxFollows := cctx.Int("max-follows")
 	maxMutes := cctx.Int("max-mutes")
-	fracImage := cctx.Float64("frac-image")
-	fracMention := cctx.Float64("frac-mention")
 	httpClient := cliutil.NewHttpClient()
 
 	if maxFollows > len(catalog.Regulars) {
@@ -333,25 +346,19 @@ func genInitialContent(cctx *cli.Context) error {
 	// TODO: profile: avatar, display name, description
 	for _, acc := range append(catalog.Celebs, catalog.Regulars...) {
 		// create an ATP client for this specific user
-		atpc := &api.ATProto{
-			C: &xrpc.Client{
-				Client: httpClient,
-				Host:   pdsHost,
-				Auth:   &acc.Auth,
-			},
+		xrpcc := &xrpc.Client{
+			Client: httpClient,
+			Host:   pdsHost,
+			Auth:   &acc.Auth,
 		}
-
-		if err = pdsGenPosts(atpc, catalog, &acc, maxPosts, fracImage, fracMention); err != nil {
-			return err
-		}
-		if err = pdsGenFollowsAndMutes(atpc, catalog, &acc, maxFollows, maxMutes); err != nil {
+		if err = pdsGenFollowsAndMutes(xrpcc, catalog, &acc, maxFollows, maxMutes); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func pdsGenPosts(atpc *api.ATProto, catalog *AccountCatalog, acc *AccountContext, maxPosts int, fracImage float64, fracMention float64) error {
+func pdsGenPosts(xrpcc *xrpc.Client, catalog *AccountCatalog, acc *AccountContext, maxPosts int, fracImage float64, fracMention float64) error {
 
 	var mention *appbsky.FeedPost_Entity
 	var tgt *AccountContext
@@ -404,7 +411,11 @@ func pdsGenPosts(atpc *api.ATProto, catalog *AccountCatalog, acc *AccountContext
 		if mention != nil {
 			post.Entities = []*appbsky.FeedPost_Entity{mention}
 		}
-		if _, err := atpc.RepoCreateRecord(ctx, acc.Auth.Did, "app.bsky.feed.post", true, &post); err != nil {
+		if _, err := comatproto.RepoCreateRecord(ctx, xrpcc, &comatproto.RepoCreateRecord_Input{
+			Collection: "app.bsky.feed.post",
+			Did:        acc.Auth.Did,
+			Record:     lexutil.LexiconTypeDecoder{&post},
+		}); err != nil {
 			return err
 		}
 	}
@@ -412,7 +423,7 @@ func pdsGenPosts(atpc *api.ATProto, catalog *AccountCatalog, acc *AccountContext
 	return nil
 }
 
-func pdsCreateFollow(atpc *api.ATProto, tgt *AccountContext) error {
+func pdsCreateFollow(xrpcc *xrpc.Client, tgt *AccountContext) error {
 	follow := &appbsky.GraphFollow{
 		CreatedAt: time.Now().Format(time.RFC3339),
 		Subject: &appbsky.ActorRef{
@@ -421,11 +432,15 @@ func pdsCreateFollow(atpc *api.ATProto, tgt *AccountContext) error {
 			DeclarationCid: "bafyreid27zk7lbis4zw5fz4podbvbs4fc5ivwji3dmrwa6zggnj4bnd57u",
 		},
 	}
-	_, err := atpc.RepoCreateRecord(context.TODO(), atpc.C.Auth.Did, "app.bsky.graph.follow", true, &follow)
+	_, err := comatproto.RepoCreateRecord(context.TODO(), xrpcc, &comatproto.RepoCreateRecord_Input{
+		Collection: "app.bsky.graph.follow",
+		Did:        xrpcc.Auth.Did,
+		Record:     lexutil.LexiconTypeDecoder{follow},
+	})
 	return err
 }
 
-func pdsCreateLike(atpc *api.ATProto, viewPost *appbsky.FeedFeedViewPost) error {
+func pdsCreateLike(xrpcc *xrpc.Client, viewPost *appbsky.FeedFeedViewPost) error {
 	vote := &appbsky.FeedSetVote_Input{
 		Direction: "up",
 		Subject: &comatproto.RepoStrongRef{
@@ -434,11 +449,11 @@ func pdsCreateLike(atpc *api.ATProto, viewPost *appbsky.FeedFeedViewPost) error 
 		},
 	}
 	// TODO: may have already voted? in that case should ignore error
-	_, err := appbsky.FeedSetVote(context.TODO(), atpc.C, vote)
+	_, err := appbsky.FeedSetVote(context.TODO(), xrpcc, vote)
 	return err
 }
 
-func pdsCreateRepost(atpc *api.ATProto, viewPost *appbsky.FeedFeedViewPost) error {
+func pdsCreateRepost(xrpcc *xrpc.Client, viewPost *appbsky.FeedFeedViewPost) error {
 	repost := &appbsky.FeedRepost{
 		CreatedAt: time.Now().Format(time.RFC3339),
 		Subject: &comatproto.RepoStrongRef{
@@ -446,11 +461,15 @@ func pdsCreateRepost(atpc *api.ATProto, viewPost *appbsky.FeedFeedViewPost) erro
 			Cid: viewPost.Post.Cid,
 		},
 	}
-	_, err := atpc.RepoCreateRecord(context.TODO(), atpc.C.Auth.Did, "app.bsky.feed.repost", true, &repost)
+	_, err := comatproto.RepoCreateRecord(context.TODO(), xrpcc, &comatproto.RepoCreateRecord_Input{
+		Collection: "app.bsky.feed.repost",
+		Did:        xrpcc.Auth.Did,
+		Record:     lexutil.LexiconTypeDecoder{repost},
+	})
 	return err
 }
 
-func pdsCreateReply(atpc *api.ATProto, viewPost *appbsky.FeedFeedViewPost) error {
+func pdsCreateReply(xrpcc *xrpc.Client, viewPost *appbsky.FeedFeedViewPost) error {
 	text := gofakeit.Sentence(10)
 	if len(text) > 200 {
 		text = text[0:200]
@@ -466,19 +485,19 @@ func pdsCreateReply(atpc *api.ATProto, viewPost *appbsky.FeedFeedViewPost) error
 			Cid: viewPost.Reply.Root.Cid,
 		}
 	}
-	replyPost := &appbsky.FeedPost{
-		CreatedAt: time.Now().Format(time.RFC3339),
-		Text:      text,
-		Reply: &appbsky.FeedPost_ReplyRef{
-			Parent: parent,
-			Root:   root,
-		},
+	reply := &appbsky.FeedPost_ReplyRef{
+		Parent: parent,
+		Root:   root,
 	}
-	_, err := atpc.RepoCreateRecord(context.TODO(), atpc.C.Auth.Did, "app.bsky.feed.post", true, &replyPost)
+	_, err := comatproto.RepoCreateRecord(context.TODO(), xrpcc, &comatproto.RepoCreateRecord_Input{
+		Collection: "app.bsky.feed.post",
+		Did:        xrpcc.Auth.Did,
+		Record:     lexutil.LexiconTypeDecoder{reply},
+	})
 	return err
 }
 
-func pdsGenFollowsAndMutes(atpc *api.ATProto, catalog *AccountCatalog, acc *AccountContext, maxFollows int, maxMutes int) error {
+func pdsGenFollowsAndMutes(xrpcc *xrpc.Client, catalog *AccountCatalog, acc *AccountContext, maxFollows int, maxMutes int) error {
 
 	// TODO: have a "shape" to likelihood of doing a follow
 	var tgt *AccountContext
@@ -491,7 +510,7 @@ func pdsGenFollowsAndMutes(atpc *api.ATProto, catalog *AccountCatalog, acc *Acco
 		if tgt.Auth.Did == acc.Auth.Did {
 			continue
 		}
-		if err := pdsCreateFollow(atpc, tgt); err != nil {
+		if err := pdsCreateFollow(xrpcc, tgt); err != nil {
 			return err
 		}
 	}
@@ -500,7 +519,7 @@ func pdsGenFollowsAndMutes(atpc *api.ATProto, catalog *AccountCatalog, acc *Acco
 		if tgt.Auth.Did == acc.Auth.Did {
 			continue
 		}
-		if err := pdsCreateFollow(atpc, tgt); err != nil {
+		if err := pdsCreateFollow(xrpcc, tgt); err != nil {
 			return err
 		}
 	}
@@ -514,7 +533,7 @@ func pdsGenFollowsAndMutes(atpc *api.ATProto, catalog *AccountCatalog, acc *Acco
 		if tgt.Auth.Did == acc.Auth.Did {
 			continue
 		}
-		if err := appbsky.GraphMute(context.TODO(), atpc.C, &appbsky.GraphMute_Input{User: tgt.Auth.Did}); err != nil {
+		if err := appbsky.GraphMute(context.TODO(), xrpcc, &appbsky.GraphMute_Input{User: tgt.Auth.Did}); err != nil {
 			return err
 		}
 	}
@@ -522,7 +541,7 @@ func pdsGenFollowsAndMutes(atpc *api.ATProto, catalog *AccountCatalog, acc *Acco
 	return nil
 }
 
-func genContent(cctx *cli.Context) error {
+func genPosts(cctx *cli.Context) error {
 	catalog, err := readAccountCatalog(cctx.String("catalog"))
 	if err != nil {
 		return err
@@ -532,6 +551,31 @@ func genContent(cctx *cli.Context) error {
 	maxPosts := cctx.Int("max-posts")
 	fracImage := cctx.Float64("frac-image")
 	fracMention := cctx.Float64("frac-mention")
+	httpClient := cliutil.NewHttpClient()
+
+	for _, acc := range append(catalog.Celebs, catalog.Regulars...) {
+		// create an ATP client for this specific user
+		xrpcc := &xrpc.Client{
+			Client: httpClient,
+			Host:   pdsHost,
+			Auth:   &acc.Auth,
+		}
+
+		// generate some more posts, similar to before (but fewer)
+		if err = pdsGenPosts(xrpcc, catalog, &acc, maxPosts, fracImage, fracMention); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func genInteractions(cctx *cli.Context) error {
+	catalog, err := readAccountCatalog(cctx.String("catalog"))
+	if err != nil {
+		return err
+	}
+
+	pdsHost := cctx.String("pds")
 	fracLike := cctx.Float64("frac-like")
 	fracRepost := cctx.Float64("frac-repost")
 	fracReply := cctx.Float64("frac-reply")
@@ -540,22 +584,15 @@ func genContent(cctx *cli.Context) error {
 	// TODO: profile: avatar, display name, description
 	for _, acc := range append(catalog.Celebs, catalog.Regulars...) {
 		// create an ATP client for this specific user
-		atpc := &api.ATProto{
-			C: &xrpc.Client{
-				Client: httpClient,
-				Host:   pdsHost,
-				Auth:   &acc.Auth,
-			},
+		xrpcc := &xrpc.Client{
+			Client: httpClient,
+			Host:   pdsHost,
+			Auth:   &acc.Auth,
 		}
 
-		// generate some more posts, similar to before (but fewer)
-		if err = pdsGenPosts(atpc, catalog, &acc, maxPosts, fracImage, fracMention); err != nil {
-			return err
-		}
-
-		t1 := measureIterations("all interactions", 1)
+		t1 := measureIterations("all interactions")
 		// fetch timeline (up to 100), and iterate over posts
-		resp, err := appbsky.FeedGetTimeline(context.TODO(), atpc.C, "", "", 100)
+		resp, err := appbsky.FeedGetTimeline(context.TODO(), xrpcc, "", "", 100)
 		if err != nil {
 			return err
 		}
@@ -567,17 +604,17 @@ func genContent(cctx *cli.Context) error {
 
 			// generate
 			if fracLike > 0.0 && rand.Float64() < fracLike {
-				if err := pdsCreateLike(atpc, post); err != nil {
+				if err := pdsCreateLike(xrpcc, post); err != nil {
 					return err
 				}
 			}
 			if fracRepost > 0.0 && rand.Float64() < fracRepost {
-				if err := pdsCreateRepost(atpc, post); err != nil {
+				if err := pdsCreateRepost(xrpcc, post); err != nil {
 					return err
 				}
 			}
 			if fracReply > 0.0 && rand.Float64() < fracReply {
-				if err := pdsCreateReply(atpc, post); err != nil {
+				if err := pdsCreateReply(xrpcc, post); err != nil {
 					return err
 				}
 			}
@@ -587,7 +624,7 @@ func genContent(cctx *cli.Context) error {
 	return nil
 }
 
-func benchBrowsing(cctx *cli.Context) error {
+func runBrowsing(cctx *cli.Context) error {
 	catalog, err := readAccountCatalog(cctx.String("catalog"))
 	if err != nil {
 		return err
@@ -598,16 +635,14 @@ func benchBrowsing(cctx *cli.Context) error {
 
 	for _, acc := range append(catalog.Celebs, catalog.Regulars...) {
 		// create an ATP client for this specific user
-		atpc := &api.ATProto{
-			C: &xrpc.Client{
-				Client: httpClient,
-				Host:   pdsHost,
-				Auth:   &acc.Auth,
-			},
+		xrpcc := &xrpc.Client{
+			Client: httpClient,
+			Host:   pdsHost,
+			Auth:   &acc.Auth,
 		}
 
 		// fetch notifications
-		resp, err := appbsky.NotificationList(context.TODO(), atpc.C, "", 50)
+		resp, err := appbsky.NotificationList(context.TODO(), xrpcc, "", 50)
 		if err != nil {
 			return err
 		}
@@ -619,18 +654,18 @@ func benchBrowsing(cctx *cli.Context) error {
 			case "repost":
 				fallthrough
 			case "follow":
-				_, err := appbsky.ActorGetProfile(context.TODO(), atpc.C, notif.Author.Did)
+				_, err := appbsky.ActorGetProfile(context.TODO(), xrpcc, notif.Author.Did)
 				if err != nil {
 					return err
 				}
-				_, err = appbsky.FeedGetAuthorFeed(context.TODO(), atpc.C, notif.Author.Did, "", 50)
+				_, err = appbsky.FeedGetAuthorFeed(context.TODO(), xrpcc, notif.Author.Did, "", 50)
 				if err != nil {
 					return err
 				}
 			case "mention":
 				fallthrough
 			case "reply":
-				_, err := appbsky.FeedGetPostThread(context.TODO(), atpc.C, 4, notif.Uri)
+				_, err := appbsky.FeedGetPostThread(context.TODO(), xrpcc, 4, notif.Uri)
 				if err != nil {
 					return err
 				}
@@ -640,7 +675,7 @@ func benchBrowsing(cctx *cli.Context) error {
 		t1()
 
 		// fetch timeline (up to 100), and iterate over posts
-		timelineResp, err := appbsky.FeedGetTimeline(context.TODO(), atpc.C, "", "", 100)
+		timelineResp, err := appbsky.FeedGetTimeline(context.TODO(), xrpcc, "", "", 100)
 		if err != nil {
 			return err
 		}
@@ -652,16 +687,16 @@ func benchBrowsing(cctx *cli.Context) error {
 			}
 			// TODO: should we do something different here?
 			if rand.Float64() < 0.25 {
-				_, err = appbsky.FeedGetPostThread(context.TODO(), atpc.C, 4, post.Post.Uri)
+				_, err = appbsky.FeedGetPostThread(context.TODO(), xrpcc, 4, post.Post.Uri)
 				if err != nil {
 					return err
 				}
 			} else if rand.Float64() < 0.25 {
-				_, err = appbsky.ActorGetProfile(context.TODO(), atpc.C, post.Post.Author.Did)
+				_, err = appbsky.ActorGetProfile(context.TODO(), xrpcc, post.Post.Author.Did)
 				if err != nil {
 					return err
 				}
-				_, err = appbsky.FeedGetAuthorFeed(context.TODO(), atpc.C, post.Post.Author.Did, "", 50)
+				_, err = appbsky.FeedGetAuthorFeed(context.TODO(), xrpcc, post.Post.Author.Did, "", 50)
 				if err != nil {
 					return err
 				}
@@ -670,7 +705,7 @@ func benchBrowsing(cctx *cli.Context) error {
 		t2()
 
 		// notification count for good measure
-		_, err = appbsky.NotificationGetCount(context.TODO(), atpc.C)
+		_, err = appbsky.NotificationGetCount(context.TODO(), xrpcc)
 	}
 	return nil
 }
