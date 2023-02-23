98de949f4aece3ee821d787dd84ef88f8f1f2116
whyrusleeping <why@ipfs.io>
2023-02-23 09:30:46 -0800

    add those missing files

---
+++ util/dbcid.go
@@ -0,0 +1,57 @@
+package util
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+
+	"github.com/ipfs/go-cid"
+)
+
+// NB: copied from my estuary code
+
+type DbCID struct {
+	CID cid.Cid
+}
+
+func (dbc *DbCID) Scan(v interface{}) error {
+	b, ok := v.([]byte)
+	if !ok {
+		return fmt.Errorf("dbcids must get bytes!")
+	}
+
+	if len(b) == 0 {
+		return nil
+	}
+
+	c, err := cid.Cast(b)
+	if err != nil {
+		return err
+	}
+
+	dbc.CID = c
+	return nil
+}
+
+func (dbc DbCID) Value() (driver.Value, error) {
+	return dbc.CID.Bytes(), nil
+}
+
+func (dbc DbCID) MarshalJSON() ([]byte, error) {
+	return json.Marshal(dbc.CID.String())
+}
+
+func (dbc *DbCID) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	c, err := cid.Decode(s)
+	if err != nil {
+		return err
+	}
+
+	dbc.CID = c
+	return nil
+}
