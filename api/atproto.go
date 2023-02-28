fdfa1fcefc7024493876d58f9aaf0142d127d294
bryan newbold <bnewbold@robocracy.org>
2023-02-27 17:47:36 -0800

    api: remove deprecated 'CreateAccount'
    Was only being used in 'cmd/stress'

--- api/atproto.go
+++ api/atproto.go
@@ -1,42 +1,9 @@
 package api
 
 import (
-	"context"
-
 	"github.com/bluesky-social/indigo/xrpc"
 )
 
 type ATProto struct {
 	C *xrpc.Client
-}
-
-const (
-	encJson = "application/json"
-)
-
-type CreateAccountResp struct {
-	AccessJwt      string `json:"accessJwt"`
-	RefreshJwt     string `json:"refreshJwt"`
-	Handle         string `json:"handle"`
-	Did            string `json:"did"`
-	DeclarationCid string `json:"declarationCid"`
-}
-
-func (atp *ATProto) CreateAccount(ctx context.Context, email, handle, password string, invite *string) (*CreateAccountResp, error) {
-	body := map[string]string{
-		"email":    email,
-		"handle":   handle,
-		"password": password,
-	}
-
-	if invite != nil {
-		body["inviteCode"] = *invite
-	}
-
-	var resp CreateAccountResp
-	if err := atp.C.Do(ctx, xrpc.Procedure, encJson, "com.atproto.account.create", nil, body, &resp); err != nil {
-		return nil, err
-	}
-
-	return &resp, nil
 }
