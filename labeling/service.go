0eb37071425b5d8679948038cb6013a02c4a1f8a
bryan newbold <bnewbold@robocracy.org>
2023-03-10 23:47:39 -0800

    cleanup labelmaker server code
    Refactoring things around a bit

--- labeling/service.go
+++ labeling/service.go
@@ -3,31 +3,27 @@ package labeling
 import (
 	"bytes"
 	"context"
-	"crypto/ecdsa"
 	"fmt"
-	"os"
 	"strings"
 	"time"
 
 	"github.com/bluesky-social/indigo/api"
-	bsky "github.com/bluesky-social/indigo/api/bsky"
+	appbsky "github.com/bluesky-social/indigo/api/bsky"
 	"github.com/bluesky-social/indigo/bgs"
 	"github.com/bluesky-social/indigo/carstore"
 	"github.com/bluesky-social/indigo/events"
 	"github.com/bluesky-social/indigo/indexer"
-	lexutil "github.com/bluesky-social/indigo/lex/util"
 	"github.com/bluesky-social/indigo/models"
 	"github.com/bluesky-social/indigo/pds"
 	"github.com/bluesky-social/indigo/repo"
 	"github.com/bluesky-social/indigo/repomgr"
 	util "github.com/bluesky-social/indigo/util"
+	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/ipfs/go-cid"
 	logging "github.com/ipfs/go-log"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/lestrrat-go/jwx/jwa"
-	jwk "github.com/lestrrat-go/jwx/jwk"
 	"github.com/whyrusleeping/go-did"
 	"gorm.io/gorm"
 )
@@ -52,7 +48,9 @@ type LabelmakerRepoConfig struct {
 	userId     util.Uid
 }
 
-func NewServer(db *gorm.DB, cs *carstore.CarStore, keyFile, repoDid, repoHandle, bgsUrl, plcUrl string) (*Server, error) {
+// In addition to configuring the service, will connect to upstream BGS and start processing events. Won't handle HTTP or WebSocket endpoints until RunAPI() is called.
+// 'useWss' is a flag to use SSL for outbound WebSocket connections
+func NewServer(db *gorm.DB, cs *carstore.CarStore, keyFile, repoDid, repoHandle, plcUrl string, useWss bool) (*Server, error) {
 
 	serkey, err := loadKey(keyFile)
 	if err != nil {
@@ -73,7 +71,7 @@ func NewServer(db *gorm.DB, cs *carstore.CarStore, keyFile, repoDid, repoHandle,
 		userId:     1,
 	}
 
-	var kl = KeywordLabeler{value: "rude", keywords: []string{"🍆", "sex", "ab", "before", "yours", "the"}}
+	var kl = KeywordLabeler{value: "meta", keywords: []string{"the", "bluesky", "atproto"}}
 
 	s := &Server{
 		db:      db,
@@ -85,7 +83,7 @@ func NewServer(db *gorm.DB, cs *carstore.CarStore, keyFile, repoDid, repoHandle,
 	}
 
 	// ensure that local labelmaker repo exists
-	// NOTE: doesn't need to have app.bsky profile and actor config, this is just expedient (reusing helper)
+	// NOTE: doesn't need to have app.bsky profile and actor config, this is just expediant (reusing an existing helper function)
 	ctx := context.Background()
 	head, _ := s.repoman.GetRepoRoot(ctx, s.user.userId)
 	if head == cid.Undef {
@@ -97,190 +95,152 @@ func NewServer(db *gorm.DB, cs *carstore.CarStore, keyFile, repoDid, repoHandle,
 		log.Infof("found labelmaker repo: %s", head)
 	}
 
-	// TODO(bnewbold): enforce ssl (last boolean argument here)
-	slurp := bgs.NewSlurper(db, s.handleBgsRepoEvent, false)
+	slurp := bgs.NewSlurper(db, s.handleBgsRepoEvent, useWss)
 	s.bgsSlurper = slurp
-
-	// subscribe our RepoEvent slurper to the BGS, to receive incoming records for labeler
-	useWebsocketSSL := false
-	log.Infof("subscribing to BGS: %s (SSL=%v)", bgsUrl, useWebsocketSSL)
-	s.bgsSlurper.SubscribeToPds(ctx, bgsUrl, useWebsocketSSL)
-
-	// NOTE: this is where outgoing RepoEvents could be generated
-	// should skip indexing (we are not a PDS) and just ship out repo event stream
-	/*
-		repoman.SetEventHandler(func(ctx context.Context, evt *repomgr.RepoEvent) {
-			if err := ix.HandleRepoEvent(ctx, evt); err != nil {
-				log.Errorw("handle repo event failed", "user", evt.User, "err", err)
-			}
-		})
-	*/
 
 	go levtman.Run()
 
 	return s, nil
 }
 
-func (s *Server) Shutdown(ctx context.Context) error {
-	return s.echo.Shutdown(ctx)
+func (s *Server) SubscribeBGS(ctx context.Context, bgsUrl string, useWss bool) {
+	// subscribe our RepoEvent slurper to the BGS, to receive incoming records for labeler
+	log.Infof("subscribing to BGS: %s (SSL=%v)", bgsUrl, useWss)
+	s.bgsSlurper.SubscribeToPds(ctx, bgsUrl, useWss)
 }
 
-// incoming repo events
+// efficiency predicate to quickly discard events we know won't want to even parse
+func (s *Server) wantAnyRecords(ctx context.Context, ra *events.RepoAppend) bool {
+
+	for _, op := range ra.Ops {
+		if op.Action != "create" && op.Action != "update" {
+			continue
+		}
+		nsid := strings.SplitN(op.Path, "/", 2)[0]
+		switch nsid {
+		case "app.bsky.feed.post":
+			return true
+		case "app.bsky.actor.profile":
+			return true
+		default:
+			continue
+		}
+	}
+	return false
+}
+
+func (s *Server) labelRecord(ctx context.Context, did, nsid, uri, cid string, rec cbg.CBORMarshaler) ([]string, error) {
+	log.Infof("labeling record: %v", uri)
+	var labelVals []string
+	switch nsid {
+	case "app.bsky.feed.post":
+		post, suc := rec.(*appbsky.FeedPost)
+		if !suc {
+			return []string{}, fmt.Errorf("record failed to deserialize from CBOR: %s", rec)
+		}
+		// run through all the keyword labelers on posts, saving any resulting labels
+		for _, labeler := range s.kwl {
+			for _, val := range labeler.labelPost(*post) {
+				labelVals = append(labelVals, val)
+			}
+		}
+	case "app.bsky.actor.profile":
+		profile, suc := rec.(*appbsky.ActorProfile)
+		if !suc {
+			return []string{}, fmt.Errorf("record failed to deserialize from CBOR: %s", rec)
+		}
+		// run through all the keyword labelers on posts, saving any resulting labels
+		for _, labeler := range s.kwl {
+			for _, val := range labeler.labelActorProfile(*profile) {
+				labelVals = append(labelVals, val)
+			}
+		}
+	}
+	return labelVals, nil
+}
+
+// Process incoming repo events coming from BGS, which includes new and updated
+// records from any PDS. This function extracts records, handes them to the
+// labeling routine, and then persists and broadcasts any resulting labels
 func (s *Server) handleBgsRepoEvent(ctx context.Context, pds *models.PDS, evt *events.RepoStreamEvent) error {
+
+	if evt.Append == nil {
+		// TODO(bnewbold): is this really invalid? do we need to handle Info and Error events here?
+		return fmt.Errorf("invalid repo append event")
+	}
+
+	// quick check if we can skip processing the CAR slice entirely
+	if !s.wantAnyRecords(ctx, evt.Append) {
+		return nil
+	}
+
+	// use an in-memory blockstore with repo wrapper to parse CAR slice
+	sliceRepo, err := repo.ReadRepoFromCar(ctx, bytes.NewReader(evt.Append.Blocks))
+	if err != nil {
+		log.Warnw("failed to parse CAR slice", "repoErr", err)
+		return err
+	}
+
 	now := time.Now().Format(util.ISO8601)
-	switch {
-	case evt.Append != nil:
-		// this is where we take incoming RepoEvents and label them
-		// use an in-memory blockstore with repo wrapper to parse CAR
-		// NOTE: could refactor to parse ops first, so we don't bother parsing the CAR if there are no posts/profiles to process (a common case, for likes/follows/reposts/etc)
-		sliceRepo, err := repo.ReadRepoFromCar(ctx, bytes.NewReader(evt.Append.Blocks))
+	labels := []events.Label{}
+
+	for _, op := range evt.Append.Ops {
+		uri := "at://" + evt.Append.Repo + "/" + op.Path
+		nsid := strings.SplitN(op.Path, "/", 2)[0]
+
+		if !(op.Action == "create" || op.Action == "update") {
+			continue
+		}
+
+		cid, rec, err := sliceRepo.GetRecord(ctx, op.Path)
 		if err != nil {
-			log.Warnw("failed to parse CAR slice", "repoErr", err)
+			return fmt.Errorf("record not in CAR slice: %s", uri)
+		}
+		cidStr := cid.String()
+		labelVals, err := s.labelRecord(ctx, s.user.did, nsid, uri, cidStr, rec)
+		if err != nil {
 			return err
 		}
-		var labels []events.Label = []events.Label{}
-		for _, op := range evt.Append.Ops {
-			uri := "at://" + evt.Append.Repo + "/" + op.Path
-			nsid := strings.SplitN(op.Path, "/", 2)[0]
-			// filter to creation/update of ony post/profile records
-			// TODO(bnewbold): how do I 'switch' on a tuple here in golang, instead of nested switch?
-			switch op.Action {
-			case "create", "update":
-				log.Infof("labeling record: %v", uri)
-				switch nsid {
-				case "app.bsky.feed.post":
-					cid, rec, err := sliceRepo.GetRecord(ctx, op.Path)
-					if err != nil {
-						return fmt.Errorf("record not in CAR slice: %s", uri)
-					}
-					cidStr := cid.String()
-					post, suc := rec.(*bsky.FeedPost)
-					if !suc {
-						return fmt.Errorf("record failed to deserialize from CBOR: %s", rec)
-					}
-					// run through all the keyword labelers on posts, saving any resulting labels
-					for _, labeler := range s.kwl {
-						for _, val := range labeler.labelPost(*post) {
-							labels = append(labels, events.Label{
-								SourceDid:  s.user.did,
-								SubjectUri: uri,
-								SubjectCid: &cidStr,
-								Value:      val,
-								Timestamp:  now,
-							})
-						}
-					}
-				case "app.bsky.actor.profile":
-					// NOTE: copypasta from post above, could refactor to not duplicate
-					cid, rec, err := sliceRepo.GetRecord(ctx, op.Path)
-					if err != nil {
-						return fmt.Errorf("record not in CAR slice: %s", uri)
-					}
-					cidStr := cid.String()
-					profile, suc := rec.(*bsky.ActorProfile)
-					if !suc {
-						return fmt.Errorf("record failed to deserialize from CBOR: %s", rec)
-					}
-					// run through all the keyword labelers on profiles, saving any resulting labels
-					for _, labeler := range s.kwl {
-						for _, val := range labeler.labelActorProfile(*profile) {
-							labels = append(labels, events.Label{
-								SourceDid:  s.user.did,
-								SubjectUri: uri,
-								SubjectCid: &cidStr,
-								Value:      val,
-								Timestamp:  now,
-							})
-						}
-					}
-				default:
-					continue
-				}
-			default:
-				continue
-			}
+		for _, val := range labelVals {
+			labels = append(labels, events.Label{
+				SourceDid:  s.user.did,
+				SubjectUri: uri,
+				SubjectCid: &cidStr,
+				Value:      val,
+				Timestamp:  now,
+			})
 		}
+	}
 
-		// if any labels generated, persist them to repo...
-		for i, l := range labels {
-			path, _, err := s.repoman.CreateRecord(ctx, s.user.userId, "com.atproto.label.label", &l)
-			if err != nil {
-				return fmt.Errorf("failed to persist label in local repo: %w", err)
-			}
-			labeluri := "at://" + s.user.did + "/" + path
-			labels[i].LabelUri = &labeluri
-			log.Infof("persisted label: %s", labeluri)
+	// if any labels generated, persist them to repo...
+	for i, l := range labels {
+		path, _, err := s.repoman.CreateRecord(ctx, s.user.userId, "com.atproto.label.label", &l)
+		if err != nil {
+			return fmt.Errorf("failed to persist label in local repo: %w", err)
 		}
+		labeluri := "at://" + s.user.did + "/" + path
+		labels[i].LabelUri = &labeluri
+		log.Infof("persisted label: %s", labeluri)
+	}
 
-		// ... then re-publish as LabelStreamEvent
-		log.Infof("%s", labels)
-		if len(labels) > 0 {
-			lev := events.LabelStreamEvent{
-				// XXX(bnewbold): what should sequence number be? do I need to maintain that?
-				Batch: &events.LabelBatch{
-					Seq:    0,
-					Labels: labels,
-				},
-			}
-			err = s.levents.AddEvent(&lev)
-			if err != nil {
-				return fmt.Errorf("failed to publish LabelStreamEvent: %w", err)
-			}
+	// ... then re-publish as LabelStreamEvent
+	log.Infof("%s", labels)
+	if len(labels) > 0 {
+		lev := events.LabelStreamEvent{
+			Batch: &events.LabelBatch{
+				// NOTE(bnewbold): seems like other code handles Seq field automatically
+				Labels: labels,
+			},
 		}
-		// TODO: update state that we successfully processed the repo event (aka, persist "last" seq in database, or something like that)
-		return nil
-	default:
-		return fmt.Errorf("invalid fed event")
+		err = s.levents.AddEvent(&lev)
+		if err != nil {
+			return fmt.Errorf("failed to publish LabelStreamEvent: %w", err)
+		}
 	}
-}
-
-func (s *Server) readRecordFunc(ctx context.Context, user util.Uid, c cid.Cid) (lexutil.CBOR, error) {
-	bs, err := s.cs.ReadOnlySession(user)
-	if err != nil {
-		return nil, err
-	}
-
-	blk, err := bs.Get(ctx, c)
-	if err != nil {
-		return nil, err
-	}
-
-	return lexutil.CborDecodeValue(blk.RawData())
-}
-
-func loadKey(kfile string) (*did.PrivKey, error) {
-	kb, err := os.ReadFile(kfile)
-	if err != nil {
-		return nil, err
-	}
-
-	sk, err := jwk.ParseKey(kb)
-	if err != nil {
-		return nil, err
-	}
-
-	var spk ecdsa.PrivateKey
-	if err := sk.Raw(&spk); err != nil {
-		return nil, err
-	}
-	curve, ok := sk.Get("crv")
-	if !ok {
-		return nil, fmt.Errorf("need a curve set")
-	}
-
-	var out string
-	kts := string(curve.(jwa.EllipticCurveAlgorithm))
-	switch kts {
-	case "P-256":
-		out = did.KeyTypeP256
-	default:
-		return nil, fmt.Errorf("unrecognized key type: %s", kts)
-	}
-
-	return &did.PrivKey{
-		Raw:  &spk,
-		Type: out,
-	}, nil
+	// TODO(bnewbold): persist state that we successfully processed the repo event (aka,
+	// persist "last" seq in database, or something like that). also above, at
+	// the short-circuit
+	return nil
 }
 
 func (s *Server) RunAPI(listen string) error {
@@ -288,16 +248,21 @@ func (s *Server) RunAPI(listen string) error {
 	s.echo = e
 	e.HideBanner = true
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: "method=${method}, uri=${uri}, status=${status} latency=${latency_human}\n",
+		Format: "method=${method} uri=${uri} status=${status} latency=${latency_human}\n",
 	}))
 
 	e.HTTPErrorHandler = func(err error, ctx echo.Context) {
-		fmt.Printf("HANDLER ERROR: (%s) %s\n", ctx.Path(), err)
+		fmt.Printf("Error at path=%s: %v\n", ctx.Path(), err)
 		ctx.Response().WriteHeader(500)
 	}
 
 	s.RegisterHandlersComAtproto(e)
-	e.GET("/events/v0/labels", s.EventsLabelsWebsocket)
+	// TODO(bnewbold): this is a speculative endpoint name
+	e.GET("/xrpc/com.atproto.label.subscribeAllLabels", s.EventsLabelsWebsocket)
 
 	return e.Start(listen)
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.echo.Shutdown(ctx)
 }
