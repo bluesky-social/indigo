a29ef3b2e4c3b130c0c4f6573e98a7e2a9a35b14
bryan newbold <bnewbold@robocracy.org>
2023-03-12 23:28:30 -0700

    labels: de-dupe list of labels

---
+++ labeling/util_test.go
@@ -0,0 +1,27 @@
+package labeling
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestDedupeStrings(t *testing.T) {
+
+	testCases := []struct {
+		in  []string
+		out []string
+	}{
+		{in: []string{"a", "b", "c"}, out: []string{"a", "b", "c"}},
+		{in: []string{"a", "a", "a"}, out: []string{"a"}},
+		{in: []string{"a", "b", "a"}, out: []string{"a", "b"}},
+	}
+
+	for _, c := range testCases {
+		res := dedupeStrings(c.in)
+		if !reflect.DeepEqual(res, c.out) {
+			t.Log(fmt.Sprintf("strings expected:%s got:%s", c.out, res))
+			t.Fail()
+		}
+	}
+}
