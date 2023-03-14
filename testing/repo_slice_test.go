98666722e497b83734b0db5e2eb33480906a6e41
bryan newbold <bnewbold@robocracy.org>
2023-03-13 20:20:04 -0700

    regression test for empty util.Blob in CBOR-decoded post image embed

---
+++ testing/repo_slice_test.go
@@ -0,0 +1,59 @@
+package testing
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"fmt"
+	"testing"
+
+	"github.com/bluesky-social/indigo/repo"
+	appbsky "github.com/bluesky-social/indigo/api/bsky"
+)
+
+// ipfs dag import testing/repo_slice.car
+// ipfs dag get bafyreiapesxwibnujg44xphqq23ekkozgcmnenj2onnx4gkgy4uipziyc4 | jq .
+// ipfs dag get bafyreiapesxwibnujg44xphqq23ekkozgcmnenj2onnx4gkgy4uipziyc4 --output-codec=dag-cbor > testing/repo_record.cbor
+
+func TestRepoSliceParse(t *testing.T) {
+	ctx := context.TODO()
+	fi, err := os.Open("repo_slice.car")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sliceRepo, err := repo.ReadRepoFromCar(ctx, fi)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, rec, err := sliceRepo.GetRecord(ctx, "app.bsky.feed.post/3jquh3emtzo2o")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	post, suc := rec.(*appbsky.FeedPost)
+	if !suc {
+		t.Fatal("failed to deserialize post")
+	}
+	postJson, err := json.Marshal(post)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Println(string(postJson))
+
+	img := post.Embed.EmbedImages.Images[0]
+	if img.Alt != "Sausage sandwich italian style" {
+		t.Fatal("didn't get expected Alt text")
+	}
+
+	if img.Image.Cid == "" {
+		t.Fatal("got nil Cid on image")
+	}
+	if img.Image.Cid != "bafkreiblkobl6arfg3j7eft3akdhn2hmr2qmzfkefcgu4agnswvssg4a6a" {
+		t.Fatal("didn't get expected image blob Cid")
+	}
+	if img.Image.MimeType != "image/jpeg" {
+		t.Fatal("didn't get expected image blob mimetype")
+	}
+}
