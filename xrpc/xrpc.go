86d761672d95f8d9b4ec16aa7579682511b0b449
whyrusleeping <why@ipfs.io>
2023-03-22 13:51:05 -0700

    error out if we dont read the entire response body

--- xrpc/xrpc.go
+++ xrpc/xrpc.go
@@ -117,7 +117,10 @@ func (c *Client) Do(ctx context.Context, kind XRPCRequestType, inpenc string, me
 
 	if out != nil {
 		if buf, ok := out.(*bytes.Buffer); ok {
-			io.Copy(buf, resp.Body)
+			_, err := io.CopyN(buf, resp.Body, resp.ContentLength)
+			if err != nil {
+				return fmt.Errorf("reading response body: %w", err)
+			}
 		} else {
 			if err := json.NewDecoder(resp.Body).Decode(out); err != nil {
 				return fmt.Errorf("decoding xrpc response: %w", err)
