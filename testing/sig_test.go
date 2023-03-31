a9c0519bc094a5609bd94cc7e1cd575802f5f49d
bryan newbold <bnewbold@robocracy.org>
2023-03-30 18:40:10 -0700

    additional signature interop test

--- testing/sig_test.go
+++ testing/sig_test.go
@@ -2,15 +2,19 @@ package testing
 
 import (
 	"context"
+	"crypto/elliptic"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/bluesky-social/indigo/repo"
 
 	"github.com/ipfs/go-datastore"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	"github.com/multiformats/go-multibase"
 	"github.com/stretchr/testify/assert"
 	"github.com/whyrusleeping/go-did"
 )
@@ -125,4 +129,88 @@ func TestVerificationP256(t *testing.T) {
 	}
 
 	assert.NoError(key.Verify(msgBytes, sigBytes))
+}
+
+func parseKeyFromMultibase(t *testing.T, s, keyType string) did.PubKey {
+	_, data, err := multibase.Decode(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return did.PubKey{
+		Type: keyType,
+		Raw:  data,
+	}
+}
+
+func parseDidKey(t *testing.T, s string) did.PubKey {
+	parts := strings.SplitN(s, ":", 3)
+	_, data, err := multibase.Decode(parts[2])
+	if err != nil {
+		t.Fatal(err)
+	}
+	switch {
+	case data[0] == 0x80 && data[1] == 0x24:
+		// p256
+		// need to "uncompress"
+		curve := elliptic.P256()
+		x, y := elliptic.UnmarshalCompressed(curve, data[2:])
+		return did.PubKey{
+			Type: "EcdsaSecp256r1VerificationKey2019",
+			Raw:  elliptic.Marshal(curve, x, y),
+		}
+	case data[0] == 0xE7 && data[1] == 0x01:
+		// k256
+		return did.PubKey{
+			Type: "EcdsaSecp256k1VerificationKey2019",
+			Raw:  data[2:],
+		}
+	default:
+		t.Fatal(fmt.Errorf("unhandled did:key type: %d %d", data[0], data[1]))
+	}
+	panic("unreachable")
+}
+
+func TestInteropSignatures(t *testing.T) {
+	assert := assert.New(t)
+
+	testTable := []struct {
+		description  string
+		keyType      string
+		docType      string
+		didKey       string
+		multibaseKey string
+		msgBase64    string
+		sigBase64    string
+	}{
+		{
+			description:  "p256/secp256r1",
+			keyType:      "ES256",
+			docType:      "EcdsaSecp256r1VerificationKey2019",
+			didKey:       "did:key:zDnaebKg3xzqP4DnpjCjaUpPkCVridQJufGQqwYWi623VPxcN",
+			multibaseKey: "zQi6N7wGYuk7GhUfU17jkaitpAPzeGyCufAyz9i7jT4T2XSC1KjbAA2QridVGidGbfXdV9X9fWotHmmkVwbeXRujR",
+			msgBase64:    "oWVoZWxsb2V3b3JsZA",
+			sigBase64:    "/SbeL+Tx6TXUZGC7uHHkI9b+1ARoLcqTzkUYJmPBqlCVAvKkxkPowfqKwnJWpZcuF4g3MT7eVgdBROHgdUplIQ",
+		},
+		{
+			description:  "k256/secp256k1",
+			keyType:      "ES256K",
+			docType:      "EcdsaSecp256k1VerificationKey2019",
+			didKey:       "did:key:zQ3shueETAVp1HCdE7pcrUD1wEHNhfaQiDJmWeJgCxZRmDJuy",
+			multibaseKey: "zRvvEWNhRDxCYpkWWKzvrCP5Sca54yhgXjLcSgfJsWW9Jk8Wq79d5h2PTW3BQDmNQv4prqGjRkXiEDwnfpq2tTsmp",
+			msgBase64:    "oWVoZWxsb2V3b3JsZA",
+			sigBase64:    "FbKI9u/VoY9SFHvtsuYsZULkt3QiNRrPUP3N4bX0xTxfWIUccyoNJ7egTITlaD1xBhXAvDdBnbyC6aOZ0jyjEA",
+		},
+	}
+
+	for _, row := range testTable {
+		pkDid := parseDidKey(t, row.didKey)
+		pkMultibase := parseKeyFromMultibase(t, row.multibaseKey, row.docType)
+		msgBytes, err := base64.RawStdEncoding.DecodeString(row.msgBase64)
+		assert.NoError(err)
+		sigBytes, err := base64.RawStdEncoding.DecodeString(row.sigBase64)
+		assert.NoError(err)
+
+		assert.NoError(pkDid.Verify(msgBytes, sigBytes), "keyType=%v format=%v", row.description, "did:key")
+		assert.NoError(pkMultibase.Verify(msgBytes, sigBytes), "keyType=%v format=%v", row.description, "multibase")
+	}
 }
