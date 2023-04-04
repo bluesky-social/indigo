c816d8a79e243dd9dc11c9c18c84b438beb674c7
bryan newbold <bnewbold@robocracy.org>
2023-04-04 16:25:03 -0700

    lexgen and cborgen updated label lexicons

--- api/label/labelsubscribeLabels.go
+++ api/label/labelsubscribeLabels.go
@@ -4,3 +4,15 @@ package label
 
 func init() {
 }
+
+type SubscribeLabels_Info struct {
+	LexiconTypeID string  `json:"$type,omitempty" cborgen:"$type,omitempty"`
+	Message       *string `json:"message,omitempty" cborgen:"message,omitempty"`
+	Name          string  `json:"name" cborgen:"name"`
+}
+
+type SubscribeLabels_Labels struct {
+	LexiconTypeID string   `json:"$type,omitempty" cborgen:"$type,omitempty"`
+	Labels        []*Label `json:"labels" cborgen:"labels"`
+	Seq           int64    `json:"seq" cborgen:"seq"`
+}
