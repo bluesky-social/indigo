466d689cb7078595db833cd5b6ef1481f4004d46
whyrusleeping <why@ipfs.io>
2023-04-25 16:43:46 -0700

    add simple auth for new admin routes and basic auth token management

---
+++ bgs/admin.go
@@ -0,0 +1,11 @@
+package bgs
+
+import "github.com/labstack/echo/v4"
+
+func (bgs *BGS) handleAdminDeleteRecord(e echo.Context) error {
+	panic("TODO")
+}
+
+func (bgs *BGS) handleAdminBlockRepoStream(e echo.Context) error {
+	panic("TODO")
+}
