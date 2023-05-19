fb794ac8d49ae69d6c7789b3620e262b9752c54a
whyrusleeping <why@ipfs.io>
2023-05-18 21:21:22 -0700

    add sorting and pagination and 'and' to search

--- search/handlers.go
+++ search/handlers.go
@@ -2,6 +2,8 @@ package search
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 	"strings"
 
 	api "github.com/bluesky-social/indigo/api"
@@ -35,7 +37,33 @@ func (s *Server) handleSearchRequestPosts(e echo.Context) error {
 		})
 	}
 
-	out, err := s.SearchPosts(ctx, q)
+	offset := 0
+	if q := strings.TrimSpace(e.QueryParam("offset")); q != "" {
+		v, err := strconv.Atoi(q)
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    400,
+				Message: fmt.Sprintf("invalid value for 'offset': %s", err),
+			}
+		}
+
+		offset = v
+	}
+
+	count := 30
+	if q := strings.TrimSpace(e.QueryParam("count")); q != "" {
+		v, err := strconv.Atoi(q)
+		if err != nil {
+			return &echo.HTTPError{
+				Code:    400,
+				Message: fmt.Sprintf("invalid value for 'count': %s", err),
+			}
+		}
+
+		count = v
+	}
+
+	out, err := s.SearchPosts(ctx, q, offset, count)
 	if err != nil {
 		return err
 	}
