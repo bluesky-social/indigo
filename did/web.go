c2d2e4e3e3a52acb22839e6e62b422e2d5d97d9d
whyrusleeping <why@ipfs.io>
2023-05-29 10:57:09 -0700

    more robust hardening checks

--- did/web.go
+++ did/web.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"unicode"
 
 	"github.com/whyrusleeping/go-did"
 )
@@ -21,8 +22,8 @@ func (wr *WebResolver) GetDocument(ctx context.Context, didstr string) (*Documen
 	}
 
 	val := pdid.Value()
-	if strings.Contains(val, ":") {
-		return nil, fmt.Errorf("did:web resolver does not handle ports or documents at sub-paths")
+	if err := checkValidDidWeb(val); err != nil {
+		return nil, err
 	}
 
 	resp, err := http.Get("https://" + val + "/.well-known/did.json")
@@ -41,4 +42,41 @@ func (wr *WebResolver) GetDocument(ctx context.Context, didstr string) (*Documen
 	}
 
 	return &out, nil
+}
+
+var disallowedTlds = map[string]bool{
+	"example":  true,
+	"invalid":  true,
+	"local":    true,
+	"arpa":     true,
+	"onion":    true,
+	"internal": true,
+}
+
+func checkValidDidWeb(val string) error {
+	// no ports or ipv6
+	if strings.Contains(val, ":") {
+		return fmt.Errorf("did:web resolver does not handle ports or documents at sub-paths")
+	}
+	// no trailing '.'
+	if strings.HasSuffix(val, ".") {
+		return fmt.Errorf("cannot have trailing period in hostname")
+	}
+
+	parts := strings.Split(val, ".")
+	if len(parts) == 1 {
+		return fmt.Errorf("no bare hostnames (must have subdomain)")
+	}
+
+	tld := parts[len(parts)-1]
+	if disallowedTlds[tld] {
+		return fmt.Errorf("domain cannot use any disallowed TLD")
+	}
+
+	// disallow tlds that start with numbers
+	if unicode.IsNumber(rune(tld[0])) {
+		return fmt.Errorf("TLD cannot start with a number")
+	}
+
+	return nil
 }
