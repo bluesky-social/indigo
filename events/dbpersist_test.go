a6619883a52fe225a5c0b47cdf91f1c1f914f90c
whyrusleeping <why@ipfs.io>
2023-06-29 14:07:50 -0700

    parallelize dbpersister event playback hydration
    Co-authored-by: Jaz <ericvolp12@gmail.com>

--- events/dbpersist_test.go
+++ events/dbpersist_test.go
@@ -146,6 +146,128 @@ func BenchmarkDBPersist(b *testing.B) {
 	}
 }
 
+func BenchmarkPlayback(b *testing.B) {
+	ctx := context.Background()
+
+	n := b.N
+
+	db, _, cs, tempPath, err := setupDBs(b)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	db.AutoMigrate(&pds.User{})
+	db.AutoMigrate(&pds.Peering{})
+	db.AutoMigrate(&models.ActorInfo{})
+
+	db.Create(&models.ActorInfo{
+		Uid: 1,
+		Did: "did:example:123",
+	})
+
+	mgr := repomgr.NewRepoManager(db, cs, &util.FakeKeyManager{})
+
+	err = mgr.InitNewActor(ctx, 1, "alice", "did:example:123", "Alice", "", "")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	_, cid, err := mgr.CreateRecord(ctx, 1, "app.bsky.feed.post", &bsky.FeedPost{
+		Text:      "hello world",
+		CreatedAt: time.Now().Format(util.ISO8601),
+	})
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	defer os.RemoveAll(tempPath)
+
+	// Initialize a DBPersister
+	dbp, err := events.NewDbPersistence(db, cs, nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	// Create a bunch of events
+	evtman := events.NewEventManager(dbp)
+
+	userRepoHead, err := mgr.GetRepoRoot(ctx, 1)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	inEvts := make([]*events.XRPCStreamEvent, n)
+	for i := 0; i < n; i++ {
+		cidLink := lexutil.LexLink(cid)
+		headLink := lexutil.LexLink(userRepoHead)
+		inEvts[i] = &events.XRPCStreamEvent{
+			RepoCommit: &atproto.SyncSubscribeRepos_Commit{
+				Repo:   "did:example:123",
+				Commit: headLink,
+				Ops: []*atproto.SyncSubscribeRepos_RepoOp{
+					{
+						Action: "add",
+						Cid:    &cidLink,
+						Path:   "path1",
+					},
+				},
+				Time: time.Now().Format(util.ISO8601),
+			},
+		}
+	}
+
+	numRoutines := 5
+	wg := sync.WaitGroup{}
+
+	errChan := make(chan error, numRoutines)
+
+	// Add events in parallel
+	for i := 0; i < numRoutines; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < n; i++ {
+				err = evtman.AddEvent(ctx, inEvts[i])
+				if err != nil {
+					errChan <- err
+				}
+			}
+		}()
+	}
+
+	wg.Wait()
+	close(errChan)
+
+	// Check for errors
+	for err := range errChan {
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	outEvtCount := 0
+	expectedEvtCount := n * numRoutines
+
+	// Flush manually
+	err = dbp.FlushBatch(ctx)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	dbp.Playback(ctx, 0, func(evt *events.XRPCStreamEvent) error {
+		outEvtCount++
+		return nil
+	})
+
+	b.StopTimer()
+
+	if outEvtCount != expectedEvtCount {
+		b.Fatalf("expected %d events, got %d", expectedEvtCount, outEvtCount)
+	}
+}
+
 func setupDBs(t testing.TB) (*gorm.DB, *gorm.DB, *carstore.CarStore, string, error) {
 	dir, err := os.MkdirTemp("", "integtest")
 	if err != nil {
@@ -164,9 +286,14 @@ func setupDBs(t testing.TB) (*gorm.DB, *gorm.DB, *carstore.CarStore, string, err
 
 	tx.Commit()
 
-	cardb, err := gorm.Open(sqlite.Open(filepath.Join(dir, "car.sqlite")))
+	cardb, err := gorm.Open(sqlite.Open(filepath.Join(dir, "car.sqlite?cache=shared&mode=rwc")))
 	if err != nil {
 		return nil, nil, nil, "", err
+	}
+
+	tx = cardb.Exec("PRAGMA journal_mode=WAL;")
+	if tx.Error != nil {
+		return nil, nil, nil, "", tx.Error
 	}
 
 	cspath := filepath.Join(dir, "carstore")
