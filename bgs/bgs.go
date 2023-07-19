2dac0a12e52584fca7d8ef82e73d295ea60d1fc3
Jaz Volpert <ericvolp12@gmail.com>
2023-07-19 22:59:00 +0000

    Remove weird newline

--- bgs/bgs.go
+++ bgs/bgs.go
@@ -866,7 +866,6 @@ func (bgs *BGS) TakeDownRepo(ctx context.Context, did string) error {
 func (bgs *BGS) ReverseTakedown(ctx context.Context, did string) error {
 	u, err := bgs.lookupUserByDid(ctx, did)
 	if err != nil {
-
 		return err
 	}
 
