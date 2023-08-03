5f22bc0d0d755f5c5c667bcefd47f09ca21f877b
Jerry Chen <jerry@apache.org>
2023-08-03 10:29:06 -0500

    query_parser_test cleanup

--- search/query_parser_test.go
+++ search/query_parser_test.go
@@ -7,88 +7,99 @@ import (
 )
 
 func TestParamsToSearchQuery(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("foo", "0", "10")
-	assert.Equal(t, "foo", searchQuery.QueryString)
+	assert.Equal("foo", searchQuery.QueryString)
 }
 
 func TestParamsToSearchQueryTrimsSpaces(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("   foo     ", "0", "10")
-	assert.Equal(t, "foo", searchQuery.QueryString)
+	assert.Equal("foo", searchQuery.QueryString)
 }
 
 func TestParamsExtractFromUserHandle(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("from:bob.tld foo", "0", "10")
-	assert.Equal(t, "foo", searchQuery.QueryString)
-	assert.Equal(t, &DidHandle{
+	assert.Equal("foo", searchQuery.QueryString)
+	assert.Equal(&DidHandle{
 		Handle: "bob.tld",
 		DID:    "",
 	}, searchQuery.FromUser)
 }
 
 func TestParamsExtractFromOperatorCaseInsensitive(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("FROM:bob.tld foo", "0", "10")
-	assert.Equal(t, "foo", searchQuery.QueryString)
-	assert.Equal(t, &DidHandle{
+	assert.Equal("foo", searchQuery.QueryString)
+	assert.Equal(&DidHandle{
 		Handle: "bob.tld",
 		DID:    "",
 	}, searchQuery.FromUser)
 
 	searchQuery, _ = paramsToSearchQuery("froM:bob.tld foo", "0", "10")
-	assert.Equal(t, "foo", searchQuery.QueryString)
-	assert.Equal(t, &DidHandle{
+	assert.Equal("foo", searchQuery.QueryString)
+	assert.Equal(&DidHandle{
 		Handle: "bob.tld",
 		DID:    "",
 	}, searchQuery.FromUser)
 }
 
 func TestParamsExtractFromUserDid(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("from:did:plc:testing foo", "0", "10")
-	assert.Equal(t, "foo", searchQuery.QueryString)
-	assert.Equal(t, &DidHandle{
+	assert.Equal("foo", searchQuery.QueryString)
+	assert.Equal(&DidHandle{
 		Handle: "",
 		DID:    "did:plc:testing",
 	}, searchQuery.FromUser)
 }
 
 func TestParamsMultipleFromOperatorsPassThroughAsQuery(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("from:bob.tld from:alice.tld", "0", "10")
-	assert.Equal(t, "from:bob.tld from:alice.tld", searchQuery.QueryString)
-	assert.Nil(t, searchQuery.FromUser)
+	assert.Equal("from:bob.tld from:alice.tld", searchQuery.QueryString)
+	assert.Nil(searchQuery.FromUser)
 }
 
 func TestParamsExtractAllowEmptyOffsetCount(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("foobar", "", "")
-	assert.Equal(t, DefaultCount, searchQuery.Count)
-	assert.Equal(t, DefaultOffset, searchQuery.Offset)
+	assert.Equal(DefaultCount, searchQuery.Count)
+	assert.Equal(DefaultOffset, searchQuery.Offset)
 }
 
 func TestParamsExtractEnforceMinOffset(t *testing.T) {
+	assert := assert.New(t)
 	searchQuery, _ := paramsToSearchQuery("foobar", "-100", "")
-	assert.Equal(t, MinOffset, searchQuery.Offset)
+	assert.Equal(MinOffset, searchQuery.Offset)
 
 	searchQuery, _ = paramsToSearchQuery("foobar", "-1", "")
-	assert.Equal(t, MinOffset, searchQuery.Offset)
+	assert.Equal(MinOffset, searchQuery.Offset)
 }
 
 func TestParamsExtractResetsDefaultCountWhenExceedsMaxCount(t *testing.T) {
+	assert := assert.New(t)
 	countParamValue := 101
 	countParam := strconv.Itoa(countParamValue)
 	searchQuery, _ := paramsToSearchQuery("foobar", "", countParam)
-	assert.True(t, countParamValue > MaxCount)
-	assert.Equal(t, DefaultCount, searchQuery.Count)
+	assert.True(countParamValue > MaxCount)
+	assert.Equal(DefaultCount, searchQuery.Count)
 }
 
 func TestParamsExtractAllowsMaxCount(t *testing.T) {
+	assert := assert.New(t)
 	countParamValue := MaxCount
 	countParam := strconv.Itoa(countParamValue)
 	searchQuery, _ := paramsToSearchQuery("foobar", "", countParam)
-	assert.Equal(t, MaxCount, searchQuery.Count)
+	assert.Equal(MaxCount, searchQuery.Count)
 }
 
 func TestParamsExtractReturnsErrorForBunkCountAndOffset(t *testing.T) {
+	assert := assert.New(t)
 	_, err := paramsToSearchQuery("foobar", "abc", "")
-	assert.Error(t, err)
+	assert.Error(err)
 
 	_, err = paramsToSearchQuery("foobar", "", "abc")
-	assert.Error(t, err)
+	assert.Error(err)
 }
