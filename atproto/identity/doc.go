7d796bcdc3980ca7b1da8630757b958482b3cc15
bryan newbold <bnewbold@robocracy.org>
2023-08-26 13:42:33 -0700

    identity: bit of progress

--- atproto/identity/doc.go
+++ atproto/identity/doc.go
@@ -1,5 +1,6 @@
-/* Package identity provides types and routines for resolving handles and DIDs from the network
+/*
+	Package identity provides types and routines for resolving handles and DIDs from the network
 
-Much of the code in this SDK is based on existing code in indigo:api/extra.go
+Much of the implementation of this SDK is based on existing code in indigo:api/extra.go
 */
 package identity
