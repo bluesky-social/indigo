62ce57493edb2c9b39f89a8c130aec16881b2976
bryan newbold <bnewbold@robocracy.org>
2023-08-26 20:27:04 -0700

    identity: progress

--- atproto/identity/cmd/atp-id/main.go
+++ atproto/identity/cmd/atp-id/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
-	//"github.com/bluesky-social/indigo/atproto/identity"
+	"github.com/bluesky-social/indigo/atproto/identity"
 	"github.com/bluesky-social/indigo/atproto/syntax"
 
 	"github.com/urfave/cli/v2"
@@ -17,32 +19,63 @@ func main() {
 	}
 	app.Commands = []*cli.Command{
 		&cli.Command{
+			Name:   "lookup",
+			Usage:  "fully resolve an at-identifier (DID or handle)",
+			Action: runLookup,
+		},
+		&cli.Command{
 			Name:   "resolve-handle",
-			Usage:  "resolve a handle",
+			Usage:  "resolve a handle to DID",
 			Action: runResolveHandle,
 		},
 		&cli.Command{
 			Name:   "resolve-did",
-			Usage:  "resolve a DID",
+			Usage:  "resolve a DID to DID Document",
 			Action: runResolveDID,
 		},
 	}
 	app.RunAndExitOnError()
 }
 
+func runLookup(cctx *cli.Context) error {
+	s := cctx.Args().First()
+	if s == "" {
+		return fmt.Errorf("need to provide identifier as an argument")
+	}
+
+	id, err := syntax.ParseAtIdentifier(s)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("valid at-identifier syntax: %s\n", id)
+
+	ncat := identity.NewNaiveCatalog("https://plc.directory")
+
+	acc, err := ncat.Lookup(context.TODO(), *id)
+	if err != nil {
+		return err
+	}
+	fmt.Println(acc)
+	return nil
+}
+
 func runResolveHandle(cctx *cli.Context) error {
 	s := cctx.Args().First()
 	if s == "" {
-		fmt.Println("need to provide handle as an argument")
-		os.Exit(-1)
+		return fmt.Errorf("need to provide handle as an argument")
 	}
 
 	handle, err := syntax.ParseHandle(s)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
+		return err
 	}
 	fmt.Printf("valid handle syntax: %s\n", handle)
+
+	did, err := identity.ResolveHandle(context.TODO(), handle)
+	if err != nil {
+		return err
+	}
+	fmt.Println(did)
 	return nil
 }
 
@@ -59,5 +92,15 @@ func runResolveDID(cctx *cli.Context) error {
 		os.Exit(-1)
 	}
 	fmt.Printf("valid DID syntax: %s\n", did)
+
+	doc, err := identity.ResolveDID(context.TODO(), did)
+	if err != nil {
+		return err
+	}
+	jsonBytes, err := json.Marshal(&doc)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(jsonBytes))
 	return nil
 }
--- atproto/identity/identity.go
+++ atproto/identity/identity.go
@@ -1,33 +1,59 @@
 package identity
 
 import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/bluesky-social/indigo/atproto/crypto"
 	"github.com/bluesky-social/indigo/atproto/syntax"
+
+	"github.com/mr-tron/base58"
 )
 
 // API for doing account lookups by DID or handle, with bi-directional verification handled automatically.
 //
-// Handles which fail to resolve or don't match DID alsoKnownAs are an error. DIDs which resolve but the handle does not resolve back to the DID return an Account where the Handle is the special `handle.invalid` value.
+// Handles which fail to resolve or don't match DID alsoKnownAs are an error. DIDs which resolve but the handle does not resolve back to the DID return an Identity where the Handle is the special `handle.invalid` value.
 //
 // Some example implementations of this interface would be:
 //   - naive direct resolution on every call
 //   - API client, which just makes requests to PDS (or other remote service)
 //   - simple in-memory caching wrapper layer to reduce network hits
 //   - services with backing datastore to do sophisticated caching, TTL, auto-refresh, etc
-type AccountCatalog interface {
-	LookupHandle(h syntax.Handle) (*Account, error)
-	LookupDID(d syntax.DID) (*Account, error)
+type Catalog interface {
+	LookupHandle(ctx context.Context, h syntax.Handle) (*Identity, error)
+	LookupDID(ctx context.Context, d syntax.DID) (*Identity, error)
+	Lookup(ctx context.Context, i syntax.AtIdentifier) (*Identity, error)
+	// TODO: add "flush" methods to purge caches?
 }
 
+var DefaultPLCURL = "https://plc.directory"
+
+func DefaultCatalog() Catalog {
+	cat := NewCacheCatalog(DefaultPLCURL)
+	return &cat
+}
+
+// TODO: DIDHistory() helper, returns log of declared handle, PDS location, and public key? or maybe this is something best left to did-method-plc helper library
+
 // Represents an atproto identity. Could be a regular user account, or a service account (eg, feed generator)
-type Account struct {
-	// these fields are required and non-nullable
-	DID    syntax.DID
+type Identity struct {
+	DID syntax.DID
+
+	// Handle/DID mapping must be bi-directionally verified. If that fails, the Handle should be the special 'handle.invalid' value
+	// TODO: should we make this nullable, instead of 'handle.invalid'?
 	Handle syntax.Handle
 
-	// these fields are nullable
+	// These fields represent a parsed subset of a DID document. They are all nullable.
+	// TODO: should we just embed DIDDocument here?
 	AlsoKnownAs []string
-	Services    map[string]Service
-	Keys        map[string]Key
+	// TODO: this doesn't preserve order (doesn't round-trip)
+	Services map[string]Service
+	// TODO: this doesn't preserve order (doesn't round-trip)
+	Keys map[string]Key
+
+	// If a valid atproto repo signing public key was parsed, it can be cached here. This is nullable/optional.
+	ParsedPublicKey crypto.PublicKey
 }
 
 type Key struct {
@@ -40,24 +66,105 @@ type Service struct {
 	URL  string
 }
 
-func (a *Account) SigningKey() *Key {
-	if a.Keys == nil {
-		return nil
+func ParseIdentity(doc *DIDDocument) Identity {
+	keys := make(map[string]Key, len(doc.VerificationMethod))
+	for _, vm := range doc.VerificationMethod {
+		parts := strings.SplitN(vm.ID, "#", 2)
+		if len(parts) < 2 {
+			continue
+		}
+		// ignore keys not controlled by this DID itself
+		if vm.Controller != doc.DID.String() {
+			continue
+		}
+		// don't want to clobber existing entries with same ID fragment
+		if _, ok := keys[parts[1]]; ok {
+			continue
+		}
+		// TODO: verify that ID and type match for atproto-specific services?
+		keys[parts[1]] = Key{
+			Type:               vm.Type,
+			PublicKeyMultibase: vm.PublicKeyMultibase,
+		}
 	}
-	atp, ok := a.Keys["atproto"]
-	if !ok {
-		return nil
+	svc := make(map[string]Service, len(doc.Service))
+	for _, s := range doc.Service {
+		parts := strings.SplitN(s.ID, "#", 2)
+		if len(parts) < 2 {
+			continue
+		}
+		// don't want to clobber existing entries with same ID fragment
+		if _, ok := svc[parts[1]]; ok {
+			continue
+		}
+		// TODO: verify that ID and type match for atproto-specific services?
+		svc[parts[1]] = Service{
+			Type: s.Type,
+			URL:  s.ServiceEndpoint,
+		}
 	}
-	return &atp
+	return Identity{
+		DID:         doc.DID,
+		Handle:      syntax.Handle("invalid.handle"),
+		AlsoKnownAs: doc.AlsoKnownAs,
+		Services:    svc,
+		Keys:        keys,
+	}
 }
 
-func (a *Account) PDS() string {
-	if a.Services == nil {
+func (i *Identity) PublicKey() (crypto.PublicKey, error) {
+	if i.ParsedPublicKey != nil {
+		return i.ParsedPublicKey, nil
+	}
+	if i.Keys == nil {
+		return nil, fmt.Errorf("identity has no atproto public key attached")
+	}
+	k, ok := i.Keys["atproto"]
+	if !ok {
+		return nil, fmt.Errorf("identity has no atproto public key attached")
+	}
+	switch k.Type {
+	case "Multikey":
+		return crypto.ParsePublicMultibase(k.PublicKeyMultibase)
+	case "EcdsaSecp256r1VerificationKey2019":
+		if len(k.PublicKeyMultibase) < 2 || k.PublicKeyMultibase[0] != 'z' {
+			return nil, fmt.Errorf("identity key not a multibase base58btc string")
+		}
+		keyBytes, err := base58.Decode(k.PublicKeyMultibase[1:])
+		if err != nil {
+			return nil, fmt.Errorf("identity key multibase parsing: %w", err)
+		}
+		return crypto.ParsePublicUncompressedBytesP256(keyBytes)
+	case "EcdsaSecp256k1VerificationKey2019":
+		if len(k.PublicKeyMultibase) < 2 || k.PublicKeyMultibase[0] != 'z' {
+			return nil, fmt.Errorf("identity key not a multibase base58btc string")
+		}
+		keyBytes, err := base58.Decode(k.PublicKeyMultibase[1:])
+		if err != nil {
+			return nil, fmt.Errorf("identity key multibase parsing: %w", err)
+		}
+		return crypto.ParsePublicUncompressedBytesK256(keyBytes)
+	default:
+		return nil, fmt.Errorf("unsupported atproto public key type: %s", k.Type)
+	}
+}
+
+func (i *Identity) PDSEndpoint() string {
+	if i.Services == nil {
 		return ""
 	}
-	atp, ok := a.Services["atproto_pds"]
+	atp, ok := i.Services["atproto_pds"]
 	if !ok {
 		return ""
 	}
 	return atp.URL
+}
+
+func (i *Identity) DeclaredHandle() (syntax.Handle, error) {
+	for _, u := range i.AlsoKnownAs {
+		if strings.HasPrefix(u, "at://") && len(u) > len("at://") {
+			return syntax.ParseHandle(u[5:])
+		}
+	}
+	return "", fmt.Errorf("DID document contains no atproto handle")
 }
