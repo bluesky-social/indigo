affd362a51fae0033dcc692201e53cf3b2fde89d
bryan newbold <bnewbold@robocracy.org>
2023-08-26 20:27:04 -0700

    identity: progress

---
+++ atproto/identity/live_test.go
@@ -0,0 +1,64 @@
+package identity
+
+import (
+	"context"
+	"testing"
+
+	"github.com/bluesky-social/indigo/atproto/syntax"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// NOTE: this hits the open internet! marked as skip below by default
+func testCatalogLive(t *testing.T, c Catalog) {
+	assert := assert.New(t)
+	ctx := context.TODO()
+
+	handle := syntax.Handle("atproto.com")
+	did := syntax.DID("did:plc:ewvi7nxzyoun6zhxrhs64oiz")
+	pds := "https://bsky.social"
+
+	resp, err := c.LookupHandle(ctx, handle)
+	assert.NoError(err)
+	assert.Equal(handle, resp.Handle)
+	assert.Equal(did, resp.DID)
+	assert.Equal(pds, resp.PDSEndpoint())
+	dh, err := resp.DeclaredHandle()
+	assert.NoError(err)
+	assert.Equal(handle, dh)
+	pk, err := resp.PublicKey()
+	assert.NoError(err)
+	assert.NotNil(pk)
+
+	resp, err = c.LookupDID(ctx, did)
+	assert.NoError(err)
+	assert.Equal(handle, resp.Handle)
+	assert.Equal(did, resp.DID)
+	assert.Equal(pds, resp.PDSEndpoint())
+
+	_, err = c.LookupHandle(ctx, syntax.Handle("fake-dummy-no-resolve.atproto.com"))
+	assert.Equal(ErrHandleNotFound, err)
+
+	_, err = c.LookupDID(ctx, syntax.DID("did:web:fake-dummy-no-resolve.atproto.com"))
+	assert.Equal(ErrDIDNotFound, err)
+
+	_, err = c.LookupDID(ctx, syntax.DID("did:plc:fake-dummy-no-resolve.atproto.com"))
+	assert.Equal(ErrDIDNotFound, err)
+
+	_, err = c.LookupHandle(ctx, syntax.Handle("handle.invalid"))
+	assert.Error(err)
+}
+
+func TestNaiveCatalog(t *testing.T) {
+	t.Skip("skipping live network test")
+	c := NewNaiveCatalog(DefaultPLCURL)
+	testCatalogLive(t, &c)
+}
+
+func TestCacheCatalog(t *testing.T) {
+	t.Skip("skipping live network test")
+	c := NewCacheCatalog(DefaultPLCURL)
+	for i := 0; i < 3; i = i + 1 {
+		testCatalogLive(t, &c)
+	}
+}
---
+++ atproto/identity/mock_catalog.go
@@ -0,0 +1,63 @@
+package identity
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bluesky-social/indigo/atproto/syntax"
+)
+
+// A fake identity catalog, for use in tests
+// TODO: should probably move this to a 'mockcatalog' sub-package?
+type MockCatalog struct {
+	Handles    map[syntax.Handle]syntax.DID
+	Identities map[syntax.DID]Identity
+}
+
+var _ Catalog = (*MockCatalog)(nil)
+
+func NewMockCatalog() MockCatalog {
+	return MockCatalog{
+		Handles:    make(map[syntax.Handle]syntax.DID),
+		Identities: make(map[syntax.DID]Identity),
+	}
+}
+
+func (c *MockCatalog) Insert(ident Identity) {
+	if !ident.Handle.IsInvalidHandle() {
+		c.Handles[ident.Handle] = ident.DID
+	}
+	c.Identities[ident.DID] = ident
+}
+
+func (c *MockCatalog) LookupHandle(ctx context.Context, h syntax.Handle) (*Identity, error) {
+	did, ok := c.Handles[h]
+	if !ok {
+		return nil, ErrHandleNotFound
+	}
+	ident, ok := c.Identities[did]
+	if !ok {
+		return nil, ErrDIDNotFound
+	}
+	return &ident, nil
+}
+
+func (c *MockCatalog) LookupDID(ctx context.Context, did syntax.DID) (*Identity, error) {
+	ident, ok := c.Identities[did]
+	if !ok {
+		return nil, ErrDIDNotFound
+	}
+	return &ident, nil
+}
+
+func (c *MockCatalog) Lookup(ctx context.Context, a syntax.AtIdentifier) (*Identity, error) {
+	handle, err := a.AsHandle()
+	if err == nil {
+		return c.LookupHandle(ctx, handle)
+	}
+	did, err := a.AsDID()
+	if err == nil {
+		return c.LookupDID(ctx, did)
+	}
+	return nil, fmt.Errorf("at-identifier neither a Handle nor a DID")
+}
