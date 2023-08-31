7f737d6f54a91440c81596f521c7e91a2daf0300
Jaz Volpert <ericvolp12@gmail.com>
2023-08-31 06:09:43 +0000

    Bench experiment

--- cmd/netsync/playback.go
+++ cmd/netsync/playback.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"slices"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -27,6 +26,7 @@ import (
 	"github.com/scylladb/gocqlx/v2/table"
 
 	"github.com/urfave/cli/v2"
+	"golang.org/x/sync/semaphore"
 	"golang.org/x/text/language"
 	"golang.org/x/text/message"
 )
@@ -251,8 +251,6 @@ func Query(cctx *cli.Context) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	start := time.Now()
-
 	cluster := gocql.NewCluster(cctx.StringSlice("scylla-nodes")...)
 	session, err := gocqlx.WrapSession(cluster.CreateSession())
 	if err != nil {
@@ -272,100 +270,150 @@ func Query(cctx *cli.Context) error {
 		return fmt.Errorf("invalid post URI: %s", postURI)
 	}
 
-	// Get the post
-	post := Post{
-		Did:  postParts[0],
-		Rkey: postParts[2],
-	}
-	err = postTable.GetQuery(session).BindStruct(&post).GetRelease(&post)
-	if err != nil {
-		return fmt.Errorf("failed to get post: %w", err)
-	}
+	numRuns := 500000
+	maxConcurrent := 400
+	sem := semaphore.NewWeighted(int64(maxConcurrent))
 
-	// Get the replies
-	replyRefs := []Reply{}
-	err = repliesTable.SelectQuery(session).BindStruct(&Reply{
-		ParentDid:  postParts[0],
-		ParentRkey: postParts[2],
-	}).SelectRelease(&replyRefs)
-	if err != nil {
-		return fmt.Errorf("failed to get replies: %w", err)
-	}
+	totalRowsRead := atomic.Uint64{}
 
-	replies := []Post{}
-	lk := sync.Mutex{}
+	runtimes := make(chan time.Duration, numRuns)
 
-	// Resolve the replies as posts in parallel
-	var wg sync.WaitGroup
-	for i := range replyRefs {
-		wg.Add(1)
-		replyRef := replyRefs[i]
-		go func(replyRef Reply) {
-			defer wg.Done()
+	start := time.Now()
 
-			reply := Post{
-				Did:  replyRef.ChildDid,
-				Rkey: replyRef.ChildRkey,
+	// Run the query in numRuns goroutines
+	var pwg sync.WaitGroup
+	for i := 0; i < numRuns; i++ {
+		pwg.Add(1)
+		go func() error {
+			defer pwg.Done()
+			sem.Acquire(ctx, 1)
+			defer sem.Release(1)
+			iterStart := time.Now()
+			defer func() {
+				runtimes <- time.Since(iterStart)
+			}()
+
+			// Get the post
+			post := Post{
+				Did:  postParts[0],
+				Rkey: postParts[2],
 			}
-
-			err = postTable.GetQuery(session).BindStruct(&reply).GetRelease(&reply)
+			err = postTable.GetQuery(session).BindStruct(&post).GetRelease(&post)
 			if err != nil {
-				log.Errorf("failed to get reply: %w", err)
-				return
+				return fmt.Errorf("failed to get post: %w", err)
 			}
-			lk.Lock()
-			replies = append(replies, reply)
-			lk.Unlock()
-		}(replyRef)
-	}
 
-	// Resolve the parent up to the root
-	parents := []Post{}
-	if post.ParentDid != "" && post.ParentRkey != "" {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			parentDid := post.ParentDid
-			parentRkey := post.ParentRkey
-			for {
-				parent := Post{
-					Did:  parentDid,
-					Rkey: parentRkey,
-				}
-				err = postTable.GetQuery(session).BindStruct(&parent).GetRelease(&parent)
-				if err != nil {
-					log.Errorf("failed to get parent: %w", err)
-					return
-				}
+			totalRowsRead.Add(1)
 
-				parents = append(parents, parent)
-
-				if parent.ParentDid == "" {
-					break
-				}
-
-				parentDid = parent.ParentDid
-				parentRkey = parent.ParentRkey
+			// Get the replies
+			replyRefs := []Reply{}
+			err = repliesTable.SelectQuery(session).BindStruct(&Reply{
+				ParentDid:  postParts[0],
+				ParentRkey: postParts[2],
+			}).SelectRelease(&replyRefs)
+			if err != nil {
+				return fmt.Errorf("failed to get replies: %w", err)
 			}
+
+			totalRowsRead.Add(uint64(len(replyRefs)))
+
+			replies := []Post{}
+			lk := sync.Mutex{}
+
+			// Resolve the replies as posts in parallel
+			var wg sync.WaitGroup
+			for i := range replyRefs {
+				wg.Add(1)
+				replyRef := replyRefs[i]
+				go func(replyRef Reply) {
+					defer wg.Done()
+
+					reply := Post{
+						Did:  replyRef.ChildDid,
+						Rkey: replyRef.ChildRkey,
+					}
+
+					err = postTable.GetQuery(session).BindStruct(&reply).GetRelease(&reply)
+					if err != nil {
+						log.Errorf("failed to get reply: %+v", err)
+						return
+					}
+					lk.Lock()
+					replies = append(replies, reply)
+					lk.Unlock()
+				}(replyRef)
+			}
+
+			totalRowsRead.Add(uint64(len(replies)))
+
+			// Resolve the parent up to the root
+			parents := []Post{}
+			if post.ParentDid != "" && post.ParentRkey != "" {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					parentDid := post.ParentDid
+					parentRkey := post.ParentRkey
+					for {
+						parent := Post{
+							Did:  parentDid,
+							Rkey: parentRkey,
+						}
+						err = postTable.GetQuery(session).BindStruct(&parent).GetRelease(&parent)
+						if err != nil && err != gocql.ErrNotFound {
+							log.Errorf("failed to get parent: %+v", err)
+							return
+						}
+
+						parents = append(parents, parent)
+
+						if parent.ParentDid == "" {
+							break
+						}
+
+						parentDid = parent.ParentDid
+						parentRkey = parent.ParentRkey
+					}
+				}()
+			}
+
+			totalRowsRead.Add(uint64(len(parents)))
+
+			wg.Wait()
+			return nil
 		}()
 	}
 
-	wg.Wait()
+	// // Print the thread
+	// p := message.NewPrinter(language.English)
+	// log.Debugf("post: %s", post.Content)
+	// log.Debugf("replies: %d", len(replies))
+	// for _, reply := range replies {
+	// 	log.Debugf("  %s", reply.Content)
+	// }
+	// slices.Reverse(parents)
+	// log.Debugf("parents: %d", len(parents))
+	// for _, parent := range parents {
+	// 	log.Debugf("  %s", parent.Content)
+	// }
 
-	// Print the thread
+	// log.Info(p.Sprintf("processed post with %d replies and resolved %d parents in %s", len(replies), len(parents), time.Since(start)))
+
+	// Wait for all the queries to finish
+	pwg.Wait()
+	clockTime := time.Since(start)
+	close(runtimes)
+
+	// Calculate the average runtime
+	var total time.Duration
+	for runtime := range runtimes {
+		total += runtime
+	}
+	avg := total / time.Duration(numRuns)
+
 	p := message.NewPrinter(language.English)
-	log.Infof("post: %s", post.Content)
-	log.Infof("replies: %d", len(replies))
-	for _, reply := range replies {
-		log.Infof("  %s", reply.Content)
-	}
-	slices.Reverse(parents)
-	log.Infof("parents: %d", len(parents))
-	for _, parent := range parents {
-		log.Infof("  %s", parent.Content)
-	}
 
-	log.Info(p.Sprintf("processed post with %d replies and resolved %d parents in %s", len(replies), len(parents), time.Since(start)))
+	log.Info(p.Sprintf("processed post %d times (%d total reads) in %s (avg: %s, total: %s)", numRuns, totalRowsRead.Load(), clockTime, avg, total))
 
 	return nil
 }
