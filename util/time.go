5251558cf5c585b586438a37922179ffa6944c63
bryan newbold <bnewbold@robocracy.org>
2023-09-13 01:29:09 -0700

    util: allow non-fractional-second timestamps

--- util/time.go
+++ util/time.go
@@ -13,6 +13,10 @@ const ISO8601_numtz = "2006-01-02T15:04:05.000-07:00"
 
 const ISO8601_numtz_milli = "2006-01-02T15:04:05.000000-07:00"
 
+const ISO8601_sec = "2006-01-02T15:04:05Z"
+
+const ISO8601_numtz_sec = "2006-01-02T15:04:05-07:00"
+
 func ParseTimestamp(s string) (time.Time, error) {
 	t, err := time.Parse(ISO8601, s)
 	if err == nil {
@@ -30,6 +34,16 @@ func ParseTimestamp(s string) (time.Time, error) {
 	}
 
 	t, err = time.Parse(ISO8601_numtz_milli, s)
+	if err == nil {
+		return t, nil
+	}
+
+	t, err = time.Parse(ISO8601_sec, s)
+	if err == nil {
+		return t, nil
+	}
+
+	t, err = time.Parse(ISO8601_numtz_sec, s)
 	if err == nil {
 		return t, nil
 	}
