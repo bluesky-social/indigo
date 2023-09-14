23816938a11543915ea9f727f1126d3e659d26dc
Jaz Volpert <ericvolp12@gmail.com>
2023-09-14 22:35:59 +0000

    Slapdash GraphD WIP

---
+++ cmd/graphd/main.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/bluesky-social/indigo/graphd"
+	"github.com/bluesky-social/indigo/util/version"
+	"github.com/labstack/echo-contrib/echoprometheus"
+	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := cli.App{
+		Name:    "graphd",
+		Usage:   "relational graph daemon",
+		Version: version.Version,
+	}
+
+	app.Flags = []cli.Flag{
+		&cli.BoolFlag{
+			Name:  "debug",
+			Usage: "enable debug logging",
+		},
+		&cli.IntFlag{
+			Name:  "port",
+			Usage: "listen port for http server",
+			Value: 8345,
+		},
+		&cli.StringFlag{
+			Name:  "graph-csv",
+			Usage: "path to graph csv file",
+		},
+	}
+
+	app.Action = GraphD
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+type HealthStatus struct {
+	Status  string `json:"status"`
+	Version string `json:"version"`
+	Message string `json:"msg,omitempty"`
+}
+
+func GraphD(cctx *cli.Context) error {
+	logLevel := slog.LevelInfo
+	if cctx.Bool("debug") {
+		logLevel = slog.LevelDebug
+	}
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: logLevel,
+	})))
+
+	graph := graphd.NewGraph()
+
+	start := time.Now()
+	totalFollows := 0
+
+	nextUID := uint64(0)
+
+	// Load all the follows from the graph CSV
+	if cctx.String("graph-csv") != "" {
+		f, err := os.Open(cctx.String("graph-csv"))
+		if err != nil {
+			return fmt.Errorf("failed to open graph CSV (%s): %w", cctx.String("graph-csv"), err)
+		}
+
+		fileScanner := bufio.NewScanner(f)
+
+		fileScanner.Split(bufio.ScanLines)
+
+		for fileScanner.Scan() {
+			if totalFollows%1_000_000 == 0 {
+				slog.Info("loaded follows", "total", totalFollows, "duration", time.Since(start))
+			}
+
+			// actorDid,rkey,targetDid,createdAt,insertedAt
+			followTxt := fileScanner.Text()
+			parts := strings.Split(followTxt, ",")
+			actorDID := parts[0]
+			targetDID := parts[2]
+
+			actorUID, ok := graph.GetUid(actorDID)
+			if !ok {
+				actorUID = nextUID
+				nextUID++
+				graph.SetUid(actorDID, actorUID)
+				graph.SetDid(actorUID, actorDID)
+			}
+			targetUID, ok := graph.GetUid(targetDID)
+			if !ok {
+				targetUID = nextUID
+				nextUID++
+				graph.SetUid(targetDID, targetUID)
+				graph.SetDid(targetUID, targetDID)
+			}
+
+			graph.AddFollow(actorUID, targetUID)
+
+			totalFollows++
+		}
+
+		slog.Info("total follows", "total", totalFollows, "duration", time.Since(start))
+	}
+
+	e := echo.New()
+
+	e.GET("/_health", func(c echo.Context) error {
+		return c.JSON(200, HealthStatus{
+			Status:  "ok",
+			Version: version.Version,
+		})
+	})
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+	e.GET("/debug/*", echo.WrapHandler(http.DefaultServeMux))
+
+	e.Use(echoprometheus.NewMiddleware("graphd"))
+
+	e.GET("/followers", func(c echo.Context) error {
+		did := c.QueryParam("did")
+		queryStart := time.Now()
+
+		uid, ok := graph.GetUid(did)
+		if !ok {
+			slog.Error("uid not found")
+			return c.JSON(404, "uid not found")
+		}
+
+		uidDone := time.Now()
+		followers, err := graph.GetFollowers(uid)
+		if err != nil {
+			slog.Error("failed to get followers", "err", err)
+		}
+		membersDone := time.Now()
+
+		dids, err := graph.GetDids(followers)
+		if err != nil {
+			slog.Error("failed to get dids", "err", err)
+		}
+
+		didsDone := time.Now()
+		slog.Debug("got followers",
+			"followers", len(followers),
+			"uid", uid,
+			"uidDuration", uidDone.Sub(queryStart),
+			"membersDuration", membersDone.Sub(uidDone),
+			"didsDuration", didsDone.Sub(membersDone),
+			"totalDuration", didsDone.Sub(queryStart),
+		)
+
+		return c.JSON(200, dids)
+	})
+
+	e.GET("/following", func(c echo.Context) error {
+		did := c.QueryParam("did")
+		queryStart := time.Now()
+
+		uid, ok := graph.GetUid(did)
+		if !ok {
+			slog.Error("uid not found")
+			return c.JSON(404, "uid not found")
+		}
+
+		uidDone := time.Now()
+		following, err := graph.GetFollowing(uid)
+		if err != nil {
+			slog.Error("failed to get following", "err", err)
+		}
+		membersDone := time.Now()
+
+		dids, err := graph.GetDids(following)
+		if err != nil {
+			slog.Error("failed to get dids", "err", err)
+		}
+
+		didsDone := time.Now()
+		slog.Debug("got following",
+			"following", len(following),
+			"uid", uid,
+			"uidDuration", uidDone.Sub(queryStart),
+			"membersDuration", membersDone.Sub(uidDone),
+			"didsDuration", didsDone.Sub(membersDone),
+			"totalDuration", didsDone.Sub(queryStart),
+		)
+
+		return c.JSON(200, dids)
+	})
+
+	e.GET("/doesFollow", func(c echo.Context) error {
+		actorDid := c.QueryParam("actorDid")
+		targetDid := c.QueryParam("targetDid")
+
+		start := time.Now()
+
+		actorUID, ok := graph.GetUid(actorDid)
+		if !ok {
+			slog.Error("actor uid not found")
+			return c.JSON(404, "actor uid not found")
+		}
+
+		targetUID, ok := graph.GetUid(targetDid)
+		if !ok {
+			slog.Error("target uid not found")
+			return c.JSON(404, "target uid not found")
+		}
+
+		doesFollow, err := graph.DoesFollow(actorUID, targetUID)
+		if err != nil {
+			slog.Error("failed to check if follows", "err", err)
+			return c.JSON(500, "failed to check if follows")
+		}
+
+		slog.Debug("checked if follows", "doesFollow", doesFollow, "duration", time.Since(start))
+
+		return c.JSON(200, doesFollow)
+	})
+
+	return e.Start(fmt.Sprintf(":%d", cctx.Int("port")))
+}
---
+++ graphd/graph.go
@@ -0,0 +1,177 @@
+package graphd
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Graph struct {
+	follows   sync.Map
+	following sync.Map
+
+	utd   map[uint64]string
+	utdLk sync.RWMutex
+
+	dtu   map[string]uint64
+	dtuLk sync.RWMutex
+}
+
+type FollowMap struct {
+	data map[uint64]struct{}
+	lk   sync.RWMutex
+}
+
+func NewGraph() *Graph {
+	return &Graph{
+		utd: map[uint64]string{},
+		dtu: map[string]uint64{},
+	}
+}
+
+func (g *Graph) GetUid(did string) (uint64, bool) {
+	g.dtuLk.RLock()
+	defer g.dtuLk.RUnlock()
+	uid, ok := g.dtu[did]
+	return uid, ok
+}
+
+func (g *Graph) SetUid(did string, uid uint64) {
+	g.dtuLk.Lock()
+	defer g.dtuLk.Unlock()
+	g.dtu[did] = uid
+}
+
+func (g *Graph) GetDid(uid uint64) (string, bool) {
+	g.utdLk.RLock()
+	defer g.utdLk.RUnlock()
+	did, ok := g.utd[uid]
+	return did, ok
+}
+
+func (g *Graph) GetDids(uids []uint64) ([]string, error) {
+	g.utdLk.RLock()
+	defer g.utdLk.RUnlock()
+	dids := make([]string, len(uids))
+	for i, uid := range uids {
+		did, ok := g.utd[uid]
+		if !ok {
+			return nil, fmt.Errorf("uid %d not found", uid)
+		}
+		dids[i] = did
+	}
+	return dids, nil
+}
+
+func (g *Graph) GetUids(dids []string) ([]uint64, error) {
+	g.dtuLk.RLock()
+	defer g.dtuLk.RUnlock()
+	uids := make([]uint64, len(dids))
+	for i, did := range dids {
+		uid, ok := g.dtu[did]
+		if !ok {
+			return nil, fmt.Errorf("did %s not found", did)
+		}
+		uids[i] = uid
+	}
+	return uids, nil
+}
+
+func (g *Graph) SetDid(uid uint64, did string) {
+	g.utdLk.Lock()
+	defer g.utdLk.Unlock()
+	g.utd[uid] = did
+}
+
+func (g *Graph) AddFollow(actorUID, targetUID uint64) {
+	followMap, ok := g.follows.Load(actorUID)
+	if !ok {
+		followMap = &FollowMap{
+			data: map[uint64]struct{}{},
+		}
+		g.follows.Store(actorUID, followMap)
+	}
+	followMap.(*FollowMap).lk.Lock()
+	followMap.(*FollowMap).data[targetUID] = struct{}{}
+	followMap.(*FollowMap).lk.Unlock()
+
+	// Add the follow to the graph
+	followMap, ok = g.following.Load(targetUID)
+	if !ok {
+		followMap = &FollowMap{
+			data: map[uint64]struct{}{},
+		}
+		g.following.Store(targetUID, followMap)
+	}
+	followMap.(*FollowMap).lk.Lock()
+	followMap.(*FollowMap).data[actorUID] = struct{}{}
+	followMap.(*FollowMap).lk.Unlock()
+}
+
+func (g *Graph) RemoveFollow(actorUID, targetUID uint64) error {
+	followMap, ok := g.follows.Load(actorUID)
+	if !ok {
+		return fmt.Errorf("actor uid %d not found", actorUID)
+	}
+	followMap.(*FollowMap).lk.Lock()
+	delete(followMap.(*FollowMap).data, targetUID)
+	followMap.(*FollowMap).lk.Unlock()
+
+	// Remove the follow from the graph
+	followMap, ok = g.following.Load(targetUID)
+	if !ok {
+		return fmt.Errorf("target uid %d not found", targetUID)
+	}
+	followMap.(*FollowMap).lk.Lock()
+	delete(followMap.(*FollowMap).data, actorUID)
+	followMap.(*FollowMap).lk.Unlock()
+
+	return nil
+}
+
+func (g *Graph) GetFollowers(uid uint64) ([]uint64, error) {
+	followMap, ok := g.following.Load(uid)
+	if !ok {
+		return nil, fmt.Errorf("uid %d not found", uid)
+	}
+	followMap.(*FollowMap).lk.RLock()
+	defer followMap.(*FollowMap).lk.RUnlock()
+
+	followers := make([]uint64, len(followMap.(*FollowMap).data))
+	i := 0
+	for follower := range followMap.(*FollowMap).data {
+		followers[i] = follower
+		i++
+	}
+
+	return followers, nil
+}
+
+func (g *Graph) GetFollowing(uid uint64) ([]uint64, error) {
+	followMap, ok := g.follows.Load(uid)
+	if !ok {
+		return nil, fmt.Errorf("uid %d not found", uid)
+	}
+	followMap.(*FollowMap).lk.RLock()
+	defer followMap.(*FollowMap).lk.RUnlock()
+
+	following := make([]uint64, len(followMap.(*FollowMap).data))
+	i := 0
+	for follower := range followMap.(*FollowMap).data {
+		following[i] = follower
+		i++
+	}
+
+	return following, nil
+}
+
+func (g *Graph) DoesFollow(actorUID, targetUID uint64) (bool, error) {
+	followMap, ok := g.follows.Load(actorUID)
+	if !ok {
+		return false, fmt.Errorf("actor uid %d not found", actorUID)
+	}
+	followMap.(*FollowMap).lk.RLock()
+	defer followMap.(*FollowMap).lk.RUnlock()
+
+	_, ok = followMap.(*FollowMap).data[targetUID]
+	return ok, nil
+}
