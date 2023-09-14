1624de00c4f13926412ccabcb563014770f58653
bryan newbold <bnewbold@robocracy.org>
2023-09-13 17:20:09 -0700

    palomar: fix bug in query marshal

--- search/query.go
+++ search/query.go
@@ -167,9 +167,10 @@ func doSearch(ctx context.Context, escli *es.Client, index string, query interfa
 		return nil, fmt.Errorf("error encoding query: %s", err)
 	}
 	bod, err := json.Marshal(query)
-	if nil == err {
-		slog.Warn("sending query", "index", index, "query", string(bod))
+	if err != nil {
+		return nil, fmt.Errorf("failed to serialize query: %w", err)
 	}
+	slog.Warn("sending query", "index", index, "query", string(bod))
 
 	// Perform the search request.
 	res, err := escli.Search(
