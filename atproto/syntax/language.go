8038f1917df98bd9d995fc3a23e15c20a2c7308e
bryan newbold <bnewbold@robocracy.org>
2023-10-01 16:37:24 -0700

    syntax: basic language code (BCP-47) validation

---
+++ atproto/syntax/language.go
@@ -0,0 +1,41 @@
+package syntax
+
+import (
+	"fmt"
+	"regexp"
+)
+
+// Represents a Language specifier in string format, as would pass Lexicon syntax validation.
+//
+// Always use [ParseLanguage] instead of wrapping strings directly, especially when working with network input.
+//
+// The syntax is BCP-47. This is a partial/naive parsing implementation, designed for fast validation and exact-string passthrough with no normaliztion. For actually working with BCP-47 language specifiers in atproto code bases, we recommend the golang.org/x/text/language package.
+type Language string
+
+func ParseLanguage(raw string) (Language, error) {
+	if len(raw) > 128 {
+		return "", fmt.Errorf("Language is too long (128 chars max)")
+	}
+	var langRegex = regexp.MustCompile(`^(i|[a-z]{2,3})(-[a-zA-Z0-9]+)*$`)
+	if !langRegex.MatchString(raw) {
+		return "", fmt.Errorf("Language syntax didn't validate via regex")
+	}
+	return Language(raw), nil
+}
+
+func (l Language) String() string {
+	return string(l)
+}
+
+func (l Language) MarshalText() ([]byte, error) {
+	return []byte(l.String()), nil
+}
+
+func (l *Language) UnmarshalText(text []byte) error {
+	lang, err := ParseLanguage(string(text))
+	if err != nil {
+		return err
+	}
+	*l = lang
+	return nil
+}
