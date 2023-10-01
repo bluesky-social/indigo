c8b4be8da93024532825db58f80a771548f2512a
bryan newbold <bnewbold@robocracy.org>
2023-10-01 15:56:10 -0700

    syntax: basic CID (string only) syntax

---
+++ atproto/syntax/cid_test.go
@@ -0,0 +1,50 @@
+package syntax
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInteropCIDsValid(t *testing.T) {
+	assert := assert.New(t)
+	file, err := os.Open("testdata/cid_syntax_valid.txt")
+	assert.NoError(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+		_, err := ParseCID(line)
+		if err != nil {
+			fmt.Println("GOOD: " + line)
+		}
+		assert.NoError(err)
+	}
+	assert.NoError(scanner.Err())
+}
+
+func TestInteropCIDsInvalid(t *testing.T) {
+	assert := assert.New(t)
+	file, err := os.Open("testdata/cid_syntax_invalid.txt")
+	assert.NoError(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+		_, err := ParseCID(line)
+		if err == nil {
+			fmt.Println("BAD: " + line)
+		}
+		assert.Error(err)
+	}
+	assert.NoError(scanner.Err())
+}
