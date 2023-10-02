8dc03a113f28ee1ff66a6d792b1c30b9236f251c
bryan newbold <bnewbold@robocracy.org>
2023-10-02 00:00:50 -0700

    syntax: TID helpers, including generator 'clock'

--- atproto/syntax/tid.go
+++ atproto/syntax/tid.go
@@ -1,9 +1,21 @@
 package syntax
 
 import (
+	"encoding/base32"
 	"fmt"
 	"regexp"
+	"strings"
+	"sync"
+	"time"
 )
+
+const (
+	Base32SortAlphabet = "234567abcdefghijklmnopqrstuvwxyz"
+)
+
+func Base32Sort() *base32.Encoding {
+	return base32.NewEncoding(Base32SortAlphabet).WithPadding(base32.NoPadding)
+}
 
 // Represents a TID in string format, as would pass Lexicon syntax validation.
 //
@@ -23,7 +35,58 @@ func ParseTID(raw string) (TID, error) {
 	return TID(raw), nil
 }
 
-// TODO: additional helpers: to timestamp, from timestamp, from integer, etc
+// Naive (unsafe) one-off TID generation with the current time.
+//
+// You should usually use a [TIDClock] to ensure monotonic output.
+func NewTIDNow(clockId uint) TID {
+	return NewTID(time.Now().UTC().UnixMicro(), clockId)
+}
+
+func NewTIDFromInteger(v uint64) TID {
+	v = (0x7FFF_FFFF_FFFF_FFFF & v)
+	s := ""
+	for i := 0; i < 13; i++ {
+		s = string(Base32SortAlphabet[v&0x1F]) + s
+		v = v >> 5
+	}
+	return TID(s)
+}
+
+// Constructs a new TID from a UNIX timestamp (in milliseconds) and clock ID value.
+func NewTID(unixMilis int64, clockId uint) TID {
+	var v uint64 = (uint64(unixMilis&0x1F_FFFF_FFFF_FFFF) << 10) | uint64(clockId&0x3FF)
+	return NewTIDFromInteger(v)
+}
+
+// Returns full integer representation of this TID (not used often)
+func (t TID) Integer() uint64 {
+	s := t.String()
+	if len(s) != 13 {
+		return 0
+	}
+	var v uint64
+	for i := 0; i < 13; i++ {
+		c := strings.IndexByte(Base32SortAlphabet, s[i])
+		if c < 0 {
+			return 0
+		}
+		v = (v << 5) | uint64(c&0x1F)
+	}
+	return v
+}
+
+// Returns the golang [time.Time] corresponding to this TID's timestamp.
+func (t TID) Time() time.Time {
+	i := t.Integer()
+	i = (i >> 10) & 0x1FFF_FFFF_FFFF_FFFF
+	return time.UnixMicro(int64(i)).UTC()
+}
+
+// Returns the clock ID part of this TID, as an unsigned integer
+func (t TID) ClockID() uint {
+	i := t.Integer()
+	return uint(i & 0x3FF)
+}
 
 func (t TID) String() string {
 	return string(t)
@@ -40,4 +103,30 @@ func (t *TID) UnmarshalText(text []byte) error {
 	}
 	*t = tid
 	return nil
+}
+
+// TID generator, which keeps state to ensure TID values always monotonically increase.
+//
+// Uses [sync.Mutex], so may block briefly but safe for concurrent use.
+type TIDClock struct {
+	ClockID       uint
+	mtx           sync.Mutex
+	lastUnixMicro int64
+}
+
+func NewTIDClock(clockId uint) *TIDClock {
+	return &TIDClock{
+		ClockID: clockId,
+	}
+}
+
+func (c *TIDClock) Next() TID {
+	now := time.Now().UTC().UnixMicro()
+	c.mtx.Lock()
+	if now <= c.lastUnixMicro {
+		now = c.lastUnixMicro + 1
+	}
+	c.lastUnixMicro = now
+	c.mtx.Unlock()
+	return NewTID(now, c.ClockID)
 }
--- atproto/syntax/tid_test.go
+++ atproto/syntax/tid_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -47,4 +48,80 @@ func TestInteropTIDsInvalid(t *testing.T) {
 		assert.Error(err)
 	}
 	assert.NoError(scanner.Err())
+}
+
+func TestTIDParts(t *testing.T) {
+	assert := assert.New(t)
+
+	raw := "3kao2cl6lyj2p"
+	tid, err := ParseTID(raw)
+	assert.NoError(err)
+	// TODO: assert.Equal(uint64(0x181a8044491f3bec), tid.Integer())
+	// TODO: assert.Equal(uint(1004), tid.ClockID())
+	assert.Equal(2023, tid.Time().Year())
+
+	out := NewTID(tid.Time().UnixMicro(), tid.ClockID())
+	assert.Equal(raw, out.String())
+	assert.Equal(tid.ClockID(), out.ClockID())
+	assert.Equal(tid.Time(), out.Time())
+	assert.Equal(tid.Integer(), out.Integer())
+
+	out2 := NewTIDFromInteger(tid.Integer())
+	assert.Equal(tid.Integer(), out2.Integer())
+}
+
+func TestTIDExamples(t *testing.T) {
+	assert := assert.New(t)
+	// TODO: seems like TS code might be wrong? "242k52k4kg3s2"
+	assert.Equal("242k52k4kg3sc", NewTIDFromInteger(0x0102030405060708).String())
+	assert.Equal(uint64(0x0102030405060708), TID("242k52k4kg3sc").Integer())
+	//assert.Equal("2222222222222", NewTIDFromInteger(0x0000000000000000).String())
+	//assert.Equal(uint64(), TID("242k52k4kg3s2").Integer())
+	assert.Equal("2222222222223", NewTIDFromInteger(0x0000000000000001).String())
+	assert.Equal(uint64(0x0000000000000001), TID("2222222222223").Integer())
+
+	assert.Equal("6222222222222", NewTIDFromInteger(0x4000000000000000).String())
+	assert.Equal(uint64(0x4000000000000000), TID("6222222222222").Integer())
+
+	// ignoring type byte
+	assert.Equal("2222222222222", NewTIDFromInteger(0x8000000000000000).String())
+}
+
+func TestTIDNoPanic(t *testing.T) {
+	for _, s := range []string{"", "3jzfcijpj2z2aa", "3jzfcijpj2z2", ".."} {
+		bad := TID(s)
+		_ = bad.ClockID()
+		_ = bad.Integer()
+		_ = bad.Time()
+		_ = bad.String()
+	}
+}
+
+func TestTIDConstruction(t *testing.T) {
+	assert := assert.New(t)
+
+	zero := NewTID(0, 0)
+	assert.Equal("2222222222222", zero.String())
+	assert.Equal(uint64(0), zero.Integer())
+	assert.Equal(uint(0), zero.ClockID())
+	assert.Equal(time.UnixMilli(0).UTC(), zero.Time())
+
+	now := NewTIDNow(1011)
+	assert.Equal(uint(1011), now.ClockID())
+	assert.True(time.Since(now.Time()) < time.Minute)
+
+	over := NewTIDNow(4096)
+	assert.Equal(uint(0), over.ClockID())
+}
+
+func TestTIDClock(t *testing.T) {
+	assert := assert.New(t)
+
+	clk := NewTIDClock(0)
+	last := NewTID(0, 0)
+	for i := 0; i < 100; i++ {
+		next := clk.Next()
+		assert.Greater(next, last)
+		last = next
+	}
 }
