74ffc243191d8969b860f8f3f7ecb904a4f8ee72
bryan newbold <bnewbold@robocracy.org>
2023-10-30 21:40:50 -0700

    data: doc comments

--- atproto/data/data.go
+++ atproto/data/data.go
@@ -7,11 +7,15 @@ import (
 	cbor "github.com/ipfs/go-ipld-cbor"
 )
 
+// Checks that generic data (object) complies with the atproto data model.
 func Validate(obj map[string]any) error {
 	_, err := parseObject(obj)
 	return err
 }
 
+// Parses generic data (object) in JSON, validating against the atproto data model at the same time.
+//
+// The standard library's MarshalJSON can be used to invert this function.
 func UnmarshalJSON(b []byte) (map[string]any, error) {
 	var rawObj map[string]any
 	err := json.Unmarshal(b, &rawObj)
@@ -25,6 +29,7 @@ func UnmarshalJSON(b []byte) (map[string]any, error) {
 	return out, nil
 }
 
+// Parses generic data (object) in CBOR (specifically, IPLD dag-cbor), validating against the atproto data model at the same time.
 func UnmarshalCBOR(b []byte) (map[string]any, error) {
 	var rawObj map[string]any
 	err := cbor.DecodeInto(b, &rawObj)
@@ -38,11 +43,16 @@ func UnmarshalCBOR(b []byte) (map[string]any, error) {
 	return out, nil
 }
 
+// Serializes generic atproto data (object) to DAG-CBOR bytes
+//
+// Does not re-validate that data conforms to atproto data model, but does handle Blob, Bytes, and CIDLink as expected.
 func MarshalCBOR(obj map[string]any) ([]byte, error) {
 	return cbor.DumpObject(forCBOR(obj))
 }
 
+// helper to get generic data in the correct "shape" for serialization with ipfs/go-ipld-cbor
 func forCBOR(obj map[string]any) map[string]any {
+	// NOTE: a faster version might mutate the map in-place instead of copying (many allocations)?
 	out := make(map[string]any, len(obj))
 	for k, val := range obj {
 		switch v := val.(type) {
@@ -69,7 +79,9 @@ func forCBOR(obj map[string]any) map[string]any {
 	return out
 }
 
+// recursive helper for forCBOR
 func forCBORArray(arr []any) []any {
+	// NOTE: a faster version might mutate the array in-place instead of copying (many allocations)?
 	out := make([]any, len(arr))
 	for i, val := range arr {
 		switch v := val.(type) {
