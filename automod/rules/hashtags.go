d46ed60a32102a4d42a4cd73c87b7a81a9e52317
bryan newbold <bnewbold@robocracy.org>
2023-11-15 19:05:51 -0800

    rules: refactor with some helpers

--- automod/rules/hashtags.go
+++ automod/rules/hashtags.go
@@ -5,21 +5,10 @@ import (
 )
 
 func BanHashtagsPostRule(evt *automod.PostEvent) error {
-	for _, tag := range evt.Post.Tags {
+	for _, tag := range ExtractHashtags(evt.Post) {
 		if evt.InSet("banned-hashtags", tag) {
 			evt.AddRecordLabel("bad-hashtag")
 			break
-		}
-	}
-	for _, facet := range evt.Post.Facets {
-		for _, feat := range facet.Features {
-			if feat.RichtextFacet_Tag != nil {
-				tag := feat.RichtextFacet_Tag.Tag
-				if evt.InSet("banned-hashtags", tag) {
-					evt.AddRecordLabel("bad-hashtag")
-					break
-				}
-			}
 		}
 	}
 	return nil
--- automod/rules/misleading.go
+++ automod/rules/misleading.go
@@ -9,35 +9,32 @@ import (
 )
 
 func MisleadingURLPostRule(evt *automod.PostEvent) error {
-	for _, facet := range evt.Post.Facets {
-		for _, feat := range facet.Features {
-			if feat.RichtextFacet_Link != nil {
-				if int(facet.Index.ByteEnd) > len([]byte(evt.Post.Text)) || facet.Index.ByteStart > facet.Index.ByteEnd {
-					evt.Logger.Warn("invalid facet range")
-					evt.AddRecordLabel("invalid") // TODO: or some other "this record is corrupt" indicator?
-					continue
-				}
-				txt := string([]byte(evt.Post.Text)[facet.Index.ByteStart:facet.Index.ByteEnd])
+	facets, err := ExtractFacets(evt.Post)
+	if err != nil {
+		evt.Logger.Warn("invalid facets", "err", err)
+		evt.AddRecordLabel("invalid") // TODO: or some other "this record is corrupt" indicator?
+		return nil
+	}
+	for _, facet := range facets {
+		if facet.URL != nil {
+			linkURL, err := url.Parse(*facet.URL)
+			if err != nil {
+				evt.Logger.Warn("invalid link metadata URL", "uri", facet.URL)
+				continue
+			}
 
-				linkURL, err := url.Parse(feat.RichtextFacet_Link.Uri)
-				if err != nil {
-					evt.Logger.Warn("invalid link metadata URL", "uri", feat.RichtextFacet_Link.Uri)
-					continue
-				}
+			// try parsing as a full URL
+			textURL, err := url.Parse(facet.Text)
+			if err != nil {
+				evt.Logger.Warn("invalid link text URL", "uri", facet.Text)
+				continue
+			}
 
-				// try parsing as a full URL
-				textURL, err := url.Parse(txt)
-				if err != nil {
-					evt.Logger.Warn("invalid link text URL", "uri", txt)
-					continue
-				}
-
-				// for now just compare domains to handle the most obvious cases
-				// this public code will obviously get discovered and bypassed. this doesn't earn you any security cred!
-				if linkURL.Host != textURL.Host {
-					evt.Logger.Warn("misleading mismatched domains", "link", linkURL.Host, "text", textURL.Host)
-					evt.AddRecordLabel("misleading")
-				}
+			// for now just compare domains to handle the most obvious cases
+			// this public code will obviously get discovered and bypassed. this doesn't earn you any security cred!
+			if linkURL.Host != textURL.Host {
+				evt.Logger.Warn("misleading mismatched domains", "link", linkURL.Host, "text", textURL.Host)
+				evt.AddRecordLabel("misleading")
 			}
 		}
 	}
@@ -47,37 +44,36 @@ func MisleadingURLPostRule(evt *automod.PostEvent) error {
 func MisleadingMentionPostRule(evt *automod.PostEvent) error {
 	// TODO: do we really need to route context around? probably
 	ctx := context.TODO()
-	for _, facet := range evt.Post.Facets {
-		for _, feat := range facet.Features {
-			if feat.RichtextFacet_Mention != nil {
-				if int(facet.Index.ByteEnd) > len([]byte(evt.Post.Text)) || facet.Index.ByteStart > facet.Index.ByteEnd {
-					evt.Logger.Warn("invalid facet range")
-					evt.AddRecordLabel("invalid") // TODO: or some other "this record is corrupt" indicator?
-					continue
-				}
-				txt := string([]byte(evt.Post.Text)[facet.Index.ByteStart:facet.Index.ByteEnd])
-				if txt[0] == '@' {
-					txt = txt[1:]
-				}
-				handle, err := syntax.ParseHandle(txt)
-				if err != nil {
-					evt.Logger.Warn("mention was not a valid handle", "text", txt)
-					continue
-				}
+	facets, err := ExtractFacets(evt.Post)
+	if err != nil {
+		evt.Logger.Warn("invalid facets", "err", err)
+		evt.AddRecordLabel("invalid") // TODO: or some other "this record is corrupt" indicator?
+		return nil
+	}
+	for _, facet := range facets {
+		if facet.DID != nil {
+			txt := facet.Text
+			if txt[0] == '@' {
+				txt = txt[1:]
+			}
+			handle, err := syntax.ParseHandle(txt)
+			if err != nil {
+				evt.Logger.Warn("mention was not a valid handle", "text", txt)
+				continue
+			}
 
-				mentioned, err := evt.Engine.Directory.LookupHandle(ctx, handle)
-				if err != nil {
-					evt.Logger.Warn("could not resolve handle", "handle", handle)
-					evt.AddRecordLabel("misleading")
-					break
-				}
+			mentioned, err := evt.Engine.Directory.LookupHandle(ctx, handle)
+			if err != nil {
+				evt.Logger.Warn("could not resolve handle", "handle", handle)
+				evt.AddRecordLabel("misleading")
+				break
+			}
 
-				// TODO: check if mentioned DID was recently updated? might be a caching issue
-				if mentioned.DID.String() != feat.RichtextFacet_Mention.Did {
-					evt.Logger.Warn("misleading mention", "text", txt, "did", mentioned.DID)
-					evt.AddRecordLabel("misleading")
-					continue
-				}
+			// TODO: check if mentioned DID was recently updated? might be a caching issue
+			if mentioned.DID.String() != *facet.DID {
+				evt.Logger.Warn("misleading mention", "text", txt, "did", facet.DID)
+				evt.AddRecordLabel("misleading")
+				continue
 			}
 		}
 	}
