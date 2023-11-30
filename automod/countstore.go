94df4294b1064c23ec7c696fcd26160737f91268
bryan newbold <bnewbold@robocracy.org>
2023-11-29 19:53:22 -0800

    automod: distinct counters (mem and redis)

--- automod/countstore.go
+++ automod/countstore.go
@@ -17,16 +17,20 @@ type CountStore interface {
 	GetCount(ctx context.Context, name, val, period string) (int, error)
 	Increment(ctx context.Context, name, val string) error
 	// TODO: batch increment method
+	GetCountDistinct(ctx context.Context, name, bucket, period string) (int, error)
+	IncrementDistinct(ctx context.Context, name, bucket, val string) error
 }
 
 // TODO: this implementation isn't race-safe (yet)!
 type MemCountStore struct {
-	Counts map[string]int
+	Counts         map[string]int
+	DistinctCounts map[string]map[string]bool
 }
 
 func NewMemCountStore() MemCountStore {
 	return MemCountStore{
-		Counts: make(map[string]int),
+		Counts:         make(map[string]int),
+		DistinctCounts: make(map[string]map[string]bool),
 	}
 }
 
@@ -63,6 +67,27 @@ func (s MemCountStore) Increment(ctx context.Context, name, val string) error {
 		}
 		v = v + 1
 		s.Counts[k] = v
+	}
+	return nil
+}
+
+func (s MemCountStore) GetCountDistinct(ctx context.Context, name, bucket, period string) (int, error) {
+	v, ok := s.DistinctCounts[PeriodBucket(name, bucket, period)]
+	if !ok {
+		return 0, nil
+	}
+	return len(v), nil
+}
+
+func (s MemCountStore) IncrementDistinct(ctx context.Context, name, bucket, val string) error {
+	for _, p := range []string{PeriodTotal, PeriodDay, PeriodHour} {
+		k := PeriodBucket(name, bucket, p)
+		m, ok := s.DistinctCounts[k]
+		if !ok {
+			m = make(map[string]bool)
+		}
+		m[val] = true
+		s.DistinctCounts[k] = m
 	}
 	return nil
 }
