fb5a9a1d146b5a5d62cbc5d1493cc341e4ed4d43
Eric Myhre <hash@exultant.us>
2023-12-05 18:37:55 +0100

    automod: periodBucket helper is used in both impls; move to file with interfaces and shared logic.
    Also, no clear reason for it to be exported, so it no longer is.

--- automod/countstore/countstore.go
+++ automod/countstore/countstore.go
@@ -2,6 +2,9 @@ package countstore
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"time"
 )
 
 const (
@@ -16,4 +19,20 @@ type CountStore interface {
 	// TODO: batch increment method
 	GetCountDistinct(ctx context.Context, name, bucket, period string) (int, error)
 	IncrementDistinct(ctx context.Context, name, bucket, val string) error
+}
+
+func periodBucket(name, val, period string) string {
+	switch period {
+	case PeriodTotal:
+		return fmt.Sprintf("%s/%s", name, val)
+	case PeriodDay:
+		t := time.Now().UTC().Format(time.DateOnly)
+		return fmt.Sprintf("%s/%s/%s", name, val, t)
+	case PeriodHour:
+		t := time.Now().UTC().Format(time.RFC3339)[0:13]
+		return fmt.Sprintf("%s/%s/%s", name, val, t)
+	default:
+		slog.Warn("unhandled counter period", "period", period)
+		return fmt.Sprintf("%s/%s", name, val)
+	}
 }
