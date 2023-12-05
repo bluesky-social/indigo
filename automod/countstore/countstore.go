ab64ed8751a51c34f723a7f1d7ff34c604446779
Eric Myhre <hash@exultant.us>
2023-12-05 20:08:27 +0100

    automod: quick docs for CountStore interface.

--- automod/countstore/countstore.go
+++ automod/countstore/countstore.go
@@ -13,11 +13,22 @@ const (
 	PeriodHour  = "hour"
 )
 
+// CountStore is an interface for storing incrementing event counts, bucketed into periods.
+// It is implemented by MemCountStore and by RedisCountStore.
+//
+// Period bucketing works on the basis of the current date (as determined mid-call).
+// See the `Period*` consts for the available period types.
+//
+// The "GetCount" and "Increment" methods perform actual counting.
+// The "*Distinct" methods store booleans for values, rather than incrementing;
+// fetching "GetCountDistinct" asks how many values have been seen at least once,
+// rather than how many times they've been seen.
 type CountStore interface {
 	GetCount(ctx context.Context, name, val, period string) (int, error)
 	Increment(ctx context.Context, name, val string) error
 	// TODO: batch increment method
 	GetCountDistinct(ctx context.Context, name, bucket, period string) (int, error)
+	// REVIEW: s/IncrementDistinct/MarkDistinct/ ?
 	IncrementDistinct(ctx context.Context, name, bucket, val string) error
 }
 
