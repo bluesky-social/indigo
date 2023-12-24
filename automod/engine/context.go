62497bc4bf1109853bf73eaee51a05a905f07a7f
bryan newbold <bnewbold@robocracy.org>
2023-12-24 15:36:00 +0100

    introduct Context types

---
+++ automod/engine/context.go
@@ -0,0 +1,131 @@
+package engine
+
+import (
+	"context"
+	"log/slog"
+)
+
+// The primary interface exposed to rules.
+type BaseContext struct {
+	// Actual golang "context.Context", if needed for timeouts etc
+	Ctx context.Context
+	// Any errors encountered while processing methods on this struct (or sub-types) get rolled up in this nullable field
+	Err error
+	// slog logger handle, with event-specific structured fields pre-populated. Pointer, but expected to never be nil.
+	Logger *slog.Logger
+
+	engine  *Engine // NOTE: pointer, but expected never to be nil
+	effects Effects
+}
+
+type AccountContext struct {
+	BaseContext
+
+	Account AccountMeta
+}
+
+type RecordContext struct {
+	AccountContext
+
+	RecordOp RecordOp
+	// TODO: could consider adding commit-level metadata here. probably nullable if so, commit-level metadata isn't always available. might be best to do a separate event/context type for that
+}
+
+var (
+	CreateOp = "create"
+	UpdateOp = "update"
+	DeleteOp = "delete"
+)
+
+// Immutable
+type RecordOp struct {
+	// Indicates type of record mutation: create, update, or delete.
+	// The term "action" is copied from com.atproto.sync.subscribeRepos#repoOp
+	Action     string
+	DID        string // NOTE: redundant with account, but eases discoverability?
+	Collection string
+	RecordKey  string
+	CID        *string // TODO: cid.Cid?
+	Record     any
+}
+
+// TODO: in the future *may* have an IdentityContext with an IdentityOp sub-field
+
+// request external state via engine (indirect)
+func (c *BaseContext) GetCount(name, val, period string) int {
+	out, err := c.engine.Counters.GetCount(c.Ctx, name, val, period)
+	if err != nil {
+		if nil == c.Err {
+			c.Err = err
+		}
+		return 0
+	}
+	return out
+}
+
+func (c *BaseContext) GetCountDistinct(name, bucket, period string) int {
+	out, err := c.engine.Counters.GetCountDistinct(c.Ctx, name, bucket, period)
+	if err != nil {
+		if nil == c.Err {
+			c.Err = err
+		}
+		return 0
+	}
+	return out
+}
+
+func (c *BaseContext) InSet(name, val string) bool {
+	out, err := c.engine.Sets.InSet(c.Ctx, name, val)
+	if err != nil {
+		if nil == c.Err {
+			c.Err = err
+		}
+		return false
+	}
+	return out
+}
+
+// update effects (indirect)
+func (c *BaseContext) Increment(name, val string) {
+	c.effects.Increment(name, val)
+}
+
+func (c *BaseContext) IncrementDistinct(name, bucket, val string) {
+	c.effects.IncrementDistinct(name, bucket, val)
+}
+
+func (c *BaseContext) IncrementPeriod(name, val string, period string) {
+	c.effects.IncrementPeriod(name, val, period)
+}
+
+func (c *AccountContext) AddAccountFlag(val string) {
+	c.effects.AddAccountFlag(val)
+}
+
+func (c *AccountContext) AddAccountLabel(val string) {
+	c.effects.AddAccountLabel(val)
+}
+
+func (c *AccountContext) ReportAccount(reason, comment string) {
+	c.effects.ReportAccount(reason, comment)
+}
+
+func (c *AccountContext) TakedownAccount() {
+	c.effects.TakedownAccount()
+}
+
+func (c *RecordContext) AddRecordFlag(val string) {
+	c.effects.AddRecordFlag(val)
+}
+
+func (c *RecordContext) AddRecordLabel(val string) {
+	c.effects.AddRecordLabel(val)
+}
+
+func (c *RecordContext) ReportRecord(reason, comment string) {
+	c.effects.ReportRecord(reason, comment)
+}
+
+func (c *RecordContext) TakedownRecord() {
+	c.effects.TakedownRecord()
+}
