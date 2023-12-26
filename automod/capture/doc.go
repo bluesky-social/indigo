c6b0accbb86ba5aeb96f1e9be3a36e333f879b68
bryan newbold <bnewbold@robocracy.org>
2023-12-26 21:50:34 +0100

    automod: initial doc files for all sub-packages

---
+++ automod/capture/doc.go
@@ -0,0 +1,4 @@
+// Automod development helpers for fetching and saving snapshots of real-world content and metadata.
+//
+// NOTE: real-world content is sensitive, even if it is abusive.
+package capture
---
+++ automod/setstore/doc.go
@@ -0,0 +1,2 @@
+// Interface for simple sets of strings, with fast inclusion checks.
+package setstore
