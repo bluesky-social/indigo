a90b8b15f655a29aee875eeb79986a517bd40e01
bryan newbold <bnewbold@robocracy.org>
2024-01-12 15:16:58 -0800

    initial pass at blob fetching and rules in automod

--- automod/engine/ruletypes.go
+++ automod/engine/ruletypes.go
@@ -2,9 +2,11 @@ package engine
 
 import (
 	appbsky "github.com/bluesky-social/indigo/api/bsky"
+	lexutil "github.com/bluesky-social/indigo/lex/util"
 )
 
 type IdentityRuleFunc = func(c *AccountContext) error
 type RecordRuleFunc = func(c *RecordContext) error
 type PostRuleFunc = func(c *RecordContext, post *appbsky.FeedPost) error
 type ProfileRuleFunc = func(c *RecordContext, profile *appbsky.ActorProfile) error
+type BlobRuleFunc = func(c *RecordContext, blob lexutil.LexBlob, data []byte) error
