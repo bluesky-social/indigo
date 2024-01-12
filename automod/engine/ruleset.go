145001379a942829271bcd1b42ba4e11753e441c
bryan newbold <bnewbold@robocracy.org>
2024-01-12 15:16:58 -0800

    initial pass at blob fetching and rules in automod

--- automod/engine/ruleset.go
+++ automod/engine/ruleset.go
@@ -12,6 +12,7 @@ type RuleSet struct {
 	RecordRules       []RecordRuleFunc
 	RecordDeleteRules []RecordRuleFunc
 	IdentityRules     []IdentityRuleFunc
+	BlobRules         []BlobRuleFunc
 }
 
 func (r *RuleSet) CallRecordRules(c *RecordContext) error {
@@ -42,6 +43,31 @@ func (r *RuleSet) CallRecordRules(c *RecordContext) error {
 		}
 		for _, f := range r.ProfileRules {
 			err := f(c, profile)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	// then blob rules, if any
+	if len(r.BlobRules) == 0 || c.RecordOp.Action != CreateOp {
+		return nil
+	}
+	blobs, err := c.Blobs()
+	if err != nil {
+		// TODO: should this really return error, or just log?
+		return err
+	}
+	if len(blobs) == 0 {
+		return nil
+	}
+	// TODO: concurrency
+	for _, blob := range blobs {
+		data, err := fetchBlob(c, blob)
+		if err != nil {
+			return err
+		}
+		for _, f := range r.BlobRules {
+			err := f(c, blob, data)
 			if err != nil {
 				return err
 			}
