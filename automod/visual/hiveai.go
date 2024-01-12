2a01c809e86791b96a18f507119e7415cf32aa4f
bryan newbold <bnewbold@robocracy.org>
2024-01-11 18:45:57 -0800

    update hive labeling to match appview auto-labeling code

--- automod/visual/hiveai.go
+++ automod/visual/hiveai.go
@@ -1,12 +1,12 @@
 package visual
 
 import (
-	"log/slog"
 	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"mime/multipart"
 	"net/http"
 
@@ -51,39 +51,105 @@ func NewHiveAILabeler(token string) HiveAILabeler {
 	}
 }
 
+// Simple direct mappings from individual classes to to labels
+//
+// hive gore and violence: https://docs.thehive.ai/docs/class-descriptions-violence-gore
+func summarizeSimpleLabels(cl []HiveAIResp_Class) []string {
+	var labels []string
+
+	for _, cls := range cl {
+		if cls.Class == "very_bloody" && cls.Score >= 0.90 {
+			labels = append(labels, "gore")
+		}
+		if cls.Class == "human_corpse" && cls.Score >= 0.90 {
+			labels = append(labels, "corpse")
+		}
+		if cls.Class == "hanging" && cls.Score >= 0.90 {
+			labels = append(labels, "corpse")
+		}
+		if cls.Class == "yes_self_harm" && cls.Score >= 0.96 {
+			labels = append(labels, "self-harm")
+		}
+	}
+	return labels
+}
+
+// Matches only one (or none) of: porn, sexual, nudity
+//
+// porn: sexual and nudity. including both explicit activity or full-frontal and suggestive/intent
+// sexual: sexually suggestive, not explicit; may include some forms of nudity
+// nudity: non-sexual nudity (eg, artistic, possibly some photographic)
+//
+// hive docs/definitions: https://docs.thehive.ai/docs/sexual-content
+func summarizeSexualLabels(cl []HiveAIResp_Class) string {
+
+	scores := make(map[string]float64)
+	for _, cls := range cl {
+		scores[cls.Class] = cls.Score
+	}
+
+	// first check if porn...
+	for _, pornClass := range []string{"yes_sexual_activity", "animal_genitalia_and_human", "yes_realistic_nsfw"} {
+		if scores[pornClass] >= 0.9 {
+			return "porn"
+		}
+	}
+	if scores["general_nsfw"] >= 0.9 {
+		// special case for some anime examples
+		if scores["animated_animal_genitalia"] >= 0.5 {
+			return "porn"
+		}
+
+		// special case for some pornographic/explicit classic drawings
+		if scores["yes_undressed"] >= 0.9 && scores["yes_sexual_activity"] >= 0.9 {
+			return "porn"
+		}
+	}
+
+	// then check for sexual suggestive (which may include nudity)...
+	for _, sexualClass := range []string{"yes_sexual_intent", "yes_sex_toy"} {
+		if scores[sexualClass] >= 0.9 {
+			return "sexual"
+		}
+	}
+	if scores["yes_undressed"] >= 0.9 {
+		// special case for bondage examples
+		if scores["yes_sex_toy"] > 0.75 {
+			return "sexual"
+		}
+	}
+
+	// then non-sexual nudity...
+	for _, nudityClass := range []string{"yes_male_nudity", "yes_female_nudity", "yes_undressed"} {
+		if scores[nudityClass] >= 0.9 {
+			return "nudity"
+		}
+	}
+
+	// then finally flag remaining "underwear" images in to sexually suggestive
+	// (after non-sexual content already labeled above)
+	for _, underwearClass := range []string{"yes_male_underwear", "yes_female_underwear"} {
+		if scores[underwearClass] >= 0.9 {
+			return "sexual"
+		}
+	}
+
+	return ""
+}
+
 func (resp *HiveAIResp) SummarizeLabels() []string {
 	var labels []string
 
 	for _, status := range resp.Status {
 		for _, out := range status.Response.Output {
-			for _, cls := range out.Classes {
-				// TODO(bnewbold): lots more upstream tags could be included here.
-				// for example, "sexy" for not nude but still explicit/suggestive,
-				// or lolicon (animated, not nude, "sugggestive"
+			simple := summarizeSimpleLabels(out.Classes)
+			if len(simple) > 0 {
+				labels = append(labels, simple...)
+			}
 
-				// sexual: https://docs.thehive.ai/docs/sexual-content
-				// note: won't apply "nude" if "porn" already applied
-				if cls.Class == "yes_sexual_activity" && cls.Score >= 0.90 {
-					// NOTE: will include "hentai"
-					labels = append(labels, "porn")
-				} else if cls.Class == "animal_genitalia_and_human" && cls.Score >= 0.90 {
-					labels = append(labels, "porn")
-				} else if cls.Class == "yes_male_nudity" && cls.Score >= 0.90 {
-					labels = append(labels, "nude")
-				} else if cls.Class == "yes_female_nudity" && cls.Score >= 0.90 {
-					labels = append(labels, "nude")
-				}
-
-				// gore and violence: https://docs.thehive.ai/docs/class-descriptions-violence-gore
-				if cls.Class == "very_bloody" && cls.Score >= 0.90 {
-					labels = append(labels, "gore")
-				}
-				if cls.Class == "human_corpse" && cls.Score >= 0.90 {
-					labels = append(labels, "corpse")
-				}
-				if cls.Class == "yes_self_harm" && cls.Score >= 0.90 {
-					labels = append(labels, "self-harm")
-				}
+			sexual := summarizeSexualLabels(out.Classes)
+			if sexual != "" {
+				labels = append(labels, sexual)
 			}
 		}
 	}
