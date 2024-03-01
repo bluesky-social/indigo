b83aca0d4f12743851d328cfe30b493ccce586e4
Jaz Volpert <ericvolp12@gmail.com>
2024-03-01 21:24:52 +0000

    Support searching for quotes of a post when you see a bsky.app post URL in a search term

--- search/parse_query.go
+++ search/parse_query.go
@@ -25,6 +25,7 @@ func ParseQuery(ctx context.Context, dir identity.Directory, raw string) (string
 	for _, p := range parts {
 		p = strings.Trim(p, "\"")
 
+		// Hashtag Parsing
 		if strings.HasPrefix(p, "#") && len(p) > 1 {
 			filters = append(filters, map[string]interface{}{
 				"term": map[string]interface{}{
@@ -36,29 +37,38 @@ func ParseQuery(ctx context.Context, dir identity.Directory, raw string) (string
 			})
 			continue
 		}
+
+		// DID Parsing
 		if strings.HasPrefix(p, "did:") {
 			filters = append(filters, map[string]interface{}{
 				"term": map[string]interface{}{"did": p},
 			})
 			continue
 		}
+
+		// Explicit Handle/DID Parsing
 		if strings.HasPrefix(p, "from:") && len(p) > 6 {
-			handle, err := syntax.ParseHandle(p[5:])
-			if err != nil {
-				keep = append(keep, p)
+			did, err := identifierToDID(ctx, dir, p[5:])
+			if nil == err {
+				filters = append(filters, map[string]interface{}{
+					"term": map[string]interface{}{"from": did.String()},
+				})
 				continue
 			}
-			id, err := dir.LookupHandle(ctx, handle)
-			if err != nil {
-				if err != identity.ErrHandleNotFound {
-					slog.Error("failed to resolve handle", "err", err)
-				}
-				continue
-			}
-			filters = append(filters, map[string]interface{}{
-				"term": map[string]interface{}{"did": id.DID.String()},
-			})
+			slog.Error("failed to parse identifier in from: clause", "error", err)
 			continue
+		}
+
+		// Bsky URL Parsing (for quote posts)
+		if strings.HasPrefix(p, "https://bsky.app/profile/") {
+			atURI, err := bskyURLtoATURI(ctx, dir, p)
+			if nil == err {
+				filters = append(filters, map[string]interface{}{
+					"term": map[string]interface{}{"embed_aturi": atURI.String()},
+				})
+				continue
+			}
+			slog.Error("failed to parse bsky URL", "error", err)
 		}
 
 		keep = append(keep, p)
@@ -84,4 +94,57 @@ func ParseQuery(ctx context.Context, dir identity.Directory, raw string) (string
 		out = "*"
 	}
 	return out, filters
+}
+
+func identifierToDID(ctx context.Context, dir identity.Directory, term string) (*syntax.DID, error) {
+	atID, err := syntax.ParseAtIdentifier(term)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse identifier: %w", err)
+	}
+
+	if atID.IsDID() {
+		asDID, _ := atID.AsDID()
+		return &asDID, nil
+	}
+
+	if atID.IsHandle() {
+		asHandle, _ := atID.AsHandle()
+		id, err := dir.LookupHandle(ctx, asHandle)
+		if err != nil {
+			return nil, fmt.Errorf("failed to resolve handle: %w", err)
+		}
+		return &id.DID, nil
+	}
+
+	return nil, fmt.Errorf("unexpectedly reached bottom of parsing: %s", term)
+}
+
+func bskyURLtoATURI(ctx context.Context, dir identity.Directory, term string) (*syntax.ATURI, error) {
+	// split up the URL
+	parts := strings.Split(term, "/")
+	if len(parts) < 7 {
+		return nil, fmt.Errorf("Post URL missing parts: %s", term)
+	}
+
+	if parts[3] != "profile" && parts[5] != "post" {
+		return nil, fmt.Errorf("unexpected URL pattern: not of the form /profile/<ident>/post/<rkey>")
+	}
+
+	rkey, err := syntax.ParseRecordKey(parts[6])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse record key: %w", err)
+	}
+
+	did, err := identifierToDID(ctx, dir, parts[4])
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse identifier: %w", err)
+	}
+
+	atURI, err := syntax.ParseATURI(fmt.Sprintf("at://%s/app.bsky.feed.post/%s", did, rkey))
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct ATURI: %w", err)
+	}
+
+	return &atURI, nil
+
 }
