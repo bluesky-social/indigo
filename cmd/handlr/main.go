8e9a05f0458e46d4a41b3f750de841ff63cbb0e0
bryan newbold <bnewbold@robocracy.org>
2024-05-04 15:46:42 -0700

    flesh out handlr service

--- cmd/handlr/main.go
+++ cmd/handlr/main.go
@@ -1,130 +1,106 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"log"
 	"log/slog"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/bluesky-social/indigo/atproto/syntax"
-	"github.com/miekg/dns"
+	"github.com/hashicorp/golang-lru/v2/expirable"
+	_ "github.com/joho/godotenv/autoload"
+	cli "github.com/urfave/cli/v2"
 )
 
-type HTTPResolver struct {
-	client      *http.Client
-	backendHost string
-	suffix      string
-	ttl         int
-	// TODO: expiring LRU cache
-}
-
-type ResolveResp struct {
-	DID string `json:"did"`
-}
-
 func main() {
+	if err := run(os.Args); err != nil {
+		slog.Error("exiting", "err", err)
+		os.Exit(-1)
+	}
+}
 
-	bind := ":5333"
+func run(args []string) error {
+
+	app := cli.App{
+		Name:  "handlr",
+		Usage: "atproto handle DNS TXT proxy demon",
+	}
+
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:    "bind",
+			Usage:   "local UDP IP and port to listen on. note that DNS port 53 requires superuser on most systems",
+			Value:   ":5333",
+			EnvVars: []string{"HANDLR_BIND"},
+		},
+		&cli.StringFlag{
+			Name:    "backend-host",
+			Usage:   "HTTP method, hostname, and port of backend resolution service",
+			Value:   "http://localhost:5000",
+			EnvVars: []string{"HANDLR_BACKEND_HOST"},
+		},
+		&cli.StringFlag{
+			Name:    "domain-suffix",
+			Usage:   "domain suffix to filter against",
+			EnvVars: []string{"HANDLR_DOMAIN_SUFFIX"},
+		},
+		&cli.IntFlag{
+			Name:    "ttl",
+			Usage:   "TTL for both DNS TXT responses, and internal caching",
+			Value:   5 * 60,
+			EnvVars: []string{"HANDLR_TTL"},
+		},
+		&cli.StringFlag{
+			Name:    "log-level",
+			Usage:   "log level (debug, info, warn, error)",
+			Value:   "info",
+			EnvVars: []string{"HANDLR_LOG_LEVEL", "LOG_LEVEL"},
+		},
+	}
+	app.Commands = []*cli.Command{
+		&cli.Command{
+			Name:   "serve",
+			Usage:  "run the handlr daemon",
+			Action: runServe,
+			Flags:  flags,
+		},
+	}
+	return app.Run(args)
+}
+
+func runServe(cctx *cli.Context) error {
+
+	logLevel := slog.LevelInfo
+	switch strings.ToLower(cctx.String("log-level")) {
+	case "debug":
+		logLevel = slog.LevelDebug
+	case "info":
+		logLevel = slog.LevelInfo
+	case "warn":
+		logLevel = slog.LevelWarn
+	case "error":
+		logLevel = slog.LevelError
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     logLevel,
+		AddSource: true,
+	}))
+	slog.SetDefault(logger)
+
+	// set a minimum for the internal cache
+	var cache *expirable.LRU[syntax.Handle, syntax.DID]
+	ttl := cctx.Int("ttl")
+	if ttl != 0 {
+		cache = expirable.NewLRU[syntax.Handle, syntax.DID](10_000, nil, time.Second*time.Duration(ttl))
+	}
 	client := http.Client{Timeout: time.Second * 5}
 	hr := HTTPResolver{
 		client:      &client,
-		backendHost: "https://public.api.bsky.app",
-		suffix:      "",
-		ttl:         60 * 60 * 12, // TODO: configurable TTL?
+		backendHost: cctx.String("backend-host"),
+		suffix:      cctx.String("domain-suffix"),
+		ttl:         cctx.Int("ttl"),
+		cache:       cache,
 	}
-
-	srv := &dns.Server{Addr: bind, Net: "udp"}
-
-	dns.HandleFunc(".", hr.handleTXT)
-	slog.Info("listening on UDP", "bind", bind, "backendHost", hr.backendHost, "ttl", hr.ttl, "suffix", hr.suffix)
-	log.Fatal(srv.ListenAndServe())
-}
-
-func (hr *HTTPResolver) parseDomain(domain string) (syntax.Handle, error) {
-	if !strings.HasPrefix(domain, "_atproto.") {
-		return "", fmt.Errorf("missing _atproto prefix")
-	}
-	domain = strings.TrimPrefix(domain, "_atproto.")
-	domain = strings.TrimSuffix(domain, ".")
-	if hr.suffix != "" && !strings.HasSuffix(domain, hr.suffix) {
-		return "", fmt.Errorf("does not have required suffix: %s", hr.suffix)
-	}
-	return syntax.ParseHandle(domain)
-}
-
-func (hr *HTTPResolver) resolveHandle(hdl syntax.Handle) (syntax.DID, error) {
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s/xrpc/com.atproto.identity.resolveHandle?handle=%s", hr.backendHost, hdl), nil)
-	if err != nil {
-		return "", err
-	}
-	req.Header.Set("User-Agent", "indigo-handlr")
-	resp, err := hr.client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == 404 {
-		return "", nil
-	}
-	if resp.StatusCode != 200 {
-		return "", fmt.Errorf("failed to resolve handle status=%d", resp.StatusCode)
-	}
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return "", err
-	}
-	var resolveResp ResolveResp
-	if err = json.Unmarshal(bodyBytes, &resolveResp); err != nil {
-		return "", err
-	}
-	return syntax.ParseDID(resolveResp.DID)
-}
-
-func (hr *HTTPResolver) handleTXT(w dns.ResponseWriter, r *dns.Msg) {
-	//fmt.Printf("%s", r)
-	msg := dns.Msg{}
-	msg.SetReply(r)
-	if len(r.Question) == 0 {
-		w.WriteMsg(&msg)
-		return
-	}
-	// TODO: what about multiple questions?
-	switch r.Question[0].Qtype {
-	case dns.TypeTXT:
-		msg.Authoritative = true // TODO: configurable?
-		domain := msg.Question[0].Name
-		hdl, err := hr.parseDomain(domain)
-		slog.Warn("DNS TXT request", "domain", domain, "handle", hdl, "parseErr", err)
-		if err != nil {
-			w.WriteMsg(&msg)
-			return
-		}
-		did, err := hr.resolveHandle(hdl)
-		if err != nil {
-			slog.Error("error resolving handle", "handle", hdl, "err", err)
-			msg.SetRcode(r, dns.RcodeServerFailure)
-			w.WriteMsg(&msg)
-			return
-		}
-		if did == "" {
-			// not found: NXDOMAIN
-			msg.SetRcode(r, dns.RcodeNameError)
-			w.WriteMsg(&msg)
-			return
-		}
-		msg.Answer = append(msg.Answer, &dns.TXT{
-			Hdr: dns.RR_Header{
-				Name:   domain,
-				Rrtype: dns.TypeTXT,
-				Class:  dns.ClassINET,
-				Ttl:    uint32(hr.ttl),
-			},
-			Txt: []string{"did=" + did.String()},
-		})
-	}
-	w.WriteMsg(&msg)
+	return hr.Run(cctx.String("bind"))
 }
---
+++ cmd/handlr/server.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"strings"
+
+	"github.com/bluesky-social/indigo/atproto/syntax"
+	"github.com/hashicorp/golang-lru/v2/expirable"
+	"github.com/miekg/dns"
+)
+
+type HTTPResolver struct {
+	client      *http.Client
+	backendHost string
+	suffix      string
+	ttl         int
+	cache       *expirable.LRU[syntax.Handle, syntax.DID]
+}
+
+// represents JSON response from backend com.atproto.identity.resolveHandle request
+type ResolveResp struct {
+	DID string `json:"did"`
+}
+
+func (hr *HTTPResolver) Run(bind string) error {
+	srv := &dns.Server{Addr: bind, Net: "udp"}
+	dns.HandleFunc(".", hr.handleTXT)
+	slog.Info("listening on UDP", "bind", bind, "backendHost", hr.backendHost, "ttl", hr.ttl, "suffix", hr.suffix)
+	return srv.ListenAndServe()
+}
+
+func (hr *HTTPResolver) parseDomain(domain string) (syntax.Handle, error) {
+	if !strings.HasPrefix(domain, "_atproto.") {
+		return "", fmt.Errorf("missing _atproto prefix")
+	}
+	domain = strings.TrimPrefix(domain, "_atproto.")
+	domain = strings.TrimSuffix(domain, ".")
+	if hr.suffix != "" && !strings.HasSuffix(domain, hr.suffix) {
+		return "", fmt.Errorf("does not have required suffix: %s", hr.suffix)
+	}
+	return syntax.ParseHandle(domain)
+}
+
+func (hr *HTTPResolver) resolveHandle(hdl syntax.Handle) (syntax.DID, error) {
+
+	// first try cache
+	if hr.cache != nil {
+		val, ok := hr.cache.Get(hdl)
+		if ok {
+			return val, nil
+		}
+	}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/xrpc/com.atproto.identity.resolveHandle?handle=%s", hr.backendHost, hdl), nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("User-Agent", "indigo-handlr")
+	resp, err := hr.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 404 || resp.StatusCode == 400 {
+		return "", nil
+	}
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("failed to resolve handle status=%d", resp.StatusCode)
+	}
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	var resolveResp ResolveResp
+	if err = json.Unmarshal(bodyBytes, &resolveResp); err != nil {
+		return "", err
+	}
+	did, err := syntax.ParseDID(resolveResp.DID)
+	if err != nil {
+		return "", err
+	}
+	if hr.cache != nil {
+		hr.cache.Add(hdl, did)
+	}
+	return did, nil
+}
+
+func (hr *HTTPResolver) handleTXT(w dns.ResponseWriter, r *dns.Msg) {
+	//fmt.Printf("%s", r)
+	msg := dns.Msg{}
+	msg.SetReply(r)
+	if len(r.Question) == 0 {
+		w.WriteMsg(&msg)
+		return
+	}
+	// TODO: what about multiple questions?
+	switch r.Question[0].Qtype {
+	case dns.TypeTXT:
+		msg.Authoritative = true // TODO: configurable?
+		domain := msg.Question[0].Name
+		hdl, err := hr.parseDomain(domain)
+		slog.Debug("DNS TXT request", "domain", domain, "handle", hdl, "parseErr", err)
+		if err != nil {
+			w.WriteMsg(&msg)
+			return
+		}
+		did, err := hr.resolveHandle(hdl)
+		if err != nil {
+			slog.Error("error resolving handle", "handle", hdl, "err", err)
+			msg.SetRcode(r, dns.RcodeServerFailure)
+			w.WriteMsg(&msg)
+			return
+		}
+		if did == "" {
+			// not found: NXDOMAIN
+			msg.SetRcode(r, dns.RcodeNameError)
+			w.WriteMsg(&msg)
+			return
+		}
+		msg.Answer = append(msg.Answer, &dns.TXT{
+			Hdr: dns.RR_Header{
+				Name:   domain,
+				Rrtype: dns.TypeTXT,
+				Class:  dns.ClassINET,
+				Ttl:    uint32(hr.ttl),
+			},
+			Txt: []string{"did=" + did.String()},
+		})
+	}
+	w.WriteMsg(&msg)
+}
