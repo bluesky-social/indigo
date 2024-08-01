0a9dcf542c389cd0f3487d8364c3061b5b8048ac
bryan newbold <bnewbold@robocracy.org>
2024-08-01 15:00:51 -0700

    clean up account createdAt hydration, and add a helper

--- automod/rules/helpers_test.go
+++ automod/rules/helpers_test.go
@@ -2,7 +2,11 @@ package rules
 
 import (
 	"testing"
+	"time"
 
+	"github.com/bluesky-social/indigo/atproto/identity"
+	"github.com/bluesky-social/indigo/atproto/syntax"
+	"github.com/bluesky-social/indigo/automod"
 	"github.com/bluesky-social/indigo/automod/keyword"
 	"github.com/stretchr/testify/assert"
 )
@@ -60,4 +64,45 @@ func TestHashOfString(t *testing.T) {
 
 	// hashing function should be consistent over time
 	assert.Equal("4e6f69c0e3d10992", HashOfString("dummy-value"))
+}
+
+func TestAccountIsYoungerThan(t *testing.T) {
+	assert := assert.New(t)
+
+	am := automod.AccountMeta{
+		Identity: &identity.Identity{
+			DID:    syntax.DID("did:plc:abc111"),
+			Handle: syntax.Handle("handle.example.com"),
+		},
+		Profile: automod.ProfileSummary{},
+		Private: nil,
+	}
+	now := time.Now()
+	ac := automod.AccountContext{
+		Account: am,
+	}
+	assert.False(AccountIsYoungerThan(&ac, time.Hour))
+
+	ac.Account.CreatedAt = &now
+	assert.True(AccountIsYoungerThan(&ac, time.Hour))
+
+	yesterday := time.Now().Add(-1 * time.Hour * 24)
+	ac.Account.CreatedAt = &yesterday
+	assert.False(AccountIsYoungerThan(&ac, time.Hour))
+
+	old := time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC)
+	ac.Account.CreatedAt = &old
+	assert.False(AccountIsYoungerThan(&ac, time.Hour))
+	assert.False(AccountIsYoungerThan(&ac, time.Hour*24*365*100))
+
+	future := time.Date(3000, 1, 1, 0, 0, 0, 0, time.UTC)
+	ac.Account.CreatedAt = &future
+	assert.False(AccountIsYoungerThan(&ac, time.Hour))
+
+	ac.Account.CreatedAt = nil
+	ac.Account.Private = &automod.AccountPrivate{
+		Email:     "account@example.com",
+		IndexedAt: time.Now(),
+	}
+	assert.True(AccountIsYoungerThan(&ac, time.Hour))
 }
