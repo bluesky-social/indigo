75acf80a583e50b82fb0ae0920ab99454383de24
bryan newbold <bnewbold@robocracy.org>
2024-08-01 22:20:59 -0700

    commit old initial progress on goat

---
+++ cmd/goat/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"github.com/carlmjohnson/versioninfo"
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	if err := run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v", err)
+		os.Exit(-1)
+	}
+}
+
+var authFlags = []cli.Flag{
+	&cli.StringFlag{
+		Name:    "pds-host",
+		Usage:   "method, hostname, and port of PDS instance",
+		Value:   "http://localhost:4849",
+		EnvVars: []string{"ATP_PDS_HOST"},
+	},
+	&cli.StringFlag{
+		Name:     "admin-password",
+		Usage:    "admin authentication password for PDS",
+		Required: true,
+		EnvVars:  []string{"ATP_AUTH_ADMIN_PASSWORD"},
+	},
+	&cli.StringFlag{
+		Name:     "handle",
+		Usage:    "for PDS login",
+		Required: true,
+		EnvVars:  []string{"ATP_AUTH_HANDLE"},
+	},
+	&cli.StringFlag{
+		Name:     "password",
+		Usage:    "for PDS login",
+		Required: true,
+		EnvVars:  []string{"ATP_AUTH_PASSWORD"},
+	},
+}
+
+func run(args []string) error {
+
+	app := cli.App{
+		Name:    "goat",
+		Usage:   "golang atproto CLI tool",
+		Version: versioninfo.Short(),
+	}
+	app.Commands = []*cli.Command{
+		cmdGet,
+		cmdResolve,
+		//cmdAccount,
+	}
+	return app.Run(args)
+}
