666dc8fca3043e9d325cd801ebdbfb4697710032
bryan newbold <bnewbold@robocracy.org>
2024-08-11 11:39:08 -0700

    goat progress

---
+++ cmd/goat/status.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/urfave/cli/v2"
+)
+
+var cmdStatus = &cli.Command{
+	Name:   "status",
+	Usage:  "check auth session",
+	Flags:  []cli.Flag{},
+	Action: runStatus,
+}
+
+func runStatus(cctx *cli.Context) error {
+	ctx := context.Background()
+
+	client, err := loadAuthClient(ctx)
+	if err == ErrNoAuthSession {
+	} else if err != nil {
+		return err
+	}
+	fmt.Printf("DID: %s\n", client.Auth.Did)
+	fmt.Printf("PDS: %s\n", client.Host)
+
+	return nil
+}
