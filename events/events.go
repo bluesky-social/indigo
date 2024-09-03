2e94168974b718ee7a7045baab109f512fb8dbed
Brian Olson <brian.olson@gmail.com>
2024-09-03 02:33:31 -0400

    serialize outbound once

--- events/events.go
+++ events/events.go
@@ -1,17 +1,21 @@
 package events
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"sync"
 	"time"
 
 	comatproto "github.com/bluesky-social/indigo/api/atproto"
+	lexutil "github.com/bluesky-social/indigo/lex/util"
 	"github.com/bluesky-social/indigo/models"
 	"github.com/prometheus/client_golang/prometheus"
 
 	logging "github.com/ipfs/go-log"
+	cbg "github.com/whyrusleeping/cbor-gen"
 	"go.opentelemetry.io/otel"
 )
 
@@ -63,6 +67,9 @@ func (em *EventManager) Shutdown(ctx context.Context) error {
 func (em *EventManager) broadcastEvent(evt *XRPCStreamEvent) {
 	em.subsLk.Lock()
 	defer em.subsLk.Unlock()
+
+	// the main thing we do is send it out, so MarshalCBOR once
+	evt.Preserialize()
 
 	// TODO: for a larger fanout we should probably have dedicated goroutines
 	// for subsets of the subscriber set, and tiered channels to distribute
@@ -165,6 +172,57 @@ type XRPCStreamEvent struct {
 	PrivUid         models.Uid `json:"-" cborgen:"-"`
 	PrivPdsId       uint       `json:"-" cborgen:"-"`
 	PrivRelevantPds []uint     `json:"-" cborgen:"-"`
+	Preserialized   []byte     `json:"-" cborgen:"-"`
+}
+
+func (evt *XRPCStreamEvent) Serialize(wc io.Writer) error {
+	header := EventHeader{Op: EvtKindMessage}
+	var obj lexutil.CBOR
+
+	switch {
+	case evt.Error != nil:
+		header.Op = EvtKindErrorFrame
+		obj = evt.Error
+	case evt.RepoCommit != nil:
+		header.MsgType = "#commit"
+		obj = evt.RepoCommit
+	case evt.RepoHandle != nil:
+		header.MsgType = "#handle"
+		obj = evt.RepoHandle
+	case evt.RepoIdentity != nil:
+		header.MsgType = "#identity"
+		obj = evt.RepoIdentity
+	case evt.RepoAccount != nil:
+		header.MsgType = "#account"
+		obj = evt.RepoAccount
+	case evt.RepoInfo != nil:
+		header.MsgType = "#info"
+		obj = evt.RepoInfo
+	case evt.RepoMigrate != nil:
+		header.MsgType = "#migrate"
+		obj = evt.RepoMigrate
+	case evt.RepoTombstone != nil:
+		header.MsgType = "#tombstone"
+		obj = evt.RepoTombstone
+	default:
+		return fmt.Errorf("unrecognized event kind")
+	}
+
+	cborWriter := cbg.NewCborWriter(wc)
+	if err := header.MarshalCBOR(cborWriter); err != nil {
+		return fmt.Errorf("failed to write header: %w", err)
+	}
+	return obj.MarshalCBOR(cborWriter)
+}
+
+// serialize content into Preserialized cache
+func (evt *XRPCStreamEvent) Preserialize() {
+	if evt.Preserialized != nil {
+		return
+	}
+	var buf bytes.Buffer
+	evt.Serialize(&buf)
+	evt.Preserialized = buf.Bytes()
 }
 
 type ErrorFrame struct {
