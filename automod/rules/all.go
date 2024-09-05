a0e238967bfce7524b669d82d67e62d79c308912
bryan newbold <bnewbold@robocracy.org>
2024-09-04 21:02:38 -0700

    add trivial spam text rule

--- automod/rules/all.go
+++ automod/rules/all.go
@@ -27,6 +27,7 @@ func DefaultRules() automod.RuleSet {
 			HarassmentTargetInteractionPostRule,
 			HarassmentTrivialPostRule,
 			NostrSpamPostRule,
+			TrivialSpamPostRule,
 		},
 		ProfileRules: []automod.ProfileRuleFunc{
 			GtubeProfileRule,
