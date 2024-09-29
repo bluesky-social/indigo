5a876ccd660e1aa0aeb7e03d35ca1d686474823c
bryan newbold <bnewbold@robocracy.org>
2024-09-29 16:55:11 -0700

    more account helpers

--- cmd/goat/account.go
+++ cmd/goat/account.go
@@ -68,6 +68,12 @@ var cmdAccount = &cli.Command{
 			Action:    runAccountLookup,
 		},
 		&cli.Command{
+			Name:      "update-handle",
+			Usage:     "change handle for current account",
+			ArgsUsage: `<handle>`,
+			Action:    runAccountUpdateHandle,
+		},
+		&cli.Command{
 			Name:   "status",
 			Usage:  "show current account status at PDS",
 			Action: runAccountStatus,
@@ -76,6 +82,74 @@ var cmdAccount = &cli.Command{
 			Name:   "missing-blobs",
 			Usage:  "list any missing blobs for current account",
 			Action: runAccountMissingBlobs,
+		},
+		&cli.Command{
+			Name:  "service-auth",
+			Usage: "create service auth token",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:    "endpoint",
+					Aliases: []string{"lxm"},
+					Usage:   "restrict token to API endpoint (NSID, optional)",
+				},
+				&cli.StringFlag{
+					Name:     "audience",
+					Aliases:  []string{"aud"},
+					Required: true,
+					Usage:    "DID of service that will receive and validate token",
+				},
+				&cli.IntFlag{
+					Name:  "duration-sec",
+					Value: 60,
+					Usage: "validity time window of token (seconds)",
+				},
+			},
+			Action: runAccountServiceAuth,
+		},
+		&cli.Command{
+			Name:  "create",
+			Usage: "create a new account on the indicated PDS host",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:     "pds-host",
+					Usage:    "URL of the PDS to create account on",
+					Required: true,
+					EnvVars:  []string{"ATP_PDS_HOST"},
+				},
+				&cli.StringFlag{
+					Name:     "handle",
+					Usage:    "handle for new account",
+					Required: true,
+					EnvVars:  []string{"ATP_AUTH_HANDLE"},
+				},
+				&cli.StringFlag{
+					Name:     "password",
+					Usage:    "initial account password",
+					Required: true,
+					EnvVars:  []string{"ATP_AUTH_PASSWORD"},
+				},
+				&cli.StringFlag{
+					Name:  "invite-code",
+					Usage: "invite code for account signup",
+				},
+				&cli.StringFlag{
+					Name:  "email",
+					Usage: "email address for new account",
+				},
+				&cli.StringFlag{
+					Name:  "existing-did",
+					Usage: "an existing DID to use (eg, non-PLC DID, or migration)",
+				},
+				&cli.StringFlag{
+					Name:  "recovery-key",
+					Usage: "public cryptographic key (did:key) to add as PLC recovery",
+				},
+				&cli.StringFlag{
+					Name:  "service-auth",
+					Usage: "service auth token (for account migration)",
+				},
+			},
+			Action: runAccountCreate,
 		},
 		cmdAccountMigrate,
 	},
@@ -151,6 +225,8 @@ func runAccountStatus(cctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
+	fmt.Printf("DID: %s\n", client.Auth.Did)
+	fmt.Printf("Host: %s\n", client.Host)
 	fmt.Println(string(b))
 
 	return nil
@@ -217,5 +293,139 @@ func runAccountDeactivate(cctx *cli.Context) error {
 		return fmt.Errorf("failed deactivating account: %w", err)
 	}
 
+	return nil
+}
+
+func runAccountUpdateHandle(cctx *cli.Context) error {
+	ctx := context.Background()
+
+	raw := cctx.Args().First()
+	if raw == "" {
+		return fmt.Errorf("need to provide new handle as argument")
+	}
+	handle, err := syntax.ParseHandle(raw)
+	if err != nil {
+		return err
+	}
+
+	client, err := loadAuthClient(ctx)
+	if err == ErrNoAuthSession {
+		return fmt.Errorf("auth required, but not logged in")
+	} else if err != nil {
+		return err
+	}
+
+	err = comatproto.IdentityUpdateHandle(ctx, client, &comatproto.IdentityUpdateHandle_Input{
+		Handle: handle.String(),
+	})
+	if err != nil {
+		return fmt.Errorf("failed updating handle: %w", err)
+	}
+
+	return nil
+}
+
+func runAccountServiceAuth(cctx *cli.Context) error {
+	ctx := context.Background()
+
+	client, err := loadAuthClient(ctx)
+	if err == ErrNoAuthSession {
+		return fmt.Errorf("auth required, but not logged in")
+	} else if err != nil {
+		return err
+	}
+
+	lxm := cctx.String("endpoint")
+	if lxm != "" {
+		_, err := syntax.ParseNSID(lxm)
+		if err != nil {
+			return fmt.Errorf("lxm argument must be a valid NSID: %w", err)
+		}
+	}
+
+	aud := cctx.String("audience")
+	// TODO: can aud DID have a fragment?
+	_, err = syntax.ParseDID(aud)
+	if err != nil {
+		return fmt.Errorf("aud argument must be a valid DID: %w", err)
+	}
+
+	durSec := cctx.Int("duration-sec")
+	expTimestamp := time.Now().Unix() + int64(durSec)
+
+	resp, err := comatproto.ServerGetServiceAuth(ctx, client, aud, expTimestamp, lxm)
+	if err != nil {
+		return fmt.Errorf("failed updating handle: %w", err)
+	}
+
+	fmt.Println(resp.Token)
+
+	return nil
+}
+
+func runAccountCreate(cctx *cli.Context) error {
+	ctx := context.Background()
+
+	// validate args
+	pdsHost := cctx.String("pds-host")
+	if !strings.Contains(pdsHost, "://") {
+		return fmt.Errorf("PDS host is not a url: %s", pdsHost)
+	}
+	handle := cctx.String("handle")
+	_, err := syntax.ParseHandle(handle)
+	if err != nil {
+		return err
+	}
+	password := cctx.String("password")
+	params := &comatproto.ServerCreateAccount_Input{
+		Handle:   handle,
+		Password: &password,
+	}
+	raw := cctx.String("existing-did")
+	if raw != "" {
+		_, err := syntax.ParseDID(raw)
+		if err != nil {
+			return err
+		}
+		s := raw
+		params.Did = &s
+	}
+	raw = cctx.String("email")
+	if raw != "" {
+		s := raw
+		params.Email = &s
+	}
+	raw = cctx.String("invite-code")
+	if raw != "" {
+		s := raw
+		params.InviteCode = &s
+	}
+	raw = cctx.String("recovery-key")
+	if raw != "" {
+		s := raw
+		params.RecoveryKey = &s
+	}
+
+	// create a new API client to connect to the account's PDS
+	xrpcc := xrpc.Client{
+		Host: pdsHost,
+	}
+
+	raw = cctx.String("service-auth")
+	if raw != "" && params.Did != nil {
+		xrpcc.Auth = &xrpc.AuthInfo{
+			Did: *params.Did,
+			AccessJwt: raw,
+		}
+	}
+
+	resp, err := comatproto.ServerCreateAccount(ctx, &xrpcc, params)
+	if err != nil {
+		return fmt.Errorf("failed to create account: %w", err)
+	}
+
+	fmt.Println("Success!")
+	fmt.Printf("DID: %s\n", resp.Did)
+	fmt.Printf("Handle: %s\n", resp.Handle)
 	return nil
 }
