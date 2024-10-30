0ed95120d0188d19a83dce1e2c562e8aba9ab2c9
bryan newbold <bnewbold@robocracy.org>
2024-10-29 17:37:54 -0700

    add gtube tags for testing

--- automod/rules/gtube.go
+++ automod/rules/gtube.go
@@ -16,6 +16,7 @@ func GtubePostRule(c *automod.RecordContext, post *appbsky.FeedPost) error {
 	if strings.Contains(post.Text, gtubeString) {
 		c.AddRecordLabel("spam")
 		c.Notify("slack")
+		c.AddRecordTag("gtube-record")
 	}
 	return nil
 }
@@ -26,6 +27,7 @@ func GtubeProfileRule(c *automod.RecordContext, profile *appbsky.ActorProfile) e
 	if profile.Description != nil && strings.Contains(*profile.Description, gtubeString) {
 		c.AddRecordLabel("spam")
 		c.Notify("slack")
+		c.AddAccountTag("gtuber-account")
 	}
 	return nil
 }
