8fa7f3f48e57f5182545d064a657fa7fb437b65a
Brian Olson <brian.olson@gmail.com>
2024-11-15 09:27:35 -0500

    add gauge spl_active_clients

--- splitter/metrics.go
+++ splitter/metrics.go
@@ -9,3 +9,8 @@ var eventsSentCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "spl_events_sent_counter",
 	Help: "The total number of events sent to consumers",
 }, []string{"remote_addr", "user_agent"})
+
+var activeClientGauge = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "spl_active_clients",
+	Help: "Current number of active clients",
+})
