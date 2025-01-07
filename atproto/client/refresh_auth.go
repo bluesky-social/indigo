2d2ccecf77de21702beddb15e4e87e8bee60b941
bryan newbold <bnewbold@robocracy.org>
2025-01-06 22:21:38 -0800

    sketch API notes

---
+++ atproto/client/refresh_auth.go
@@ -0,0 +1,31 @@
+package client
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/bluesky-social/indigo/atproto/syntax"
+)
+
+type RefreshAuth struct {
+	AccessToken  string
+	RefreshToken string
+	DID          syntax.DID
+	// The AuthHost might different from any APIClient host, if there is an entryway involved
+	AuthHost	string
+}
+
+// TODO:
+//func NewRefreshAuth(pdsHost, accountIdentifier, password string) (*RefreshAuth, error) {
+
+func (a *RefreshAuth) DoWithAuth(ctx context.Context, httpReq *http.Request, httpClient *http.Client) (*http.Response, error) {
+	httpReq.Header.Set("Authorization", "Bearer "+a.AccessToken)
+	// XXX: check response. if it is 403, because access token is expired, then take a lock and do a refresh
+	// TODO: when doing a refresh request, copy at least the User-Agent header from httpReq, and re-use httpClient
+	return httpClient.Do(httpReq)
+}
+
+// Admin bearer token auth does not involve an account DID
+func (a *RefreshAuth) AccountDID() syntax.DID {
+	return a.DID
+}
