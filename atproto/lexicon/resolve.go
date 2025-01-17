ad991e41b4581e43cde245d5aca72df62639cb69
bryan newbold <bnewbold@robocracy.org>
2025-01-17 12:16:47 -0800

    refactor lexicon record fetching a bit

--- atproto/lexicon/resolve.go
+++ atproto/lexicon/resolve.go
@@ -2,6 +2,7 @@ package lexicon
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 
@@ -19,6 +20,36 @@ import (
 // Calling code should usually use ResolvingCatalog, which handles basic caching and validation of the Lexicon language itself.
 func ResolveLexiconData(ctx context.Context, dir identity.Directory, nsid syntax.NSID) (map[string]any, error) {
 
+	record, err := resolveLexiconJSON(ctx, dir, nsid)
+	if err != nil {
+		return nil, err
+	}
+
+	d, err := data.UnmarshalJSON(*record)
+	if err != nil {
+		return nil, fmt.Errorf("fetched Lexicon schema record was invalid: %w", err)
+	}
+	return d, nil
+}
+
+// Low-level routine for resolving an NSID to `SchemaFile`.
+//
+// Same as `ResolveLexiconData`, but returns a parsed `SchemaFile` struct.
+func ResolveLexiconSchemaFile(ctx context.Context, dir identity.Directory, nsid syntax.NSID) (*SchemaFile, error) {
+	record, err := resolveLexiconJSON(ctx, dir, nsid)
+	if err != nil {
+		return nil, err
+	}
+
+	var sf SchemaFile
+	if err := json.Unmarshal(*record, &sf); err != nil {
+		return nil, fmt.Errorf("fetched Lexicon schema record was invalid: %w", err)
+	}
+	return &sf, nil
+}
+
+// internal helper for fetching lexicon record as JSON bytes
+func resolveLexiconJSON(ctx context.Context, dir identity.Directory, nsid syntax.NSID) (*json.RawMessage, error) {
 	baseDir := identity.BaseDirectory{}
 	did, err := baseDir.ResolveNSID(ctx, nsid)
 	if err != nil {
@@ -32,14 +63,14 @@ func ResolveLexiconData(ctx context.Context, dir identity.Directory, nsid syntax
 	}
 
 	aturi := syntax.ATURI(fmt.Sprintf("at://%s/com.atproto.lexicon.schema/%s", did, nsid))
-	record, err := fetchRecord(ctx, *ident, aturi)
+	msg, err := fetchRecordJSON(ctx, *ident, aturi)
 	if err != nil {
 		return nil, err
 	}
-	return record, nil
+	return msg, err
 }
 
-func fetchRecord(ctx context.Context, ident identity.Identity, aturi syntax.ATURI) (map[string]any, error) {
+func fetchRecordJSON(ctx context.Context, ident identity.Identity, aturi syntax.ATURI) (*json.RawMessage, error) {
 
 	slog.Debug("fetching record", "did", ident.DID.String(), "collection", aturi.Collection().String(), "rkey", aturi.RecordKey().String())
 	xrpcc := xrpc.Client{
@@ -53,10 +84,6 @@ func fetchRecord(ctx context.Context, ident identity.Identity, aturi syntax.ATUR
 	if nil == resp.Value {
 		return nil, fmt.Errorf("empty record in response")
 	}
-	record, err := data.UnmarshalJSON(*resp.Value)
-	if err != nil {
-		return nil, fmt.Errorf("fetched record was invalid data: %w", err)
-	}
 
-	return record, nil
+	return resp.Value, nil
 }
