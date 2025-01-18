454210764bcf5042b877fd0d710b27e89549941f
bryan newbold <bnewbold@robocracy.org>
2025-01-18 14:47:21 -0800

    progress; breaking up big functions

--- atproto/repo/mst/debug.go
+++ atproto/repo/mst/debug.go
@@ -1,6 +1,7 @@
 package mst
 
 import (
+	"bytes"
 	"fmt"
 	"sort"
 
@@ -21,29 +22,127 @@ func DebugPrintMap(m map[string]cid.Cid) {
 }
 
 func DebugPrintTree(n *Node, depth int) {
+	if n == nil {
+		fmt.Printf("EMPTY TREE")
+		return
+	}
 	if depth == 0 {
-		fmt.Printf("tree root\n")
+		fmt.Printf("tree root (height=%d)\n", n.Height)
 	}
 	for i, e := range n.Entries {
 		if depth > 0 && i == 0 {
-			for range depth - 1 {
-				fmt.Printf("│")
+			if len(n.Entries) > 1 {
+				fmt.Printf("┬")
+			} else {
+				fmt.Printf("─")
 			}
-			fmt.Printf("┬")
 		} else {
 			for range depth {
 				fmt.Printf("│")
 			}
-			fmt.Printf("├")
+			if i+1 == len(n.Entries) {
+				fmt.Printf("└")
+			} else {
+				fmt.Printf("├")
+			}
 		}
 		if e.IsValue() {
-			fmt.Printf(" %s -> %s (%d)\n", e.Key, e.Value, HeightForKey(e.Key))
-		} else if e.IsPointer() {
+			fmt.Printf(" (%d) %s -> %s\n", HeightForKey(e.Key), e.Key, e.Value)
+		} else if e.IsChild() {
 			if e.Child != nil {
 				DebugPrintTree(e.Child, depth+1)
 			} else {
 				fmt.Printf(" (partial) %s", e.ChildCID)
 			}
+		}
+	}
+}
+
+func DebugTreeStructure(n *Node, height int, key []byte) error {
+	if n == nil {
+		return fmt.Errorf("nil tree")
+	}
+	if n.CID == nil && n.Dirty == false {
+		return fmt.Errorf("node missing CID, but not marked dirty")
+	}
+	if len(n.Entries) == 0 {
+		return fmt.Errorf("empty tree node")
+	}
+
+	if height < 0 {
+		// do a quick pass to compute current height
+		for _, e := range n.Entries {
+			if e.IsValue() {
+				height = HeightForKey(e.Key)
+				break
+			}
+		}
+	}
+	if height < 0 {
+		return fmt.Errorf("top of tree is just a pointer to child")
+	}
+
+	lastWasChild := false
+	for _, e := range n.Entries {
+		if e.IsChild() {
+			if lastWasChild {
+				return fmt.Errorf("sibling children in entries list")
+			}
+			lastWasChild = true
+			if e.IsValue() {
+				return fmt.Errorf("entry is both a child and a value")
+			}
+			if height == 0 {
+				return fmt.Errorf("child below zero height")
+			}
+			if e.Child != nil {
+				if err := DebugTreeStructure(e.Child, height-1, key); err != nil {
+					return err
+				}
+			}
+		} else if e.IsValue() {
+			lastWasChild = false
+			if bytes.Equal(key, e.Key) {
+				return fmt.Errorf("duplicate key in tree")
+			}
+			if bytes.Compare(key, e.Key) > 0 {
+				return fmt.Errorf("out of order keys")
+			}
+			key = e.Key
+			if height != HeightForKey(e.Key) {
+				return fmt.Errorf("wrong height for key")
+			}
+		} else {
+			return fmt.Errorf("entry was neither child nor value")
+		}
+	}
+	return nil
+}
+
+func DebugCountEntries(n *Node) int {
+	if n == nil {
+		return 0
+	}
+	count := 0
+	for _, e := range n.Entries {
+		if e.IsValue() {
+			count++
+		}
+		if e.IsChild() && e.Child != nil {
+			count += DebugCountEntries(e.Child)
+		}
+	}
+	return count
+}
+
+func DebugPrintNodePointers(n *Node) {
+	fmt.Printf("%p\n", n)
+	if n == nil {
+		return
+	}
+	for _, e := range n.Entries {
+		if e.IsChild() && e.Child != nil {
+			DebugPrintNodePointers(e.Child)
 		}
 	}
 }
