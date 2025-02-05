2b4e28d57fd53295463a1b53ab82ecedad3f79f5
bryan newbold <bnewbold@robocracy.org>
2025-02-04 20:48:11 -0800

    mst: cleanup files

--- atproto/repo/mst/encoding.go
+++ atproto/repo/mst/encoding.go
@@ -9,6 +9,7 @@ import (
 	bf "github.com/ipfs/go-block-format"
 	"github.com/ipfs/go-cid"
 	blockstore "github.com/ipfs/go-ipfs-blockstore"
+	ipld "github.com/ipfs/go-ipld-format"
 	"github.com/multiformats/go-multihash"
 )
 
@@ -190,4 +191,38 @@ func writeNodeBlocks(ctx context.Context, n *Node, bs blockstore.Blockstore, onl
 		}
 	}
 	return c, nil
+}
+
+func loadNodeFromStore(ctx context.Context, bs blockstore.Blockstore, ref cid.Cid) (*Node, error) {
+	block, err := bs.Get(ctx, ref)
+	if err != nil {
+		return nil, err
+	}
+
+	nd, err := NodeDataFromCBOR(bytes.NewReader(block.RawData()))
+	if err != nil {
+		return nil, err
+	}
+
+	n := nd.Node(&ref)
+
+	for i, e := range n.Entries {
+		if e.IsChild() {
+			child, err := loadNodeFromStore(ctx, bs, *e.ChildCID)
+			if err != nil && ipld.IsNotFound(err) {
+				// allow "partial" trees
+				continue
+			}
+			if err != nil {
+				return nil, err
+			}
+			n.Entries[i].Child = child
+			// NOTE: this is kind of a hack
+			if n.Height == -1 && child.Height >= 0 {
+				n.Height = child.Height + 1
+			}
+		}
+	}
+
+	return &n, nil
 }
--- atproto/repo/mst/node.go
+++ atproto/repo/mst/node.go
@@ -240,3 +240,41 @@ func writeNodeToMap(n *Node, m map[string]cid.Cid) error {
 	}
 	return nil
 }
+
+// Reads the value (CID) corresponding to the key. If key is not in the tree, returns (nil, nil).
+//
+// n: Node at top of sub-tree to operate on. Must not be nil.
+// key: key or path being inserted. must not be empty/nil
+// height: tree height corresponding to key. if a negative value is provided, will be computed; use -1 instead of 0 if height is not known
+func nodeGet(n *Node, key []byte, height int) (*cid.Cid, error) {
+	if height < 0 {
+		height = HeightForKey(key)
+	}
+
+	if height > n.Height {
+		// key from a higher layer; key was not in tree
+		return nil, nil
+	}
+
+	if height < n.Height {
+		// look for a child node
+		idx := findExistingChild(n, key)
+		if idx >= 0 {
+			if n.Entries[idx].Child == nil {
+				return nil, fmt.Errorf("could not search for key: %w", ErrPartialTree)
+			}
+			return nodeGet(n.Entries[idx].Child, key, height)
+		}
+		// otherwise, not found
+		return nil, nil
+	}
+
+	// search at this height
+	idx := findExistingEntry(n, key)
+	if idx >= 0 {
+		return n.Entries[idx].Value, nil
+	}
+
+	// not found
+	return nil, nil
+}
