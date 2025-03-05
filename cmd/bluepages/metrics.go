9faa092c2ec48566beab7cc416cf95e2b32f9f6e
bryan newbold <bnewbold@robocracy.org>
2025-03-05 12:26:08 -0800

    use separate metric names again (for now)

--- cmd/bluepages/metrics.go
+++ cmd/bluepages/metrics.go
@@ -6,23 +6,23 @@ import (
 )
 
 var handleResolution = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "atproto_identity_resolve_handle",
+	Name: "atproto_identity_bluepages_resolve_handle",
 	Help: "ATProto handle resolutions",
 }, []string{"directory", "status"})
 
 var handleResolutionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-	Name:    "atproto_identity_resolve_handle_duration",
+	Name:    "atproto_identity_bluepages_resolve_handle_duration",
 	Help:    "Time to resolve a handle",
 	Buckets: prometheus.ExponentialBucketsRange(0.001, 2, 15),
 }, []string{"directory", "status"})
 
 var didResolution = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "atproto_identity_resolve_did",
+	Name: "atproto_identity_bluepages_resolve_did",
 	Help: "ATProto DID resolutions",
 }, []string{"directory", "status"})
 
 var didResolutionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
-	Name:    "atproto_identity_resolve_did_duration",
+	Name:    "atproto_identity_bluepages_resolve_did_duration",
 	Help:    "Time to resolve a DID",
 	Buckets: prometheus.ExponentialBucketsRange(0.001, 2, 15),
 }, []string{"directory", "status"})
