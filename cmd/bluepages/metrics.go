2f6bd1d1735fd10b9a59f380134d8ea062f12b91
bryan newbold <bnewbold@robocracy.org>
2025-03-12 14:40:45 -0700

    fix metrics buckets

--- cmd/bluepages/metrics.go
+++ cmd/bluepages/metrics.go
@@ -13,13 +13,13 @@ var handleResolution = promauto.NewCounterVec(prometheus.CounterOpts{
 var handleResolutionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "atproto_identity_bluepages_resolve_handle_duration",
 	Help:    "Time to resolve a handle",
-	Buckets: prometheus.ExponentialBucketsRange(0.0001, 2, 20),
+	Buckets: prometheus.ExponentialBuckets(0.0001, 2, 20),
 }, []string{"directory", "status"})
 
 var handleExternalResolutionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "atproto_identity_bluepages_resolve_external_handle_duration",
 	Help:    "Time to resolve a handle from live network, segmented by type",
-	Buckets: prometheus.ExponentialBucketsRange(0.0001, 2, 20),
+	Buckets: prometheus.ExponentialBuckets(0.0001, 2, 20),
 }, []string{"segment", "status"})
 
 var didResolution = promauto.NewCounterVec(prometheus.CounterOpts{
@@ -30,11 +30,11 @@ var didResolution = promauto.NewCounterVec(prometheus.CounterOpts{
 var didResolutionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "atproto_identity_bluepages_resolve_did_duration",
 	Help:    "Time to resolve a DID",
-	Buckets: prometheus.ExponentialBucketsRange(0.0001, 2, 20),
+	Buckets: prometheus.ExponentialBuckets(0.0001, 2, 20),
 }, []string{"directory", "status"})
 
 var didExternalResolutionDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 	Name:    "atproto_identity_bluepages_resolve_external_did_duration",
 	Help:    "Time to resolve a DID from live network, by method",
-	Buckets: prometheus.ExponentialBucketsRange(0.0001, 2, 20),
+	Buckets: prometheus.ExponentialBuckets(0.0001, 2, 20),
 }, []string{"method", "status"})
