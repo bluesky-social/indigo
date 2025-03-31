d3be3b0d213b7d59961cc26fb8422ff7bc9145ac
bryan newbold <bnewbold@robocracy.org>
2025-03-30 18:43:56 -0700

    move dbcid to slurper (for now)

---
+++ cmd/relayered/slurper/models_dbcid.go
@@ -0,0 +1,64 @@
+package slurper
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+
+	"github.com/ipfs/go-cid"
+)
+
+type Uid uint64
+
+type DbCID struct {
+	CID cid.Cid
+}
+
+func (dbc *DbCID) Scan(v interface{}) error {
+	b, ok := v.([]byte)
+	if !ok {
+		return fmt.Errorf("dbcids must get bytes!")
+	}
+
+	if len(b) == 0 {
+		return nil
+	}
+
+	c, err := cid.Cast(b)
+	if err != nil {
+		return err
+	}
+
+	dbc.CID = c
+	return nil
+}
+
+func (dbc DbCID) Value() (driver.Value, error) {
+	if !dbc.CID.Defined() {
+		return nil, fmt.Errorf("cannot serialize undefined cid to database")
+	}
+	return dbc.CID.Bytes(), nil
+}
+
+func (dbc DbCID) MarshalJSON() ([]byte, error) {
+	return json.Marshal(dbc.CID.String())
+}
+
+func (dbc *DbCID) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	c, err := cid.Decode(s)
+	if err != nil {
+		return err
+	}
+
+	dbc.CID = c
+	return nil
+}
+
+func (dbc *DbCID) GormDataType() string {
+	return "bytes"
+}
