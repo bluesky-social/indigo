c54795278951775a5a20dae87855b671e98d6be3
bryan newbold <bnewbold@robocracy.org>
2025-03-30 21:55:14 -0700

    back to relay pkg

--- cmd/relayered/relay/domain_ban.go
+++ cmd/relayered/relay/domain_ban.go
@@ -1,15 +1,15 @@
-package main
+package relay
 
 import (
 	"context"
 	"strings"
 
-	"github.com/bluesky-social/indigo/cmd/relayered/slurper"
+	"github.com/bluesky-social/indigo/cmd/relayered/relay/slurper"
 )
 
-// domainIsBanned checks if the given host is banned, starting with the host
+// DomainIsBanned checks if the given host is banned, starting with the host
 // itself, then checking every parent domain up to the tld
-func (s *Service) domainIsBanned(ctx context.Context, host string) (bool, error) {
+func (r *Relay) DomainIsBanned(ctx context.Context, host string) (bool, error) {
 	// ignore ports when checking for ban status
 	hostport := strings.Split(host, ":")
 
@@ -29,7 +29,7 @@ func (s *Service) domainIsBanned(ctx context.Context, host string) (bool, error)
 
 	for i := 0; i < len(segments)-1; i++ {
 		dchk := strings.Join(segments[i:], ".")
-		found, err := s.findDomainBan(ctx, dchk)
+		found, err := r.findDomainBan(ctx, dchk)
 		if err != nil {
 			return false, err
 		}
@@ -41,9 +41,9 @@ func (s *Service) domainIsBanned(ctx context.Context, host string) (bool, error)
 	return false, nil
 }
 
-func (s *Service) findDomainBan(ctx context.Context, host string) (bool, error) {
+func (r *Relay) findDomainBan(ctx context.Context, host string) (bool, error) {
 	var db slurper.DomainBan
-	if err := s.db.Find(&db, "domain = ?", host).Error; err != nil {
+	if err := r.db.Find(&db, "domain = ?", host).Error; err != nil {
 		return false, err
 	}
 
---
+++ cmd/relayered/relay/relay.go
@@ -0,0 +1,130 @@
+package relay
+
+import (
+	"log/slog"
+	"sync"
+	"time"
+
+	"github.com/bluesky-social/indigo/atproto/identity"
+	"github.com/bluesky-social/indigo/cmd/relayered/relay/slurper"
+	"github.com/bluesky-social/indigo/cmd/relayered/stream/eventmgr"
+	"github.com/bluesky-social/indigo/xrpc"
+
+	lru "github.com/hashicorp/golang-lru/v2"
+	promclient "github.com/prometheus/client_golang/prometheus"
+	"go.opentelemetry.io/otel"
+	"gorm.io/gorm"
+)
+
+var tracer = otel.Tracer("relay")
+
+type Relay struct {
+	db      *gorm.DB
+	dir     identity.Directory
+
+	Slurper *slurper.Slurper
+	Events  *eventmgr.EventManager
+	Validator *slurper.Validator
+
+	Config RelayConfig
+
+
+	// extUserLk serializes a section of syncPDSAccount()
+	// TODO: at some point we will want to lock specific DIDs, this lock as is
+	// is overly broad, but i dont expect it to be a bottleneck for now
+	extUserLk sync.Mutex
+
+	// Management of Socket Consumers
+	consumersLk    sync.RWMutex
+	nextConsumerID uint64
+	consumers      map[uint64]*SocketConsumer
+
+	// Account cache
+	userCache *lru.Cache[string, *slurper.Account]
+	Logger            *slog.Logger
+}
+
+type RelayConfig struct {
+	SSL               bool
+	DefaultRepoLimit  int64
+	ConcurrencyPerPDS int64
+	MaxQueuePerPDS    int64
+	ApplyPDSClientSettings func(c *xrpc.Client)
+}
+
+func DefaultRelayConfig() *RelayConfig {
+	return &RelayConfig{
+		SSL:               true,
+		DefaultRepoLimit:  100,
+		ConcurrencyPerPDS: 100,
+		MaxQueuePerPDS:    1_000,
+	}
+}
+
+func NewRelay(db *gorm.DB, validator *slurper.Validator, evtman *eventmgr.EventManager, dir identity.Directory, config *RelayConfig) (*Relay, error) {
+
+	if config == nil {
+		config = DefaultRelayConfig()
+	}
+
+	uc, _ := lru.New[string, *slurper.Account](1_000_000)
+
+	r := &Relay{
+		db: db,
+		Events: evtman,
+		Validator: validator,
+		dir:       dir,
+		Config: *config,
+
+		consumersLk: sync.RWMutex{},
+		consumers:   make(map[uint64]*SocketConsumer),
+
+		userCache: uc,
+
+		Logger: slog.Default().With("system", "relay"),
+	}
+
+	slOpts := slurper.DefaultSlurperOptions()
+	slOpts.SSL = config.SSL
+	slOpts.DefaultRepoLimit = config.DefaultRepoLimit
+	slOpts.ConcurrencyPerPDS = config.ConcurrencyPerPDS
+	slOpts.MaxQueuePerPDS = config.MaxQueuePerPDS
+	slOpts.Logger = r.Logger
+	s, err := slurper.NewSlurper(db, r.handleFedEvent, slOpts)
+	if err != nil {
+		return nil, err
+	}
+	r.Slurper = s
+
+	if err := r.Slurper.RestartAll(); err != nil {
+		return nil, err
+	}
+
+	if err := r.MigrateDatabase(); err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func (r *Relay) MigrateDatabase() error  {
+	if err := r.db.AutoMigrate(slurper.DomainBan{}); err != nil {
+		return err
+	}
+	if err := r.db.AutoMigrate(slurper.PDS{}); err != nil {
+		return err
+	}
+	if err := r.db.AutoMigrate(slurper.Account{}); err != nil {
+		return err
+	}
+	if err := r.db.AutoMigrate(slurper.AccountPreviousState{}); err != nil {
+		return err
+	}
+	return nil
+}
+
+type SocketConsumer struct {
+	UserAgent   string
+	RemoteAddr  string
+	ConnectedAt time.Time
+	EventsSent  promclient.Counter
+}
