89ed3650e382118c2c52cfb22cba04e2d7e9b1b4
bryan newbold <bnewbold@robocracy.org>
2025-04-03 10:45:25 -0700

    URL helpers

---
+++ cmd/relayered/relay/host_test.go
@@ -0,0 +1,62 @@
+package relay
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type HostnameFixture struct {
+	Val      string
+	Error    bool
+	Hostname string
+	NoSSL    bool
+}
+
+func TestParseHostname(t *testing.T) {
+	assert := assert.New(t)
+
+	fixtures := []HostnameFixture{
+		HostnameFixture{Val: "asdf", Error: true},
+		HostnameFixture{Val: "https://pds.example.com", Hostname: "pds.example.com", NoSSL: false},
+		HostnameFixture{Val: "http://pds.example.com", Hostname: "pds.example.com", NoSSL: true},
+		HostnameFixture{Val: "ws://pds.example.com", Hostname: "pds.example.com", NoSSL: true},
+		HostnameFixture{Val: "pds.example.com", Error: true},
+		HostnameFixture{Val: "https://8.8.8.8", Error: true},
+		HostnameFixture{Val: "https://internal", Error: true},
+		HostnameFixture{Val: "https://service.local", Hostname: "service.local", NoSSL: false}, // TODO: SSRF
+	}
+
+	for _, f := range fixtures {
+		hostname, noSSL, err := ParseHostname(f.Val)
+		if f.Error {
+			assert.Error(err)
+			continue
+		}
+		assert.Equal(f.Hostname, hostname)
+		assert.Equal(f.NoSSL, noSSL)
+	}
+}
+
+type TrustedFixture struct {
+	Val     string
+	Domains []string
+	Trusted bool
+}
+
+func TestIsTrustedDomain(t *testing.T) {
+	assert := assert.New(t)
+
+	fixtures := []TrustedFixture{
+		TrustedFixture{Val: "evil.com", Domains: []string{"good.com"}, Trusted: false},
+		TrustedFixture{Val: "pds.host.example.com", Domains: []string{"*.example.com"}, Trusted: true},
+		TrustedFixture{Val: "pds.host.example.com", Domains: []string{"example.com"}, Trusted: false},
+		TrustedFixture{Val: "pds.host.example.com", Domains: []string{"*.good.com"}, Trusted: false},
+		TrustedFixture{Val: "pds.host.example.com", Domains: []string{"*.good.com", "pds.host.example.com"}, Trusted: true},
+		TrustedFixture{Val: "good.com", Domains: []string{"*.good.com"}, Trusted: false},
+	}
+
+	for _, f := range fixtures {
+		assert.Equal(f.Trusted, IsTrustedHostname(f.Val, f.Domains))
+	}
+}
