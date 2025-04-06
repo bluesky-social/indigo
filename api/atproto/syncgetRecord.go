babf990357e2d1aa3637fac8315328346b637a63
bryan newbold <bnewbold@robocracy.org>
2025-04-06 15:04:07 -0700

    update com.atproto lexgen (except for subscribeRepos)

--- api/atproto/syncgetRecord.go
+++ api/atproto/syncgetRecord.go
@@ -13,15 +13,13 @@ import (
 
 // SyncGetRecord calls the XRPC method "com.atproto.sync.getRecord".
 //
-// commit: DEPRECATED: referenced a repo commit by CID, and retrieved record as of that commit
 // did: The DID of the repo.
 // rkey: Record Key
-func SyncGetRecord(ctx context.Context, c *xrpc.Client, collection string, commit string, did string, rkey string) ([]byte, error) {
+func SyncGetRecord(ctx context.Context, c *xrpc.Client, collection string, did string, rkey string) ([]byte, error) {
 	buf := new(bytes.Buffer)
 
 	params := map[string]interface{}{
 		"collection": collection,
-		"commit":     commit,
 		"did":        did,
 		"rkey":       rkey,
 	}
