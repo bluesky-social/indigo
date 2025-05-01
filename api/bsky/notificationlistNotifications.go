d8715e39c5344ce194fe0840aa5efe0909cdd278
Jaz Volpert <ericvolp12@gmail.com>
2025-05-01 18:18:53 +0000

    Update lexgen for bsky lexicons

--- api/bsky/notificationlistNotifications.go
+++ api/bsky/notificationlistNotifications.go
@@ -19,7 +19,7 @@ type NotificationListNotifications_Notification struct {
 	IndexedAt string                             `json:"indexedAt" cborgen:"indexedAt"`
 	IsRead    bool                               `json:"isRead" cborgen:"isRead"`
 	Labels    []*comatprototypes.LabelDefs_Label `json:"labels,omitempty" cborgen:"labels,omitempty"`
-	// reason: Expected values are 'like', 'repost', 'follow', 'mention', 'reply', 'quote', and 'starterpack-joined'.
+	// reason: Expected values are 'like', 'repost', 'follow', 'mention', 'reply', 'quote', 'starterpack-joined', 'verified', and 'unverified'.
 	Reason        string                   `json:"reason" cborgen:"reason"`
 	ReasonSubject *string                  `json:"reasonSubject,omitempty" cborgen:"reasonSubject,omitempty"`
 	Record        *util.LexiconTypeDecoder `json:"record" cborgen:"record"`
