dc6cfb28bc1bb0eb9baf83b4170f124b7eafa00f
bryan newbold <bnewbold@robocracy.org>
2025-05-01 16:33:08 -0700

    remove unused newHost flag

--- cmd/relay/relay/crawl.go
+++ cmd/relay/relay/crawl.go
@@ -15,14 +15,12 @@ func (r *Relay) SubscribeToHost(ctx context.Context, hostname string, noSSL, adm
 	}
 
 	// fetch host info from database. this query will not error if host does not yet exist
-	newHost := false
 	var host models.Host
 	if err := r.db.WithContext(ctx).Find(&host, "hostname = ?", hostname).Error; err != nil {
 		return err
 	}
 
 	if host.ID == 0 {
-		newHost = true
 
 		// check if we're over the limit for new hosts today (bypass if admin mode)
 		if !adminForce && !r.HostPerDayLimiter.Allow() {
@@ -53,7 +51,7 @@ func (r *Relay) SubscribeToHost(ctx context.Context, hostname string, noSSL, adm
 		return fmt.Errorf("cannot subscribe to banned pds")
 	}
 
-	return r.Slurper.Subscribe(&host, newHost)
+	return r.Slurper.Subscribe(&host)
 }
 
 // This function expects to be run when starting up, to re-connect to known active hosts
@@ -69,7 +67,7 @@ func (r *Relay) ResubscribeAllHosts(ctx context.Context) error {
 		logger.Info("re-subscribing to active host")
 		// make a copy of host
 		host := host
-		err := r.Slurper.Subscribe(&host, false)
+		err := r.Slurper.Subscribe(&host)
 		if err != nil {
 			logger.Warn("failed to re-subscribe to host", "err", err)
 		}
