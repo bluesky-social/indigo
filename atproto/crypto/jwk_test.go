920f5025b95ebc9df521cea053d27bbb0fe373f8
bryan newbold <bnewbold@robocracy.org>
2025-05-09 21:28:14 -0700

    fix crypto JWK tests

--- atproto/crypto/jwk_test.go
+++ atproto/crypto/jwk_test.go
@@ -10,18 +10,30 @@ func TestParseJWK(t *testing.T) {
 	assert := assert.New(t)
 
 	jwkTestFixtures := []string{
-		// https://openid.net/specs/draft-jones-json-web-key-03.html
+		// https://datatracker.ietf.org/doc/html/rfc7517#appendix-A.1
+		`{
+			"kty":"EC",
+          	"crv":"P-256",
+          	"x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
+          	"y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
+          	"d":"870MB6gfuTJ4HtUnUvYMyJpr5eUZNP4Bk43bVdj3eAE",
+          	"use":"enc",
+          	"kid":"1"
+      	}`,
+		// https://openid.net/specs/draft-jones-json-web-key-03.html; with kty in addition to alg
 		`{
 			"alg":"EC",
+			"kty":"EC",
 			"crv":"P-256",
 			"x":"MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4",
 			"y":"4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM",
 			"use":"enc",
 			"kid":"1"
 		}`,
-		// https://w3c-ccg.github.io/lds-ecdsa-secp256k1-2019/
+		// https://w3c-ccg.github.io/lds-ecdsa-secp256k1-2019/; with kty in addition to alg
 		`{
     		"alg": "EC",
+    		"kty": "EC",
     		"crv": "secp256k1",
     		"kid": "JUvpllMEYUZ2joO59UNui_XYDqxVqiFLLAJ8klWuPBw",
     		"x": "dWCvM4fTdeM0KmloF57zxtBPXTOythHPMm1HCLrdd3A",
