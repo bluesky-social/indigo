b831c6580a84d39e3ba68eeb6b2b7d8b67d2bb36
bryan newbold <bnewbold@robocracy.org>
2025-05-16 17:41:41 -0700

    iterate on APIClient

--- atproto/client/api_request.go
+++ atproto/client/api_request.go
@@ -3,49 +3,77 @@ package client
 import (
 	"context"
 	"io"
+	"fmt"
 	"net/http"
 	"net/url"
 
 	"github.com/bluesky-social/indigo/atproto/syntax"
 )
 
+var (
+	// atproto API "Query" Lexicon method, which is HTTP GET. Not to be confused with proposed "HTTP QUERY" method.
+	MethodQuery = http.MethodGet
+
+	// atproto API "Procedure" Lexicon method, which is HTTP POST.
+	MethodProcedure = http.MethodPost
+)
+
 type APIRequest struct {
-	HTTPVerb    string // TODO: type?
+	// HTTP method, eg "GET" (required)
+	Method      string
+
+	// atproto API endpoint, as NSID (required)
 	Endpoint    syntax.NSID
+
+	// optional request body. if this is provided, then 'Content-Type' header should be specified
 	Body        io.Reader
-	QueryParams map[string]string // TODO: better type for this?
-	Headers     map[string]string
+
+	// optional query parameters. These will be encoded as provided.
+	QueryParams url.Values
+
+	// optional HTTP headers. Only the first value will be included for each header key ("Set" behavior).
+	Headers     http.Header
 }
 
-func (r *APIRequest) HTTPRequest(ctx context.Context, host string, headers map[string]string) (*http.Request, error) {
+// Turns the API request in to an `http.Request`.
+//
+// `host` parameter should be a URL prefix: schema, hostname, port.
+// `headers` parameters are treated as client-level defaults. Only a single value is allowed per key ("Set" behavior), and will be clobbered by any request-level header values.
+func (r *APIRequest) HTTPRequest(ctx context.Context, host string, headers http.Header) (*http.Request, error) {
 	u, err := url.Parse(host)
 	if err != nil {
 		return nil, err
 	}
-	u.Path = "/xrpc/" + r.Endpoint.String()
-	if r.QueryParams != nil {
-		q := u.Query()
-		for k, v := range r.QueryParams {
-			q.Add(k, v)
-		}
-		u.RawQuery = q.Encode()
+	if u.Host == "" {
+		return nil, fmt.Errorf("empty hostname in host URL")
 	}
-	httpReq, err := http.NewRequestWithContext(ctx, r.HTTPVerb, u.String(), r.Body)
+	if u.Scheme == "" {
+		return nil, fmt.Errorf("empty scheme in host URL")
+	}
+	if r.Endpoint == "" {
+		return nil, fmt.Errorf("empty request endpoint")
+	}
+	u.Path = "/xrpc/" + r.Endpoint.String()
+	u.RawQuery = nil
+	if r.QueryParams != nil {
+		u.RawQuery = r.QueryParams.Encode()
+	}
+	httpReq, err := http.NewRequestWithContext(ctx, r.Method, u.String(), r.Body)
 	if err != nil {
 		return nil, err
 	}
 
 	// first set default headers...
 	if headers != nil {
-		for k, v := range headers {
-			httpReq.Header.Set(k, v)
+		for k := range headers {
+			httpReq.Header.Set(k, headers.Get(k))
 		}
 	}
 
 	// ... then request-specific take priority (overwrite)
 	if r.Headers != nil {
-		for k, v := range r.Headers {
-			httpReq.Header.Set(k, v)
+		for k := range r.Headers {
+			httpReq.Header.Set(k, headers.Get(k))
 		}
 	}
 
