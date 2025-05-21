6624071f7e57ff9418d6af52c785fa9b5fbfd42d
whyrusleeping <why@ipfs.io>
2025-05-20 17:21:20 -0700

    remove all the unused event fields

--- testing/utils.go
+++ testing/utils.go
@@ -666,13 +666,6 @@ func (b *TestRelay) Events(t *testing.T, since int64) *EventStream {
 				es.Lk.Unlock()
 				return nil
 			},
-			RepoHandle: func(evt *atproto.SyncSubscribeRepos_Handle) error {
-				fmt.Println("received handle event: ", evt.Seq, evt.Did)
-				es.Lk.Lock()
-				es.Events = append(es.Events, &events.XRPCStreamEvent{RepoHandle: evt})
-				es.Lk.Unlock()
-				return nil
-			},
 			RepoIdentity: func(evt *atproto.SyncSubscribeRepos_Identity) error {
 				fmt.Println("received identity event: ", evt.Seq, evt.Did)
 				es.Lk.Lock()
