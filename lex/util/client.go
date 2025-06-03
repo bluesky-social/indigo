66566a8f73caa7af0a5d8cbef45e2cf889c5f7bf
bryan newbold <bnewbold@robocracy.org>
2025-06-02 23:31:29 -0700

    better method param names for LexDo()

--- lex/util/client.go
+++ lex/util/client.go
@@ -11,6 +11,8 @@ const (
 )
 
 // API client interface used in lexgen.
+//
+// 'method' is the HTTP method type. 'inputEncoding' is the Content-Type for bodyData in Procedure calls. 'params' are query parameters. 'bodyData' should be either 'nil', an [io.Reader], or a type which can be marshalled to JSON. 'out' is optional; if not nil it should be a pointer to a type which can be un-Marshaled as JSON, for the response body.
 type LexClient interface {
-	LexDo(ctx context.Context, kind string, inpenc string, method string, params map[string]any, bodyobj any, out any) error
+	LexDo(ctx context.Context, method string, inputEncoding string, endpoint string, params map[string]any, bodyData any, out any) error
 }
