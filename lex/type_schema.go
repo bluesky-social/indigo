70081f6cc5286d33cd757b3e50300cf1f294dea5
bryan newbold <bnewbold@robocracy.org>
2025-07-09 13:50:45 -0700

    lexgen: support another true unknown field (modTool meta)

--- lex/type_schema.go
+++ lex/type_schema.go
@@ -552,7 +552,7 @@ func (s *TypeSchema) typeNameForField(name, k string, v TypeSchema) (string, err
 		return "string", nil
 	case "unknown":
 		// NOTE: sometimes a record, for which we want LexiconTypeDecoder, sometimes any object
-		if k == "didDoc" || k == "plcOp" {
+		if k == "didDoc" || k == "plcOp" || k == "meta" {
 			return "interface{}", nil
 		} else {
 			return "*util.LexiconTypeDecoder", nil
