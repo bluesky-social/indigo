d9cf6fb86611c3eebddb0928701e9ef1d3ea3eac
Eli Mallon <eli@aquareum.tv>
2025-07-28 17:03:14 -0700

    atproto/label: fix missing `neg` in lexicon functions

--- atproto/label/label_test.go
+++ atproto/label/label_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"testing"
 
+	comatproto "github.com/bluesky-social/indigo/api/atproto"
 	"github.com/bluesky-social/indigo/atproto/crypto"
 
 	"github.com/stretchr/testify/assert"
@@ -88,4 +89,65 @@ func TestSignLabel(t *testing.T) {
 
 	assert.NoError(l.Sign(priv))
 	assert.NoError(l.VerifySignature(pub))
+}
+
+func TestToLexicon(t *testing.T) {
+	assert := assert.New(t)
+
+	expiresAt := "2025-07-28T23:53:19.804Z"
+	negated := true
+	cid := "bafyreifxykqhed72s26cr4i64rxvrtofeqrly3j4vjzbkvo3ckkjbxjqtq"
+
+	l := Label{
+		CID:       &cid,
+		CreatedAt: "2024-10-23T17:51:19.128Z",
+		ExpiresAt: &expiresAt,
+		Negated:   &negated,
+		SourceDID: "did:plc:ewvi7nxzyoun6zhxrhs64oiz",
+		URI:       "at://did:plc:ewvi7nxzyoun6zhxrhs64oiz/app.bsky.actor.profile/self",
+		Val:       "good",
+		Version:   ATPROTO_LABEL_VERSION,
+		Sig:       []byte("sig"), // invalid, but we only care about the conversion
+	}
+
+	lex := l.ToLexicon()
+	assert.Equal(l.Version, *lex.Ver)
+	assert.Equal(l.CreatedAt, lex.Cts)
+	assert.Equal(l.URI, lex.Uri)
+	assert.Equal(l.Val, lex.Val)
+	assert.Equal(l.CID, lex.Cid)
+	assert.Equal(l.ExpiresAt, lex.Exp)
+	assert.Equal(l.Negated, lex.Neg)
+	assert.Equal(l.SourceDID, lex.Src)
+}
+
+func TestFromLexicon(t *testing.T) {
+	assert := assert.New(t)
+
+	expiresAt := "2025-07-28T23:53:19.804Z"
+	negated := true
+	cid := "bafyreifxykqhed72s26cr4i64rxvrtofeqrly3j4vjzbkvo3ckkjbxjqtq"
+	version := int64(1)
+
+	lex := &comatproto.LabelDefs_Label{
+		Cid: &cid,
+		Cts: "2024-10-23T17:51:19.128Z",
+		Exp: &expiresAt,
+		Neg: &negated,
+		Src: "did:plc:ewvi7nxzyoun6zhxrhs64oiz",
+		Uri: "at://did:plc:ewvi7nxzyoun6zhxrhs64oiz/app.bsky.actor.profile/self",
+		Val: "good",
+		Ver: &version,
+		Sig: []byte("sig"), // invalid, but we only care about the conversion
+	}
+
+	l := FromLexicon(lex)
+	assert.Equal(lex.Ver, &l.Version)
+	assert.Equal(lex.Cts, l.CreatedAt)
+	assert.Equal(lex.Uri, l.URI)
+	assert.Equal(lex.Val, l.Val)
+	assert.Equal(lex.Cid, l.CID)
+	assert.Equal(lex.Exp, l.ExpiresAt)
+	assert.Equal(lex.Neg, l.Negated)
+	assert.Equal(lex.Src, l.SourceDID)
 }
