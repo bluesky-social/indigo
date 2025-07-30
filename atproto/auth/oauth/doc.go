0de0eac7d42db6ce030769c48e26fe6e9b07b636
bryan newbold <bnewbold@robocracy.org>
2025-07-31 01:03:35 +0200

    fix weird whitespace in doc.go

--- atproto/auth/oauth/doc.go
+++ atproto/auth/oauth/doc.go
@@ -20,23 +20,21 @@ Create a single [ClientApp] instance during service setup that will be used (con
 ```
 oauthScope := "atproto transition:generic"
 config := oauth.NewPublicConfig(
-
 	"https://app.example.com/client-metadata.json",
 	"https://app.example.com/oauth/callback",
-
 )
 
 // clients are "public" by default, but if they have secure access to a secret attestation key can be "confidential"
 
-	if CLIENT_SECRET_KEY != "" {
-	    priv, err := crypto.ParsePrivateMultibase(CLIENT_SECRET_KEY)
-	    if err != nil {
-	        return err
-	    }
-	    if err := config.AddClientSecret(priv, "example1"); err != nil {
-	        return err
-	    }
+if CLIENT_SECRET_KEY != "" {
+	priv, err := crypto.ParsePrivateMultibase(CLIENT_SECRET_KEY)
+	if err != nil {
+	    return err
 	}
+	if err := config.AddClientSecret(priv, "example1"); err != nil {
+	    return err
+	}
+}
 
 oauthApp := oauth.NewClientApp(&config, oauth.NewMemStore())
 ```
@@ -48,15 +46,14 @@ The client metadata document needs to be served at the URL indicated by the `cli
 ```
 http.HandleFunc("GET /client-metadata.json", HandleClientMetadata)
 
-	func HandleClientMetadata(w http.ResponseWriter, r *http.Request) {
-		doc := oauthApp.Config.ClientMetadata(oauthScope)
-		w.Header().Set("Content-Type", "application/json")
-		if err := json.NewEncoder(w).Encode(doc); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+func HandleClientMetadata(w http.ResponseWriter, r *http.Request) {
+	doc := oauthApp.Config.ClientMetadata(oauthScope)
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(doc); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-
+}
 ```
 
 The login auth flow starts with a user identifier, which could be an atproto handle, DID, or a host URL. The high-level [StartAuthFlow()] method will resolve the identifier, send an auth request (PAR) to the server, persist request metadata in the [OAuthStore], and return a redirect URL for the user to visit:
@@ -64,19 +61,18 @@ The login auth flow starts with a user identifier, which could be an atproto han
 ```
 http.HandleFunc("GET /oauth/login", HandleLogin)
 
-	func HandleLogin(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+func HandleLogin(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
-	    // parse login identifier from the request
-	    identifier := "..."
+	// parse login identifier from the request
+	identifier := "..."
 
-	    redirectURL, err := oauthApp.StartAuthFlow(ctx, identifier)
-	    if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-	    }
-	    http.Redirect(w, r, redirectURL, http.StatusFound)
+	redirectURL, err := oauthApp.StartAuthFlow(ctx, identifier)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-
+	http.Redirect(w, r, redirectURL, http.StatusFound)
+}
 ```
 
 The service then waits for a callback request on the configured endpoint. The [ProcessCallback()] method will load the earlier request metadata from the [OAuthStore], send an initial token request to the auth server, and validate that the session is consistent with the identifier from the begining of the login flow.
@@ -84,20 +80,19 @@ The service then waits for a callback request on the configured endpoint. The [P
 ```
 http.HandleFunc("GET /client-metadata.json", HandleClientMetadata)
 
-	func HandleOAuthCallback(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+func HandleOAuthCallback(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 
-	    sessData, err := oauthApp.ProcessCallback(ctx, r.URL.Query())
-	    if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-	    }
-
-	    // web services might record the DID in a secure session cookie
-	    _ = sessData.AccountDID
-
-	    http.Redirect(w, r, "/app", http.StatusFound)
+	sessData, err := oauthApp.ProcessCallback(ctx, r.URL.Query())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 
+	// web services might record the DID in a secure session cookie
+	_ = sessData.AccountDID
+
+	http.Redirect(w, r, "/app", http.StatusFound)
+}
 ```
 
 Finally, sessions can be resumed and used to make authenticated API calls to the user's host:
@@ -114,20 +109,19 @@ sess, err := oauthApp.ResumeSession(ctx, did)
 
 c := sess.APIClient()
 
-	body := map[string]any{
-	    "repo":       *c.AccountDID,
-	    "collection": "app.bsky.feed.post",
-	    "record": map[string]any{
-	        "$type":     "app.bsky.feed.post",
-	        "text":      "Hello World via OAuth!",
-	        "createdAt": syntax.DatetimeNow(),
-	    },
-	}
+body := map[string]any{
+	"repo":       *c.AccountDID,
+	"collection": "app.bsky.feed.post",
+	"record": map[string]any{
+	    "$type":     "app.bsky.feed.post",
+	    "text":      "Hello World via OAuth!",
+	    "createdAt": syntax.DatetimeNow(),
+	},
+}
 
-	if err := c.Post(ctx, "com.atproto.repo.createRecord", body, nil); err != nil {
-	    return err
-	}
-
+if err := c.Post(ctx, "com.atproto.repo.createRecord", body, nil); err != nil {
+	return err
+}
 ```
 
 The [ClientSession] will handle nonce updates and token refreshes, and persist the results in the [OAuthStore].
