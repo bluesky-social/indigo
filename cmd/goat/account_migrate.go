6614321e769cab1c132e7ed3ae3ee70627ecd800
Bailey Townsend <baileytownsend2323@gmail.com>
2025-08-01 11:34:31 -0500

    Mostly back to where it was with new input and dep

--- cmd/goat/account_migrate.go
+++ cmd/goat/account_migrate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -8,10 +9,11 @@ import (
 	comatproto "github.com/bluesky-social/indigo/api/atproto"
 	"github.com/bluesky-social/indigo/atproto/syntax"
 	"github.com/bluesky-social/indigo/xrpc"
+	"github.com/manifoldco/promptui"
+	"github.com/urfave/cli/v2"
 	"log/slog"
 	"strings"
-
-	"github.com/urfave/cli/v2"
+	"time"
 )
 
 var cmdAccountMigrate = &cli.Command{
@@ -41,6 +43,10 @@ var cmdAccountMigrate = &cli.Command{
 			Required: true,
 			Usage:    "token from old PDS authorizing token signature",
 			EnvVars:  []string{"PLC_SIGN_TOKEN"},
+		},
+		&cli.StringFlag{
+			Name:  "automatic-plc-token",
+			Usage: "automatically request an plc token, and pauses for the user to enter the plc token",
 		},
 		&cli.StringFlag{
 			Name:  "invite-code",
@@ -76,9 +82,10 @@ func runAccountMigrate(cctx *cli.Context) error {
 		return err
 	}
 	newPassword := cctx.String("new-password")
+	inviteCode := cctx.String("invite-code")
+	newEmail := cctx.String("new-email")
+
 	plcToken := cctx.String("plc-token")
-	//inviteCode := cctx.String("invite-code")
-	//newEmail := cctx.String("new-email")
 
 	newClient := xrpc.Client{
 		Host:      newHostURL,
@@ -99,6 +106,45 @@ func runAccountMigrate(cctx *cli.Context) error {
 	// 1. Create New Account
 	slog.Info("creating account on new host", "handle", newHandle, "host", newHostURL)
 
+	// get service auth token from old host
+	// args: (ctx, client, aud string, exp int64, lxm string)
+	expTimestamp := time.Now().Unix() + 60
+	createAuthResp, err := comatproto.ServerGetServiceAuth(ctx, oldClient, newHostDID.String(), expTimestamp, "com.atproto.server.createAccount")
+	if err != nil {
+		return fmt.Errorf("failed getting service auth token from old host: %w", err)
+	}
+
+	// then create the new account
+	createParams := comatproto.ServerCreateAccount_Input{
+		Did:      &did,
+		Handle:   newHandle,
+		Password: &newPassword,
+	}
+	if newEmail != "" {
+		createParams.Email = &newEmail
+	}
+	if inviteCode != "" {
+		createParams.InviteCode = &inviteCode
+	}
+
+	// use service auth for access token, temporarily
+	newClient.Auth = &xrpc.AuthInfo{
+		Did:        did,
+		Handle:     newHandle,
+		AccessJwt:  createAuthResp.Token,
+		RefreshJwt: createAuthResp.Token,
+	}
+	createAccountResp, err := comatproto.ServerCreateAccount(ctx, &newClient, &createParams)
+	if err != nil {
+		return fmt.Errorf("failed creating new account: %w", err)
+	}
+
+	if createAccountResp.Did != did {
+		return fmt.Errorf("new account DID not a match: %s != %s", createAccountResp.Did, did)
+	}
+	newClient.Auth.AccessJwt = createAccountResp.AccessJwt
+	newClient.Auth.RefreshJwt = createAccountResp.RefreshJwt
+
 	// login client on the new host
 	sess, err := comatproto.ServerCreateSession(ctx, &newClient, &comatproto.ServerCreateSession_Input{
 		Identifier: did,
@@ -114,53 +160,53 @@ func runAccountMigrate(cctx *cli.Context) error {
 	}
 
 	// 2. Migrate Data
-	//slog.Info("migrating repo")
-	//repoBytes, err := comatproto.SyncGetRepo(ctx, oldClient, did, "")
-	//if err != nil {
-	//	return fmt.Errorf("failed exporting repo: %w", err)
-	//}
-	//err = comatproto.RepoImportRepo(ctx, &newClient, bytes.NewReader(repoBytes))
-	//if err != nil {
-	//	return fmt.Errorf("failed importing repo: %w", err)
-	//}
-	//
-	//slog.Info("migrating preferences")
-	//// TODO: service proxy header for AppView?
-	//prefResp, err := agnostic.ActorGetPreferences(ctx, oldClient)
-	//if err != nil {
-	//	return fmt.Errorf("failed fetching old preferences: %w", err)
-	//}
-	//err = agnostic.ActorPutPreferences(ctx, &newClient, &agnostic.ActorPutPreferences_Input{
-	//	Preferences: prefResp.Preferences,
-	//})
-	//if err != nil {
-	//	return fmt.Errorf("failed importing preferences: %w", err)
-	//}
+	slog.Info("migrating repo")
+	repoBytes, err := comatproto.SyncGetRepo(ctx, oldClient, did, "")
+	if err != nil {
+		return fmt.Errorf("failed exporting repo: %w", err)
+	}
+	err = comatproto.RepoImportRepo(ctx, &newClient, bytes.NewReader(repoBytes))
+	if err != nil {
+		return fmt.Errorf("failed importing repo: %w", err)
+	}
 
-	//slog.Info("migrating blobs")
-	//blobCursor := ""
-	//for {
-	//	listResp, err := comatproto.SyncListBlobs(ctx, oldClient, blobCursor, did, 100, "")
-	//	if err != nil {
-	//		return fmt.Errorf("failed listing blobs: %w", err)
-	//	}
-	//	for _, blobCID := range listResp.Cids {
-	//		blobBytes, err := comatproto.SyncGetBlob(ctx, oldClient, blobCID, did)
-	//		if err != nil {
-	//			slog.Warn("failed downloading blob", "cid", blobCID, "err", err)
-	//			continue
-	//		}
-	//		_, err = comatproto.RepoUploadBlob(ctx, &newClient, bytes.NewReader(blobBytes))
-	//		if err != nil {
-	//			slog.Warn("failed uploading blob", "cid", blobCID, "err", err, "size", len(blobBytes))
-	//		}
-	//		slog.Info("transferred blob", "cid", blobCID, "size", len(blobBytes))
-	//	}
-	//	if listResp.Cursor == nil || *listResp.Cursor == "" {
-	//		break
-	//	}
-	//	blobCursor = *listResp.Cursor
-	//}
+	slog.Info("migrating preferences")
+	// TODO: service proxy header for AppView?
+	prefResp, err := agnostic.ActorGetPreferences(ctx, oldClient)
+	if err != nil {
+		return fmt.Errorf("failed fetching old preferences: %w", err)
+	}
+	err = agnostic.ActorPutPreferences(ctx, &newClient, &agnostic.ActorPutPreferences_Input{
+		Preferences: prefResp.Preferences,
+	})
+	if err != nil {
+		return fmt.Errorf("failed importing preferences: %w", err)
+	}
+
+	slog.Info("migrating blobs")
+	blobCursor := ""
+	for {
+		listResp, err := comatproto.SyncListBlobs(ctx, oldClient, blobCursor, did, 100, "")
+		if err != nil {
+			return fmt.Errorf("failed listing blobs: %w", err)
+		}
+		for _, blobCID := range listResp.Cids {
+			blobBytes, err := comatproto.SyncGetBlob(ctx, oldClient, blobCID, did)
+			if err != nil {
+				slog.Warn("failed downloading blob", "cid", blobCID, "err", err)
+				continue
+			}
+			_, err = comatproto.RepoUploadBlob(ctx, &newClient, bytes.NewReader(blobBytes))
+			if err != nil {
+				slog.Warn("failed uploading blob", "cid", blobCID, "err", err, "size", len(blobBytes))
+			}
+			slog.Info("transferred blob", "cid", blobCID, "size", len(blobBytes))
+		}
+		if listResp.Cursor == nil || *listResp.Cursor == "" {
+			break
+		}
+		blobCursor = *listResp.Cursor
+	}
 
 	// display migration status
 	// NOTE: this could check between the old PDS and new PDS, polling in a loop showing progress until all records have been indexed
