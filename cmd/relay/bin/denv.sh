56b11f017193bab6032df337ceb4ee37a93a8db7
rafaeleyng <rafaeleyng@gmail.com>
2025-12-29 17:47:52 -0300

    relay debugging

--- cmd/relay/bin/denv.sh
+++ cmd/relay/bin/denv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-find cmd/relay/ | entr -r go run ./cmd/relay serve
+find cmd/relay/ | entr -r dlv debug --headless --listen=0.0.0.0:12470 --api-version=2 --accept-multiclient --continue ./cmd/relay -- serve
