2eb01575c5a476456dbd90df7c781a95b5a19e0e
Dave Cheney <dave@cheney.net>
2026-01-30 15:45:36 +1100

    cmd/tap: introduce runPeriodically helper
    There is a common pattern that a component wants to run a periodic
    operation until being cancelled via context. This PR extracts that
    pattern into a helper and as a proof of concept refactors RunCursorSaver
    to use it.

--- cmd/tap/util.go
+++ cmd/tap/util.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"math/rand"
@@ -80,5 +81,22 @@ func parseOutboxMode(webhookURL string, disableAcks bool) OutboxMode {
 		return OutboxModeFireAndForget
 	} else {
 		return OutboxModeWebsocketAck
+	}
+}
+
+// runPeriodically runs the provided task function at the specified interval until the context is done or an error occurs.
+func runPeriodically(ctx context.Context, interval time.Duration, task func(context.Context) error) error {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			if err := task(ctx); err != nil {
+				return fmt.Errorf("periodic task failed: %w", err)
+			}
+		}
 	}
 }
