9799b4b3bfdec05f47693c9e754472c93a823944
dholms <dtholmgren@gmail.com>
2026-03-10 17:02:23 -0500

    swithc from limit to on/off

--- cmd/tap/tap.go
+++ cmd/tap/tap.go
@@ -40,7 +40,7 @@ type TapConfig struct {
 	ResyncParallelism          int
 	OutboxParallelism          int
 	FirehoseCursorSaveInterval time.Duration
-	FirehoseReplayLimit        time.Duration
+	NoReplay                   bool
 	RepoFetchTimeout           time.Duration
 	IdentityCacheSize          int
 	EventCacheSize             int
