88cb5ccc515ea8b928947ac890b8b3540403f544
bryan newbold <bnewbold@robocracy.org>
2026-08-13 14:47:11 -0700

    lexicon: improve doc strings

--- atproto/lexicon/extract_type.go
+++ atproto/lexicon/extract_type.go
@@ -4,12 +4,12 @@ import (
 	"encoding/json"
 )
 
-// Helper type for extracting record type from JSON
+// Helper type for extracting schema type from schema JSON
 type genericSchemaDef struct {
 	Type string `json:"type"`
 }
 
-// Parses the top-level $type field from generic atproto JSON data
+// Parses the top-level 'type' field from schema JSON data
 func ExtractTypeJSON(b []byte) (string, error) {
 	var gsd genericSchemaDef
 	if err := json.Unmarshal(b, &gsd); err != nil {
